a6ebedeb9eb845029d36f27650b407670fa569df
anikeenko <dimskay1988@icloud.com>
2022-12-22 16:03:29 +0300

    add UserViewSet

--- apps/Employees/views.py
+++ apps/Employees/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from apps.Employees.models import Employee
+from apps.Employees.serializers import UserSerializer
 
-# Create your views here.
+
+class UsersViewSet(ModelViewSet):
+    """List of all users"""
+    serializer_class = UserSerializer
+    queryset = Employee.objects.all()
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
