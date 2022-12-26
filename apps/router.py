a928afc739c29df55d0e3a250bda7bba6db6f58b
nnnLik <makhmudov_ra@vk.com>
2022-12-26 03:28:59 -0800

    [EDIT] swagger route

--- apps/router.py
+++ apps/router.py
@@ -1,7 +1,25 @@
 from django.urls import path, include
 
+from rest_framework import permissions
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+	openapi.Info(
+		title="StaffControl API",
+		default_version='v1.0.0',
+		description="StaffControl",
+	),
+	public=True,
+	permission_classes=[permissions.AllowAny],
+)
 
 routes = [
     path('employees/', include('apps.Employees.urls')),
     path('locations/', include('apps.Locations.urls')),
+    path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc')
 ]
