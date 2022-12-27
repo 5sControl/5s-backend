6b42911385b4f34f4776013b0d41bd0111964983
anikeenko <dimskay1988@icloud.com>
2022-12-27 14:30:53 +0300

    Edit admin

--- apps/Employees/admin.py
+++ apps/Employees/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import CustomUser, History
+from apps.Locations.models import Location
 from django.utils.safestring import mark_safe
 
 
@@ -11,9 +12,18 @@ class EmployeeAdmin(admin.ModelAdmin):
 
 @admin.register(History)
 class HistoryAdmin(admin.ModelAdmin):
-    list_display = ('people', 'id', 'location', 'entry_date', 'release_date', 'dataset_user', 'get_image')
+    # location = CustomUser.location
+    list_display = ('people', 'id', 'entry_date', 'release_date', 'get_image', 'dataset', 'location')
     list_filter = ('people', 'id')
     readonly_fields = ("get_image",)
+
+    def location(self, obj):
+        result = CustomUser.objects.filter(id=obj.people.id).values('location_id')[0]['location_id']
+        return result
+
+    def dataset(self, obj):
+        result = CustomUser.objects.filter(id=obj.people.id).values('dataset')[0]['dataset']
+        return result
 
     def get_image(self, obj):
         return mark_safe(f'<img src="{obj.image.url} "width="50" height="60" />')
