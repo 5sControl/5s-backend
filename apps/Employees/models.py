0885840a3e0e9a49fe16fb590a43a8f08211438d
nnnLik <makhmudov_ra@vk.com>
2023-01-03 04:15:26 -0800

    [EDIT] default name for unkm=now users

--- apps/Employees/models.py
+++ apps/Employees/models.py
@@ -1,32 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.core.files.uploadedfile import InMemoryUploadedFile
 from apps.Locations.models import Location
 
 
-# class ImageUsers(models.Model):
-#     image_user = models.ImageField(upload_to='attachments')
-
-
 class CustomUser(models.Model):
-    first_name = models.CharField(max_length=40, blank=True, null=True)
-    last_name = models.CharField(max_length=40, blank=True, null=True)
+    first_name = models.CharField(default='Unknowm', max_length=40, blank=True, null=True)
+    last_name = models.CharField(default='Unknowm', max_length=40, blank=True, null=True)
     dataset = models.TextField(verbose_name='Date Set user', blank=True, null=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-    # image = models.ForeignKey(ImageUsers, on_delete=models.CASCADE, blank=True)
     image = models.ImageField(upload_to='photo')
-
-
-    # def save(self, *args, **kwargs):
-    #     dataset =''
-    #     validated_data = self.image
-    #     for image_data in validated_data.get('image'):
-    #         link = f'{image_data.image_user}'.split('/')[0].lower()
-    #         links = f'{image_data.image_user}'.split('/')[1]
-    #         face_img = face_recognition.load_image_file(f"media/{link}/{links}")
-    #         face_enc = face_recognition.face_encodings(face_img)[0]
-    #         dataset += f'{face_enc}'
-    #     return super(CustomUser).save(dataset)
 
 
     def __str__(self):
@@ -48,26 +29,6 @@ class History(models.Model):
 
     def __str__(self):
         return f'{self.location}'
-
-    def save(self, *args, **kwargs):
-        # Opening the uploaded image
-        im = Image.open(self.image)
-
-        if im.mode == "JPEG":
-            pass
-        elif im.mode in ["RGBA", "P"]:
-            im = im.convert("RGB")
-
-        output = BytesIO()
-        
-        im.save(output, format='JPEG', subsampling=0, quality=95)
-        output.seek(0)
-
-        self.image = InMemoryUploadedFile(output, 'ImageField',
-                                                  "%s.jpg" % self.image.name.split('.')[0], 'image/jpeg',
-                                                  sys.getsizeof(output), None)
-        super(History, self).save()
-
 
     class Meta:
         verbose_name = 'History'
