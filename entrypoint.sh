547ae455a74f07efa31adf4877f1463d38d68a9c
Artsiom <artsiomzhuchkou2401@gmail.com>
2023-01-05 13:06:43 +0300

    fix: edited server host

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 sudo chown $(whoami):$(whoami) /usr/src/app/database &&
 yes Y | python manage.py makemigrations &&
 python manage.py migrate &&
-python manage.py runserver
+python manage.py runserver 0.0.0.0:8000
