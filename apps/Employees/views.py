df1482a34501d6a765dcffe7e0bee97876d24e23
nnnLik <makhmudov_ra@vk.com>
2023-01-12 22:34:49 +0300

    [ADD] count of people in all locations

--- apps/Employees/views.py
+++ apps/Employees/views.py
@@ -1,10 +1,10 @@
 from rest_framework.viewsets import ModelViewSet
 
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, response
 
 from django.contrib.auth.models import User
 
-from apps.Employees.serializers import EmployeeSerializer, HistorySerializer
+from apps.Employees.serializers import EmployeeSerializer, HistorySerializer, PeopleLocationsSerializers
 from apps.Employees.serializers import UserSerializer
 
 from apps.Employees.models import CustomUser, History
@@ -18,8 +18,9 @@ class UsersViewSet(ModelViewSet):
     queryset = User.objects.all()
 
     permission_classes = [
-            permissions.IsAuthenticatedOrReadOnly,
-            IsAdminOrReadOnly,]
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly,
+    ]
 
 
 class HistoryViewSet(ModelViewSet):
@@ -29,27 +30,49 @@ class HistoryViewSet(ModelViewSet):
     queryset = History.objects.all()
 
     permission_classes = [
-            permissions.IsAuthenticatedOrReadOnly,
-            IsAdminOrReadOnly,]
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly,
+    ]
 
 
 class EmployeeViewSet(ModelViewSet):
     """List of all employee"""
-	
+
     serializer_class = EmployeeSerializer
     queryset = CustomUser.objects.all()
 
     permission_classes = [
-            permissions.IsAuthenticatedOrReadOnly,
-            IsAdminOrReadOnly,]
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAdminOrReadOnly,
+    ]
 
 
-class PeopleViewSet(viewsets.ViewSet):
+class PeopleViewSet(viewsets.ReadOnlyModelViewSet):
     """List of all history and people"""
 
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly,
-        IsAdminOrReadOnly,]
+        IsAdminOrReadOnly,
+    ]
+    serializer_class = PeopleLocationsSerializers
 
-    def list(self, request):
-        ...
+    def list(self, request, *args, **kwargs):
+        locations = []
+        users_by_locations = {}
+
+        user_info = CustomUser.objects.all()
+        for location in user_info:
+            if location.location != None:
+                locations.append(str(location.location))
+            else:
+                locations.append(location.location)
+        print(f'[INFO] {locations}')
+
+        for location in locations:
+            users_by_locations[location] = list((CustomUser.objects
+                                                 .filter(location__name=location)
+                                                 .values_list(
+                                                     'first_name', 'last_name', 'date_joined')))
+        print(f'[INFO] {users_by_locations}')
+
+        return response.Response(users_by_locations)
