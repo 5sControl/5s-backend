73b7ed4e9bb8f59b23e86d3bd5704ccc4063936d
anikeenko <dimskay1988@icloud.com>
2023-01-12 17:47:01 +0300

    FIX settings, views

--- apps/Locations/views.py
+++ apps/Locations/views.py
@@ -1,9 +1,9 @@
 from rest_framework.viewsets import ModelViewSet
 from rest_framework import permissions
 from apps.Locations.models import Camera, Gate, Location
-
 from apps.Locations.serializers import CameraSerializer, GateSerializer, LocationSerializer
 from apps.base.permissions import IsAdminOrReadOnly
+
 
 class CameraViewSet(ModelViewSet):
     """List of all Camer"""
@@ -11,9 +11,9 @@ class CameraViewSet(ModelViewSet):
     serializer_class = CameraSerializer
     queryset = Camera.objects.all()
 
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsAdminOrReadOnly,]
+    # permission_classes = [
+    #     permissions.IsAuthenticatedOrReadOnly,
+    #     IsAdminOrReadOnly,]
 
 
 class GateViewSet(ModelViewSet):
@@ -22,9 +22,10 @@ class GateViewSet(ModelViewSet):
     serializer_class = GateSerializer
     queryset = Gate.objects.all()
 
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsAdminOrReadOnly,]
+    # permission_classes = [
+    #     permissions.IsAuthenticatedOrReadOnly,
+    #     IsAdminOrReadOnly,]
+
 
 class LocationViewSet(ModelViewSet):
     """List of all locations"""
@@ -32,6 +33,6 @@ class LocationViewSet(ModelViewSet):
     serializer_class = LocationSerializer
     queryset = Location.objects.all()
 
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly,
-        IsAdminOrReadOnly,]
+    # permission_classes = [
+    #     permissions.IsAuthenticatedOrReadOnly,
+    #     IsAdminOrReadOnly,]
