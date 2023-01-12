b6a1da7b1d979a6b14331e153027d3a49e012686
nnnLik <makhmudov_ra@vk.com>
2023-01-12 15:06:58 +0300

    [ADD] location in custom user model

--- apps/Employees/models.py
+++ apps/Employees/models.py
@@ -4,12 +4,20 @@ from apps.Locations.models import Location, Camera
 from django.utils.safestring import mark_safe
 
 class CustomUser(models.Model):
+    """Employee"""
+
     first_name = models.CharField(default='Unknown', max_length=40, blank=True, null=True)
     last_name = models.CharField(default='Unknown', max_length=40, blank=True, null=True)
+
     dataset = models.TextField(verbose_name='Date Set user', blank=True, null=True)
-    date_joined = models.DateTimeField(auto_now_add=True)
+    
     image = models.ImageField(upload_to="")
-    status = models.BooleanField(default=False, verbose_name='Status in location',)
+
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True,
+                                            related_name='location')
+
+    status = models.BooleanField(verbose_name='Status in location', default=False,)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     @property
     def image_preview(self):
@@ -26,15 +34,22 @@ class CustomUser(models.Model):
 
 
 class History(models.Model):
+    """History of visits to locations"""
+
+    action = models.CharField(verbose_name='action camera', blank=True, null=True, max_length=50)
+
+
     people = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True,
-                               related_name='people_in_location')
+                                related_name='people_in_location')
     location = models.ForeignKey(Location, related_name='Location_users',
-                                 on_delete=models.CASCADE, blank=True, null=True)
+                                on_delete=models.CASCADE, blank=True, null=True)
+
     entry_date = models.DateTimeField(auto_now_add=True)
     release_date = models.DateTimeField(blank=True, null=True)
+
     image = models.CharField(verbose_name='Image', blank=True, null=True, max_length=200)
+
     camera = models.ForeignKey(Camera, on_delete=models.CASCADE, verbose_name='NameCamera')
-    action = models.CharField(verbose_name='action camera', blank=True, null=True, max_length=50)
 
     @property
     def image_preview(self):
