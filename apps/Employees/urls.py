dd4d853cfb117348298954c52b23a15e2cbd744c
nnnLik <makhmudov_ra@vk.com>
2023-01-12 11:40:21 +0300

    [FIX] import amd debug mode route for image

--- apps/Employees/urls.py
+++ apps/Employees/urls.py
@@ -1,22 +1,12 @@
-from idlelib.multicall import r
 from rest_framework.routers import DefaultRouter
 from apps.Employees.views import UsersViewSet, HistoryViewSet, EmployeeViewSet, PeopleViewSet
-from django.urls import re_path as url
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
 
 
 router = DefaultRouter()
 
-router.register(r'users', UsersViewSet, basename='users')
+router.register(r'admins', UsersViewSet, basename='users')
 router.register(r'history', HistoryViewSet, basename='stories')
 router.register(r'employ', EmployeeViewSet, basename='employs')
 router.register(r'count_of_people', PeopleViewSet, basename='people')
 
 urlpatterns = router.urls
-
-urlpatterns += [
-    url(r'input/', views.ContactView.as_view()),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
