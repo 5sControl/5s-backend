470fb848b4fa7106b2390c99e05944d873e33826
Artsiom <56683813+ZhuchkouAA@users.noreply.github.com>
2023-01-12 16:44:25 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-yes Y | python manage.py makemigrations &&
-python manage.py migrate &&
 python manage.py runserver 0.0.0.0:8000
+# yes Y | python manage.py makemigrations &&
+# python manage.py migrate &&
+
