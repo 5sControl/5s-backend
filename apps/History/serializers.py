16f2e806847dacbe60fa4e01f3ac2a534121f29a
nnnLik <makhmudov_ra@vk.com>
2023-01-13 15:29:19 +0300

    [ADD] history serializer

--- apps/History/serializers.py
+++ apps/History/serializers.py
@@ -1,3 +1,5 @@
+import datetime
+
 from .models import History
 
 from rest_framework import serializers
@@ -12,12 +14,14 @@ class HistorySerializer(serializers.ModelSerializer):
     """
 
     def create(self, validated_data):
-        if validated_data['action'] == 'entrance':
+        if validated_data['action'] == 'entrance' and validated_data['name_file'] != 'unknown':
+            print(f'[INFO] validation data is: %s' % validated_data)
+        
             image = validated_data['image']
             camera = validated_data['camera']
             action = validated_data['action']
             name_file = validated_data['name_file']
-            id_people = int(((f"[{validated_data['name_file']}").split('_')[-1]).split('.')[0])
+            id_people = int(((f"{validated_data['name_file']}").split('_')[-1]).split('.')[0])
             location = Location.objects.filter(gate_id__camera_input__id=validated_data['camera'])[0]
             history_data = History(
                 camera=camera,
@@ -32,27 +36,56 @@ class HistorySerializer(serializers.ModelSerializer):
             user = CustomUser.objects.filter(id=id_people)
             user.update(status=True, location=location)
             return history_data
-        else:
-            if validated_data['action'] == 'exit':
-                image = validated_data['image']
-                camera = validated_data['camera']
-                action = validated_data['action']
-                name_file = validated_data['name_file']
-                id_people = int(((f"[{validated_data['name_file']}").split('_')[-1]).split('.')[0])
-                location = Location.objects.filter(gate_id__camera_output__id=validated_data['camera'])[0]
-                history_data = History(
-                    camera=camera,
-                    action=action,
-                    name_file=name_file,
-                    location=location,
-                    people=CustomUser.objects.get(id=id_people),
-                    image=image
-                )
-                history_data.save()
 
-                user = CustomUser.objects.filter(id=id_people)
-                user.update(status=False, location=None)
-                return history_data
+        elif validated_data['action'] == 'exit' and validated_data['name_file'] != 'unknown':
+            image = validated_data['image']
+            camera = validated_data['camera']
+            action = validated_data['action']
+            name_file = validated_data['name_file']
+            id_people = int(((f"{validated_data['name_file']}").split('_')[-1]).split('.')[0])
+            location = Location.objects.filter(gate_id__camera_output__id=validated_data['camera'])[0]
+            release_date = datetime.datetime.now()
+            history_data = History(
+                camera=camera,
+                action=action,
+                name_file=name_file,
+                location=location,
+                release_date=release_date,
+                people=CustomUser.objects.get(id=id_people),
+                image=image
+            )
+            history_data.save()
+
+            user = CustomUser.objects.filter(id=id_people)
+            user.update(status=False, location=None)
+            return history_data
+
+        elif validated_data['name_file'] == 'unknown':
+            image = validated_data['image']
+            camera = validated_data['camera']
+            action = validated_data['action']
+
+            release_date = []
+            location = []
+            if validated_data['action'] == 'entrance':
+                release_date.append(None)
+                location.append(Location.objects.get(gate_id__camera_input__id=validated_data['camera']))
+            else:
+                if validated_data['action'] == 'exit':
+                    release_date.append(f"{datetime.datetime.now()}")
+                    location.append(Location.objects.get(gate_id__camera_output__id=validated_data['camera']))
+            history_data = History(
+                camera=camera,
+                action=action,
+                release_date=release_date[0],
+                name_file=None,
+                location=location[0],
+                people=None,
+                image=image
+            )
+            history_data.save()
+            return history_data
+
         return
 
     class Meta:
