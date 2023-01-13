dbe2db99eac497691c2d56518965a2bd31c6d768
nnnLik <makhmudov_ra@vk.com>
2023-01-13 11:26:09 +0300

    [EDIT] history admin panel

--- apps/Employees/admin.py
+++ apps/Employees/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser, History
-from django.utils.safestring import mark_safe
-
+from .models import CustomUser
 
 @admin.register(CustomUser)
 class EmployeeAdmin(admin.ModelAdmin):
@@ -14,23 +12,3 @@ class EmployeeAdmin(admin.ModelAdmin):
 
     image_preview.short_description = 'Image Preview'
     image_preview.allow_tags = True
-
-@admin.register(History)
-class HistoryAdmin(admin.ModelAdmin):
-    list_display = ('people', 'entry_date', 'release_date', 'action', 'location', 'image_tag')
-    list_filter = ('people', 'location', 'id', 'entry_date', 'action')
-    readonly_fields = ('people', 'entry_date', 'release_date', 'action', 'location', 'image_tags')
-
-    def location(self, obj):
-        result = CustomUser.objects.filter(id=obj.people.id).values('location_id')[0]['location_id']
-        return result
-
-    def dataset(self, obj):
-        result = CustomUser.objects.filter(id=obj.people.id).values('dataset')[0]['dataset']
-        return result
-
-    def image_tag(self, obj):
-        return mark_safe(f'<img src="/{obj.image}" width="50px" height="60px" />')
-
-    def image_tags(self, obj):
-        return mark_safe(f'<img src="/{obj.image}" width="400px" height="400px" />')
