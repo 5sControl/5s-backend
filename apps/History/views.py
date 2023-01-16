736d214784c67a7942c6121e82edee889c3ce500
nnnLik <makhmudov_ra@vk.com>
2023-01-16 11:00:10 +0300

    [ADD] search and backend fields

--- apps/History/views.py
+++ apps/History/views.py
@@ -1,15 +1,22 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, generics, response, filters
 
 from apps.base.permissions import IsAdminOrReadOnly
 
 from .serializers import HistorySerializer
 from .models import History
 
+from django_filters import rest_framework as django_filters
+
+
 class HistoryViewSet(viewsets.ModelViewSet):
     """List of all history"""
 
     serializer_class = HistorySerializer
     queryset = History.objects.all()
+
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['action', 'location__name', 'camera__id',
+            'people__first_name', 'people__last_name', 'entry_date']
 
     # permission_classes = [
     #     permissions.IsAuthenticatedOrReadOnly,
