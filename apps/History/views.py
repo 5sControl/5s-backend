8de4e713529caffdc8a2b89c09b81b714fdb33e1
nnnLik <makhmudov_ra@vk.com>
2023-01-16 21:04:37 +0300

    [ADD] filtered history records by type

--- apps/History/views.py
+++ apps/History/views.py
@@ -1,24 +1,53 @@
-from rest_framework import permissions, viewsets, generics, response, filters
+from rest_framework import views, viewsets, filters
+from rest_framework.response import Response
 
 from apps.base.permissions import IsAdminOrReadOnly
 
 from .serializers import HistorySerializer
 from .models import History
 
-from django_filters import rest_framework as django_filters
-
-
 class HistoryViewSet(viewsets.ModelViewSet):
     """List of all history"""
 
-    serializer_class = HistorySerializer
     queryset = History.objects.all()
 
     filter_backends = [filters.SearchFilter]
     search_fields = ['action', 'location__name', 'camera__id',
             'people__first_name', 'people__last_name', 'entry_date']
 
-    # permission_classes = [
-    #     permissions.IsAuthenticatedOrReadOnly,
-    #     IsAdminOrReadOnly,
-    # ]
+
+class FilteredHistoryModelViewSet(views.APIView):
+    """Filtered History View"""
+
+    def post(self, request, format=None):
+        # check required keys
+        try:
+            if request.data.get('type') is not None:
+                type = request.data.get('type') 
+            else:
+                raise AttributeError
+            if request.data.get('from_date') is not None:
+                from_date = request.data.get('from_date')
+            else:
+                raise AttributeError
+            if request.data.get('to_date') is not None:
+                to_date = request.data.get('to_date') 
+            else:
+                raise AttributeError
+        except AttributeError:
+            return Response({'message': 'The required keys do not exist'})
+        else:
+            if type == 'between':
+                serializer = HistorySerializer(History.objects.filter(entry_date__gte=from_date, entry_date__lte=to_date), many=True)
+                return Response(serializer.data)
+            elif type == 'before':
+                serializer = HistorySerializer(History.objects.filter(entry_date__lte=from_date), many=True)
+                return Response(serializer.data)
+            elif type == 'after':
+                serializer = HistorySerializer(History.objects.filter(entry_date__gte=from_date), many=True)
+                return Response(serializer.data)
+            else:
+                return Response({'message': 'Invalid type'})
+
+
+    
