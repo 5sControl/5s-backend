dd5e56eec8dee7db7be10f1d1c93be7c8ff8ec0f
nnnLik <makhmudov_ra@vk.com>
2023-01-16 10:59:43 +0300

    [EDIT] filtred route

--- apps/History/urls.py
+++ apps/History/urls.py
@@ -2,9 +2,11 @@ from rest_framework.routers import DefaultRouter
 
 from .views import HistoryViewSet
 
+from django.urls import path
+
 router = DefaultRouter()
 
 router.register(r'', HistoryViewSet, basename='History records')
-# router.register(r'record', , basename='people')
 
 urlpatterns = router.urls
+
