b2af5c38739fbc85296ea83eaf27c06d9638029c
anikeenko <dimskay1988@icloud.com>
2023-01-18 17:20:56 +0300

    ADD in models fields image1, image2, image3, image4

--- apps/Employees/models.py
+++ apps/Employees/models.py
@@ -10,15 +10,19 @@ class CustomUser(models.Model):
     first_name = models.CharField(default='Unknown', max_length=40, blank=True, null=True)
     last_name = models.CharField(default='Unknown', max_length=40, blank=True, null=True)
     dataset = models.TextField(verbose_name='Date Set user', blank=True, null=True)
-    image = models.ImageField(upload_to="")
+    image1 = models.ImageField(upload_to="", verbose_name='photo below')
+    image2 = models.ImageField(upload_to="", verbose_name='photo from above')
+    image3 = models.ImageField(upload_to="", verbose_name='photo in the center')
+    image4 = models.ImageField(upload_to="", verbose_name='photo on the left')
+    image5 = models.ImageField(upload_to="", verbose_name='photo on the right')
     location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=True, null=True, related_name='location')
     status = models.BooleanField(verbose_name='Status in location', default=False,)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     @property
     def image_preview(self):
-        if self.image:
-            return mark_safe('<img src="{}" width="450" height="300" />'.format(self.image.url))
+        if self.image3:
+            return mark_safe('<img src="{}" width="450" height="300" />'.format(self.image3.url))
         return ""
 
     def __str__(self):
