69aeb46bfa1e1267e52fc8d22bacc1169f0b275d
anikeenko <dimskay1988@icloud.com>
2023-01-18 17:21:18 +0300

    EDIT admin

--- apps/Employees/admin.py
+++ apps/Employees/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from .models import CustomUser
 
+
 @admin.register(CustomUser)
 class EmployeeAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'image_preview',)
     list_filter = ('id', 'status', 'date_joined')
-    readonly_fields =('first_name', 'last_name', 'image_preview', 'status', 'date_joined')
+    readonly_fields = ('first_name', 'last_name', 'image_preview', 'status', 'date_joined')
         
     def image_preview(self, obj):
         return obj.image_preview
