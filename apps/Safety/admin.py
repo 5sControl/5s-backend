0e6ccd1f98ca3505ac5c1a67e385a7faba31262e
anikeenko <dimskay1988@icloud.com>
2023-02-02 18:34:50 +0300

    EDIT admin added ActionAdmin

--- apps/Safety/admin.py
+++ apps/Safety/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Action
 
-# Register your models here.
+
+@admin.register(Action)
+class ActionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'image', 'action', 'name_file', 'camera', 'date_created')
+    list_filter = ('id', 'image', 'action', 'name_file', 'camera', 'date_created')
