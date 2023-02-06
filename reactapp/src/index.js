9865d2bbdd579ba82a1a7e14b42821f606d0697d
etosolo322 <etosolo322@yandex.ru>
2023-02-06 13:37:44 +0300

    edit route

--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-export const API_URL = "http://0.0.0.0:8080/api/employees/admin/"
+export const API_URL = "http://0.0.0.0:8080/api/safety/action/"
 export const API_STATIC_MEDIA = "http://0.0.0.0:8000/static/"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
