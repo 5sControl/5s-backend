eb4e5f19f206eba432aeb7da27d872a33c3e3c7c
etosolo322 <etosolo322@yandex.ru>
2023-02-06 13:53:32 +0300

    ready

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,22 +1,24 @@
 import './App.css';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
 
 import { API_URL } from '.';
 
-const getStudents = (data)=>{
-  axios.get(API_URL).then(data => console.log(data.data))
-}
-
-
-
 function App() {
+  const [data, setData] = useState([])
   useEffect(()=>{
-    getStudents()
+      axios.get(API_URL).then(data => setData(data.data.results))
   },[])
   return (
     <div className="App">
-      sdfsdfsdf
+      {data.map((el) => {
+        return(
+        <div key={el.id}>
+          <span>{el.id}</span>
+          <img src={el.image} alt='image'/>
+          <span>{el.action}</span>
+        </div>) 
+      })}
     </div>
   );
 }
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -8,7 +8,5 @@ export const API_STATIC_MEDIA = "http://0.0.0.0:8000/static/"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
