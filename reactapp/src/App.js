e9cad54d7d0ff2bbe716283f093d5114e062d95d
etosolo322 <etosolo322@yandex.ru>
2023-02-06 13:04:52 +0300

    update reactapp

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,6 +1,19 @@
 import './App.css';
+import { useEffect } from 'react';
+import axios from "axios";
+
+import { API_URL } from '.';
+
+const getStudents = (data)=>{
+  axios.get(API_URL).then(data => console.log(data.data))
+}
+
+
 
 function App() {
+  useEffect(()=>{
+    getStudents()
+  },[])
   return (
     <div className="App">
       sdfsdfsdf
--- reactapp/src/index.js
+++ reactapp/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+export const API_URL = "http://0.0.0.0:8080/api/employees/admin/"
+export const API_STATIC_MEDIA = "http://0.0.0.0:8000/static/"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
