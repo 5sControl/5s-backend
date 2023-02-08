8aa94446cc09607488e70b09ca5f9f7a8c43b4cf
nnnLik <makhmudov_ra@vk.com>
2023-02-08 16:26:25 +0300

    [FIX] route

--- config/urls.py
+++ config/urls.py
@@ -1,28 +1,8 @@
-"""StaffControl URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from apps.router import routes
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
 from .views import RegisterView, setcookie, getcookie
 
 
@@ -35,7 +15,6 @@ urlpatterns = [
     path("api/", include(routes)),
 ]
 urlpatterns += [
-    path("api-auth/", include("rest_framework.urls")),
     path("scookie", setcookie),
     path("gcookie", getcookie),
 ]
