86dda80df572adac669110458735a7a44a7aa6e1
etosolo322 <etosolo322@yandex.ru>
2023-02-08 16:24:25 +0300

    migrate front to uniq repo

--- reactapp/src/api/api.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = "http://192.168.1.101/api/safety/action/?page=2"
-export const API_IMAGES = "http://192.168.1.101/"
-export const API_STATIC_MEDIA = "http://192.168.1.101/static/"
-export const API_CAMERA = "http://192.168.1.101/api/locations/get_all_cameras/"
