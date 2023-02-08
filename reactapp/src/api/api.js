c66a8128167c6c24760700b0a42a6bdbf93781d2
etosolo322 <etosolo322@yandex.ru>
2023-02-08 13:17:52 +0300

    add /

--- reactapp/src/api/api.js
+++ reactapp/src/api/api.js
@@ -1,3 +1,3 @@
 export const API_URL = "http://192.168.1.101/api/safety/action/"
-export const API_IMAGES = "http://192.168.1.101"
+export const API_IMAGES = "http://192.168.1.101/"
 export const API_STATIC_MEDIA = "http://192.168.1.101/static/"
