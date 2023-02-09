d2346d8e788834bdeb8197449d1674058e8bb997
nnnLik <makhmudov_ra@vk.com>
2023-02-09 11:17:34 +0300

    [EDIT] display reversed querysets

--- apps/Safety/views.py
+++ apps/Safety/views.py
@@ -8,10 +8,9 @@ class ActionViewSet(ModelViewSet):
     """List of all action"""
 
     serializer_class = ActionSerializer
-    queryset = Action.objects.all()
+    queryset = Action.objects.all().order_by("-id")
 
     def get_permissions(self):
-        if self.request.method != 'POST':
+        if self.request.method != "POST":
             return [IsAuthenticated()]
         return []
-
