fe78d6d38f6ceaea8dd8f871afe9b9f255c4c55a
nnnLik <makhmudov_ra@vk.com>
2023-02-13 19:31:55 +0300

    [EDIT] str method overloaded

--- src/Algorithms/models.py
+++ src/Algorithms/models.py
@@ -11,6 +11,9 @@ class Algorithm(models.Model):
     name = models.CharField(max_length=100)
     is_available = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.name
+
 
 class CameraAlgorithm(models.Model):
     """
