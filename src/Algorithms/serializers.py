d10d52c46de9e48e61ff5fd08ca84530f148017f
nnnLik <makhmudov_ra@vk.com>
2023-02-13 20:19:04 +0300

    [FIX] serializer create method

--- src/Algorithms/serializers.py
+++ src/Algorithms/serializers.py
@@ -3,8 +3,6 @@ from rest_framework import serializers
 from src.Algorithms.models import CameraAlgorithm
 from .models import Algorithm
 
-from django.core.exceptions import ValidationError
-
 
 class AlgorithmUpdateSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,20 +14,6 @@ class AlgorithmUpdateSerializer(serializers.ModelSerializer):
 
 
 class CameraAlgorithmSerializer(serializers.ModelSerializer):
-    algorithm = serializers.CharField(write_only=True)
-
     class Meta:
         model = CameraAlgorithm
         fields = ("algorithm", "camera_id")
-
-    def create(self, validated_data):
-        algorithm_name = validated_data.pop("algorithm")
-        algorithm = Algorithm.objects.get(name=algorithm_name)
-        if algorithm:
-            validated_data["algorithm"] = algorithm
-            camera_algorithm = CameraAlgorithm.objects.create(**validated_data)
-            return camera_algorithm
-        else:
-            raise ValidationError(
-                {"error": f"Algorithm with name {algorithm_name} does not exists"}
-            )
