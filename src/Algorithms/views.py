2bedc2afec73827d249a13ccd3a28cdc3a96ea4b
nnnLik <makhmudov_ra@vk.com>
2023-02-13 19:32:36 +0300

    [ADD] perform_create

--- src/Algorithms/views.py
+++ src/Algorithms/views.py
@@ -22,3 +22,6 @@ class AlgorithmUpdateView(generics.UpdateAPIView):
 class CameraAlgorithmCreateView(generics.CreateAPIView):
     queryset = CameraAlgorithm.objects.all()
     serializer_class = CameraAlgorithmSerializer
+
+    def perform_create(self, serializer):
+        serializer.save()
