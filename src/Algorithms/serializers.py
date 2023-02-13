17e3d235234d21a5814e1958c32df32657c247dc
nnnLik <makhmudov_ra@vk.com>
2023-02-13 19:32:25 +0300

    [ADD] serializer for model camera algorithms

--- src/Algorithms/serializers.py
+++ src/Algorithms/serializers.py
@@ -3,14 +3,33 @@ from rest_framework import serializers
 from src.Algorithms.models import CameraAlgorithm
 from .models import Algorithm
 
+from django.core.exceptions import ValidationError
+
 
 class AlgorithmUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Algorithm
-        fields = ["id", "is_available", "cameras"]
+        fields = [
+            "id",
+            "is_available",
+        ]
 
 
 class CameraAlgorithmSerializer(serializers.ModelSerializer):
+    algorithm = serializers.CharField(write_only=True)
+
     class Meta:
         model = CameraAlgorithm
-        fields = ["id", "algorithm", "camera_id"]
+        fields = ("algorithm", "camera_id")
+
+    def create(self, validated_data):
+        algorithm_name = validated_data.pop("algorithm")
+        algorithm = Algorithm.objects.get(name=algorithm_name)
+        if algorithm:
+            validated_data["algorithm"] = algorithm
+            camera_algorithm = CameraAlgorithm.objects.create(**validated_data)
+            return camera_algorithm
+        else:
+            raise ValidationError(
+                {"error": f"Algorithm with name {algorithm_name} does not exists"}
+            )
