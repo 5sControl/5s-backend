fbbda3fae9b6491674e2491e7708c22db717572d
nnnLik <makhmudov_ra@vk.com>
2023-02-13 20:19:23 +0300

    [ADD] create method for CameraAlgorithmCreateView

--- src/Algorithms/views.py
+++ src/Algorithms/views.py
@@ -4,6 +4,10 @@ from rest_framework.response import Response
 from .models import Algorithm, CameraAlgorithm
 from .serializers import CameraAlgorithmSerializer, AlgorithmUpdateSerializer
 
+from src.StaffControl.Locations.models import Camera
+
+from django.core.exceptions import ValidationError
+
 
 class AlgorithmUpdateView(generics.UpdateAPIView):
     queryset = Algorithm.objects.all()
@@ -20,8 +24,31 @@ class AlgorithmUpdateView(generics.UpdateAPIView):
 
 
 class CameraAlgorithmCreateView(generics.CreateAPIView):
-    queryset = CameraAlgorithm.objects.all()
     serializer_class = CameraAlgorithmSerializer
 
-    def perform_create(self, serializer):
-        serializer.save()
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        for algorithm_name, camera_ips in data.items():
+            algorithm = Algorithm.objects.filter(name=algorithm_name).first()
+            if not algorithm:
+                raise ValidationError(
+                    {"error": f"Algorithm with name {algorithm_name} does not exists"}
+                )
+
+            cameras = Camera.objects.filter(id__in=camera_ips)
+            if not cameras.exists():
+                missing_ips = set(camera_ips) - set(
+                    cameras.values_list("id", flat=True)
+                )
+                raise ValidationError(
+                    {"error": f"Cameras with ip {', '.join(missing_ips)} do not exist"}
+                )
+
+            CameraAlgorithm.objects.bulk_create(
+                [
+                    CameraAlgorithm(algorithm=algorithm, camera_id=camera)
+                    for camera in cameras
+                ]
+            )
+
+        return Response({"message": "Camera Algorithm records created successfully"})
