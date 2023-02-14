bb11683c23264572c1ed1d7f7acf829f385522a8
nnnLik <makhmudov_ra@vk.com>
2023-02-14 13:12:21 +0300

    [ADD] fill out algorithm table

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+
+# create tables 
 python manage.py makemigrations
 python manage.py migrate
+
+# fill algorithm table
+python manage.py algorithm
+
+# run server
 python manage.py runserver 0.0.0.0:8000
+
 # yes Y | python manage.py makemigrations &&
 # python manage.py migrate &&
 
