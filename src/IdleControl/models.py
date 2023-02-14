cd96326ea3095b32c0496c1bb035c3b01707427f
anikeenko <dimskay1988@icloud.com>
2023-02-14 21:29:44 +0300

    EDIT models

--- src/IdleControl/models.py
+++ src/IdleControl/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 
 class Actions(models.Model):
-    camera = models.CharField(null=True, blank=True, max_length=50)
-    start_tracking = models.DateTimeField(null=True, blank=True, max_length=50)
-    stop_tracking = models.DateTimeField(null=True, blank=True, max_length=50)
+    camera = models.CharField(null=False, blank=False, max_length=50, default="now ip")
+    start_tracking = models.DateTimeField(null=False, blank=False, max_length=50, default=None)
+    stop_tracking = models.DateTimeField(null=False, blank=False, max_length=50, default=None)
 
     def __str__(self):
         return f"{self.camera}"
