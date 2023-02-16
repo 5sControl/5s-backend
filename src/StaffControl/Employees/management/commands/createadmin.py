6b1515be18350c981fd10ceb3f962b5948b31b0c
nnnLik <makhmudov_ra@vk.com>
2023-02-16 20:22:33 +0300

    [ADD] create super user when dokcer started

---
+++ src/StaffControl/Employees/management/commands/createadmin.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand
+
+from django.contrib.auth.models import User
+
+
+class Command(BaseCommand):
+    help = "Create a superuser"
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser("admin", "admin@example.com", "password")
+            self.stdout.write(
+                self.style.SUCCESS(
+                    f"Superuser created successfully. Username: admin Password: admin"
+                )
+            )
+        else:
+            self.stdout.write(self.style.WARNING("Superuser already exists"))
