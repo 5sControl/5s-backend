50a4016881559284220c535153aa2245187a2202
nnnLik <makhmudov_ra@vk.com>
2023-02-17 19:40:29 +0300

    [EDIT] get cameras by api view

--- src/StaffControl/Locations/urls.py
+++ src/StaffControl/Locations/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from src.StaffControl.Locations.views import (
-    CameraViewSet,
+    GetCameraAPIView,
     GateViewSet,
     LocationViewSet,
     PostCameraAPIView,
@@ -11,12 +11,14 @@ from django.urls import path, include
 
 router = DefaultRouter()
 
-router.register(r"camera", CameraViewSet, basename="Camera")
 router.register(r"gate", GateViewSet, basename="gates")
 router.register(r"location", LocationViewSet, basename="locations")
 
 urlpatterns = [
     path("post_camera/", PostCameraAPIView.as_view()),
+]
+urlpatterns += [
+    path("camera/", GetCameraAPIView.as_view()),
     path("get_camera_http_links/", GetHttpCamerasLinkAPIView.as_view()),
     path("get_camera_rtsp_by_ip_links/", GetRtspCamerasLinkByIpAPIView.as_view()),
 ]
