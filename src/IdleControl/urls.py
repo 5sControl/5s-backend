971af54faf7e21420e7f941ecb90eb8fe5769ba0
anikeenko <dimskay1988@icloud.com>
2023-02-17 11:41:07 +0300

    EDIT urls added urls sorted date

--- src/IdleControl/urls.py
+++ src/IdleControl/urls.py
@@ -1,6 +1,6 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from src.IdleControl.views import ActionViewSet, ActionsWithPhotos
+from src.IdleControl.views import ActionViewSet, ActionsWithPhotos, IdleActionListView
 
 
 router_idle = DefaultRouter()
@@ -9,5 +9,6 @@ router_idle.register(r"action", ActionViewSet, basename="actions")
 
 urlpatterns = [
     path('action-with-photos/', ActionsWithPhotos.as_view()),
+    path('idle_actions/<str:date>/', IdleActionListView.as_view(), name='machine_action_list'),
     path('', include(router_idle.urls)),
 ]
