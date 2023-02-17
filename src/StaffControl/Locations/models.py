3ae599487ee077bb1caaac1f6ba425e84ab8c805
nnnLik <makhmudov_ra@vk.com>
2023-02-17 19:51:37 +0300

    [ADD] validation for id/ip

--- src/StaffControl/Locations/models.py
+++ src/StaffControl/Locations/models.py
@@ -1,10 +1,20 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Camera(models.Model):
     """Model for saving camera information"""
 
-    id = models.CharField(primary_key=True, max_length=30)
+    id = models.CharField(
+        primary_key=True,
+        max_length=30,
+        validators=[
+            RegexValidator(
+                regex=r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",
+                message="ID must be a valid IP address",
+            )
+        ],
+    )
     name = models.CharField(max_length=100, blank=True, null=True)
 
     username = models.CharField(max_length=100, default="admin", blank=True, null=True)
