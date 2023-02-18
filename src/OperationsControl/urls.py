99ee0cd8124b43a96ed127c1d7295a2739a4371c
anikeenko <dimskay1988@icloud.com>
2023-02-18 18:23:32 +0300

    ADDED serializer, urls

---
+++ src/OperationsControl/urls.py
@@ -0,0 +1,14 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path
+from src.OperationsControl.views import OperationsControlViewSet, OperationsListView
+
+
+router_operations = DefaultRouter()
+
+router_operations.register(r"all_operations", OperationsControlViewSet, basename="all_operations")
+
+urlpatterns = [
+    path('<str:date>/', OperationsListView.as_view(), name='counter_operations_list'),
+]
+
+urlpatterns += router_operations.urls
