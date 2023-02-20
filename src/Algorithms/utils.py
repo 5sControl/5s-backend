bdc146e94d5b8d85b9ceac72ac118b16cb7e40dc
nnnLik <makhmudov_ra@vk.com>
2023-02-20 13:05:58 +0300

    [FIX] logs

--- src/Algorithms/utils.py
+++ src/Algorithms/utils.py
@@ -18,21 +18,17 @@ class StartYoloProccesing:
             )
             response_json = response.json()
         except requests.exceptions.RequestException as e:
-            self.errors.append(f"Error sending request: {e}")
-            return {"errors": [f"Error sending request: {e}"]}
+            return {"message": [f"Error sending request: {e}"]}
         except ValueError as e:
-            self.errors.append(f"Error decoding response: {e}")
-            return {"errors": [f"Error decoding response: {e}"]}
+            return {"message": [f"Error decoding response: {e}"]}
         try:
             if response_json.get("status").lower() != "success":
-                self.errors.append(f"Received non-success response: {response_json}")
-                return {"errors": [f"Received non-success response: {response_json}"]}
+                return {"message": [f"Received non-success response: {response_json}"]}
             elif "pid" not in response_json:
-                self.errors.append(f"Missing PID in response: {response_json}")
-                return {"errors": [f"Missing PID in response: {response_json}"]}
+                return {"message": [f"Missing PID in response: {response_json}"]}
         except AttributeError:
             return {
-                "errors": "The process was not set in motion. No response from Yolo"
+                "message": "The process was not set in motion. No response from Yolo"
             }
 
         else:
