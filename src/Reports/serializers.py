c7d2bf990590b652c2c412c117a8bd5e0f73e5a4
anikeenko <dimskay1988@icloud.com>
2023-02-21 14:59:12 +0300

    EDIT serializers

--- src/Reports/serializers.py
+++ src/Reports/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from src.Reports.models import Report
 from src.ImageReport.serializers import PhotoSerializers
+from src.Algorithms.serializers import AlgorithmSerializer
+from src.Cameras.serializers import CameraSerializer
 
 
 class ReportSerializers(serializers.ModelSerializer):
     """All photos on Reports"""
 
     photos = PhotoSerializers(many=True)
+    algorithm = AlgorithmSerializer(many=False)
+    camera = CameraSerializer(many=False)
 
     class Meta:
         model = Report
