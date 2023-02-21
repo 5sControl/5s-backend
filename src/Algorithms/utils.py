a2c5793bdd43e0ad628ea60d246631d00b7eb3be
nnnLik <makhmudov_ra@vk.com>
2023-02-22 00:17:40 +0300

    [FIX] response, handling errors with logs

--- src/Algorithms/utils.py
+++ src/Algorithms/utils.py
@@ -1,12 +1,14 @@
 import requests
 
+from src.Algorithms.models import Algorithm
+from src.Cameras.models import Camera
+
 from src.Cameras.service import link_generator
 
 
 class StartYoloProccesing:
-    def start_yolo_processing(self, camera, algorithm, url: str):
+    def start_yolo_processing(self, camera: Camera, algorithm: Algorithm, url: str):
         rtsp_camera_url = link_generator.get_camera_rtsp_link_by_camera(camera)
-        request_server_url = f"{url}:3020/run"
         response = {
             "camera_url": rtsp_camera_url["camera_url"],
             "algorithm": algorithm.name,
@@ -14,25 +16,26 @@ class StartYoloProccesing:
         }
         try:
             response = requests.post(
-                url=request_server_url,  # Send process data to YOLOv7 server
+                url=f"{url}:3020/run",  # Send process data to YOLO server
                 json=response,
             )
-            response_json = response.json()
-            response_json["server_url"] = request_server_url
         except requests.exceptions.RequestException as e:
             return {"status": False, "message": [f"Error sending request: {e}"]}
         except ValueError as e:
             return {"status": False, "message": [f"Error decoding response: {e}"]}
+        else:
+            response_json = response.json()
+            response_json["server_url"] = url
         try:
             if response_json.get("status").lower() != "success":
                 return {
                     "status": False,
-                    "message": [f"Received non-success response: {response_json}"],
+                    "message": f"Received non-success response: {response_json}",
                 }
             elif "pid" not in response_json:
                 return {
                     "status": False,
-                    "message": [f"Missing PID in response: {response_json}"],
+                    "message": f"Missing PID in response: {response_json}",
                 }
         except AttributeError:
             return {
@@ -41,6 +44,7 @@ class StartYoloProccesing:
             }
 
         else:
+            response_json["status"] = True
             return response_json
 
 
