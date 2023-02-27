750ee73d858aae000d14ad185e7bb338eabfa8e2
nnnLik <makhmudov_ra@vk.com>
2023-02-27 12:25:41 +0300

    [ADD] check id pid exist, yolo url, stop process

--- src/Algorithms/utils.py
+++ src/Algorithms/utils.py
@@ -1,12 +1,13 @@
 import requests
 
-from src.Algorithms.models import Algorithm
-from src.Cameras.models import Camera
-
 from src.Cameras.service import link_generator
 
+from src.Algorithms.models import Algorithm, CameraAlgorithm
+from src.Cameras.models import Camera
 
-class StartYoloProccesing:
+
+
+class YoloProccesing:
 	def start_yolo_processing(self, camera: Camera, algorithm: Algorithm, url: str) -> dict:
 		rtsp_camera_url = link_generator.get_camera_rtsp_link_by_camera(camera)
 		response = {
@@ -14,16 +15,46 @@ class StartYoloProccesing:
 			"algorithm": algorithm.name,
 			"server_url": url,
 		}
-		response = requests.post(
+		request = requests.post(
 			url=f"{url}:3020/run",
 			json=response,
 		)
-		response_json = response.json()
-		response_json["server_url"] = url
-		response_json["status"] = True
+		request_json = request.json()
+		request_json["server_url"] = url
+		request_json["status"] = True
 		
+		return request_json
+	
+	def stop_process(self, pid: int):
+		is_pid_exists = self.is_pid_exists(pid)
+		if not is_pid_exists:
+			return {
+				"status": False,
+				"message": "PID not found"
+			}
+		
+		yolo_server_url = self.get_yolo_url(pid)
+
+		request = requests.post(
+			url=f"{yolo_server_url}:3020/stop",
+			json={"pid": pid},
+		)
+		response_json = request.json()
+
 		return response_json
+	
+	def is_pid_exists(self, pid: int):
+		camera_algorithm = CameraAlgorithm.objects.filter(process_id=pid).first()
+		if not camera_algorithm:
+			return False
+		else:
+			return True
+		
+	def get_yolo_url(self, pid=None):
+		if pid:
+			url = CameraAlgorithm.objects.filter(process_id=pid).first()
+		
+		return url.yolo_url
 
 
-
-yolo_proccesing = StartYoloProccesing()
+yolo_proccesing = YoloProccesing()
