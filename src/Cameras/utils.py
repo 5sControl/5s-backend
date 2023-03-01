e92f68cbe28a2ba9ca71979728c07bdde88d569e
nnnLik <makhmudov_ra@vk.com>
2023-03-01 18:53:13 +0300

    [ADD] save zip with algorithm images

--- src/Cameras/utils.py
+++ src/Cameras/utils.py
@@ -2,6 +2,7 @@ from src.Reports.models import Report
 from src.ImageReport.models import Image
 from src.Algorithms.service import algorithms_services
 
+from datetime import datetime
 import zipfile
 import os
 
@@ -9,7 +10,8 @@ import os
 class MakeZip:
     def __init__(self):
         self.image_report_path = []
-
+        self.images_folder = "images/"
+        
     def get_image_path(self, algorithm: str):
         algorithm_obj = algorithms_services.get_algorithm_by_name(algorithm)
 
@@ -21,26 +23,23 @@ class MakeZip:
         
         reports = Report.objects.filter(algorithm=algorithm_obj)
         for report in reports:
-            try:
-                image_report_objs = Image.objects.filter(report_id=report)
-            except Image.DoesNotExist:
-                continue
-            else:
-                for image_report_obj in image_report_objs:
-                    self.image_report_path.append(image_report_obj.image)
+            image_report_objs = Image.objects.filter(report_id=report)
+            for image_report_obj in image_report_objs:
+                self.image_report_path.append(image_report_obj.image)
 
     def create_zip(self, algorithm: str):
         self.get_image_path(algorithm)
-        zip_filename = f"{algorithm}_data.zip"
+        zip_filename = f"{algorithm}_data_{datetime.now().date()}.zip"
+        zip_path = os.path.join(self.images_folder, zip_filename)
 
-        with zipfile.ZipFile(zip_filename, "w") as zip_file:
-            for path in self.image_report_path:
-                zip_path = os.path.join(algorithm, path)
-                zip_file.write(path, f"images/{zip_path}")
-    
+        with zipfile.ZipFile(zip_path, "w") as zip_file:
+            for image_path in self.image_report_path:
+                zip_path = os.path.join("images", image_path)
+                zip_file.write(image_path, zip_path)
+        
         return {
             "status": True,
-            "message": f"Data from {algorithm} was successfully compressed to {zip_filename}"
+            "message": f"Zip file created at {zip_path}"
         }
 
 
