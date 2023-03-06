43b60ddc60579457184ae1166c10a797cca80f56
nnnLik <makhmudov_ra@vk.com>
2023-03-06 15:17:50 +0300

    [FIX] conn

--- src/Order/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class MyModel(models.Model):
-    field1 = models.CharField(max_length=50)
-    field2 = models.IntegerField()
-    
-    class Meta:
-        using = 'ms_sql'
