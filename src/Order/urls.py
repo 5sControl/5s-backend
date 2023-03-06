839a6c32c8f4a006aebac935eb8b7fbb5a8c41c4
nnnLik <makhmudov_ra@vk.com>
2023-03-06 11:58:55 +0300

    [ADD] init order route

---
+++ src/Order/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import (
+    GetOrderApiView,
+    DatabaseConnectionApiView,
+)
+
+urlpatterns = [
+    path("", GetOrderApiView.as_view(), name="get-order"),
+    path("connect", DatabaseConnectionApiView.as_view(), name="db-connection"),
+]
