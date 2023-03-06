41378ccce4abcbd916306bbcfdc3de12e7350801
nnnLik <makhmudov_ra@vk.com>
2023-03-06 16:48:24 +0300

    [ADD] database connection class

--- src/Order/database_conn.py
+++ src/Order/database_conn.py
@@ -1,26 +1,53 @@
 import pymssql
 
+from src.Order.models import DatabaseConnection
+
 
 class DBConn:
+    def save_conn_data(self, data):
+        db_name = data["db_name"]
+        db_user = data["db_user"]
+        db_password = data["db_password"]
+        db_host = data["db_host"]
 
-    def get_connection(self, data):
+        try:
+            if not self.check_connection(db_name, db_user, db_password, db_host):
+                raise Exception("Could not connect to database")
 
-        db_name = data['db_name']
-        db_user = data['db_user']
-        db_password = data['db_password']
-        db_host = data['db_host']
-        db_port = data['db_port']
+            newDatabaseConnection = DatabaseConnection(
+                db_name=db_name,
+                db_user=db_user,
+                db_password=db_password,
+                db_host=db_host
+            )
+            newDatabaseConnection.save()
+        except Exception:
+            return {
+                "status": False,
+                "message": "Database connection doest created successfully",
+            }
+        return {
+            "status": True,
+            "message": "Database connection created successfully",
+        }
+    
+    def check_connection(self, db_name, db_user, db_password, db_host):
+        try:
+            conn = pymssql.connect(
+                server=db_host, database=db_name, user=db_user, password=db_password
+            )
+            print("Connection: ", conn)
+            cursor = conn.cursor()
+        except Exception:
+            return False
+        return True
 
-        conn = pymssql.connect(server=db_host, database=db_name, user=db_user, password=db_password)
-        cursor = conn.cursor()
-
-        # execute query to retrieve one record from specified table
-        query = f"SELECT TOP 1 * FROM Skany"
-        cursor.execute(query)
-        record = cursor.fetchone()
-
-        # close connection and return record
-        conn.close()
-        return record
+    def get_cursor(self,):
+        cred = DatabaseConnection.objects.first()
+        conn = pymssql.connect(
+                server=cred.db_host, database=cred.db_name, user=cred.db_user, password=cred.db_password
+            )
+        return conn
+        
 
 db_conn = DBConn()
