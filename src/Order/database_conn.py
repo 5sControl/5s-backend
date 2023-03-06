702e9eedda98c2ab36789837f7cd52b3353f58cb
nnnLik <makhmudov_ra@vk.com>
2023-03-06 16:00:20 +0300

    [ADD] return record

--- src/Order/database_conn.py
+++ src/Order/database_conn.py
@@ -1,36 +1,26 @@
-from django.db import connections
-from decouple import config
-def create_database_connection(data):
-    print(config("SQL_ENGINE", ""))
+import pymssql
 
-    newDatabase = {}
 
-    db_name = data['db_name']
-    db_user = data['db_user']
-    db_password = data['db_password']
-    db_host = data['db_host']
-    db_port = data['db_port']
+class DBConn:
 
-    try:
-        database_id = "ms_sql"
-        newDatabase["id"] = database_id
-        newDatabase['ENGINE'] = 'sql_server.pyodbc',
-        newDatabase['NAME'] = db_name
-        newDatabase['USER'] = db_user
-        newDatabase['PASSWORD'] = db_password
-        newDatabase['HOST'] = db_host
-        newDatabase['PORT'] = db_port
-        newDatabase['OPTIONS'] = {
-                    'driver': 'ODBC Driver 17 for SQL Server',
-        }
-        connections.databases[database_id] = newDatabase
-    except Exception:
-        return {
-            "status": False,
-            "message": "Cannot connect to new database"
-        }
-    else:
-        return {
-            "status": True,
-            "message": "The connection was installed successfully"
-        }
+    def get_connection(self, data):
+
+        db_name = data['db_name']
+        db_user = data['db_user']
+        db_password = data['db_password']
+        db_host = data['db_host']
+        db_port = data['db_port']
+
+        conn = pymssql.connect(server=db_host, database=db_name, user=db_user, password=db_password)
+        cursor = conn.cursor()
+
+        # execute query to retrieve one record from specified table
+        query = f"SELECT TOP 1 * FROM Skany"
+        cursor.execute(query)
+        record = cursor.fetchone()
+
+        # close connection and return record
+        conn.close()
+        return record
+
+db_conn = DBConn()
