7c959370f6bae45b9ded109a50b47fb4e3a84c55
nnnLik <makhmudov_ra@vk.com>
2023-03-06 16:49:15 +0300

    [ADD] skany data service

--- src/Order/services.py
+++ src/Order/services.py
@@ -1,6 +1,16 @@
+from src.Order.database_conn import db_conn
+
 class OrderService:
-    
-    def get_order_data(self):
-        ...
+    def get_skany_data(self):
+
+        conn = db_conn.get_cursor()
+        cursor = conn.cursor()
+        query = f"SELECT TOP 1 * FROM Skany"
+        cursor.execute(query)
+        record = cursor.fetchone()
+
+        conn.close()
+        return record
+
 
 order_service = OrderService()
