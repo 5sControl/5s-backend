a9e54dccc3a36495c13f654c2b25dc806da92207
nnnLik <makhmudov_ra@vk.com>
2023-03-07 18:25:23 +0300

    [FIX] return response

--- src/OrderView/views.py
+++ src/OrderView/views.py
@@ -16,8 +16,3 @@ class GetAllDataListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         return order_service.get_data()
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data, status=200)
