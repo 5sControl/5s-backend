2caba6e4861e9598ddd024d1dda9c72d57e93b5a
nnnLik <makhmudov_ra@vk.com>
2023-03-07 16:54:26 +0300

    [ADD] get data

--- src/OrderView/services.py
+++ src/OrderView/services.py
@@ -1,8 +1,27 @@
 from src.OrderView.models import Skany
+from src.OrderView.models import Zlecenia, SkanyVsZlecenia, Skany
+
 
 class OrderService:
     def get_data(self):
-        Skany.objects.using('mssql').filter(indeks=1227966)
+        zlecenia = Zlecenia.objects.using("mssql").all()
+        results = []
+        for zlecenie in zlecenia:
+            skany_zlecenia = (
+                SkanyVsZlecenia.objects.using("mssql")
+                .filter(indekszlecenia=zlecenie.indeks)
+                .first()
+            )
+            if not skany_zlecenia:
+                result = {"zlecenie": zlecenie, "skany": None}
+                continue
+            skany = (
+                Skany.objects.using("mssql")
+                .filter(indeks=skany_zlecenia.indeksskanu)
+                .first()
+            )
+            result = {"zlecenie": zlecenie, "skany": skany}
+            results.append(result)
 
 
 order_service = OrderService()
