d84fed471f9bea375cddf23ea01f02b1014fc153
nnnLik <makhmudov_ra@vk.com>
2023-03-07 14:02:13 +0300

    [EDIT] deleted database connection from pymssql

--- src/Order/serializer.py
+++ src/Order/serializer.py
@@ -1,9 +1,1 @@
-# from rest_framework import serializers
-
-
-# class DatabaseConnectionSerializer(serializers.Serializer):
-#     db_name = serializers.CharField(required=True, max_length=255)
-#     db_user = serializers.CharField(required=True, max_length=255)
-#     db_password = serializers.CharField(required=True, max_length=255)
-#     db_port = serializers.CharField(required=True, max_length=255)
-#     db_host = serializers.CharField(required=True, max_length=255)
+from rest_framework import serializers
--- src/Order/services.py
+++ src/Order/services.py
@@ -1,16 +1,6 @@
-# from src.Order.database_conn import db_conn
+class OrderService:
+    def get_skany_data(self):
+        ...
 
 
-# class OrderService:
-#     def get_skany_data(self):
-#         conn = db_conn.get_cursor()
-#         cursor = conn.cursor()
-#         query = f"SELECT TOP 1 * FROM Skany"
-#         cursor.execute(query)
-#         record = cursor.fetchone()
-
-#         conn.close()
-#         return record
-
-
-# order_service = OrderService()
+order_service = OrderService()
