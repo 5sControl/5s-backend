a5799a38cbb652849d898c9f8d7c28a00b570a41
nnnLik <makhmudov_ra@vk.com>
2023-03-07 16:54:13 +0300

    [EDIT] pep8

--- src/OrderView/serializers.py
+++ src/OrderView/serializers.py
@@ -1,14 +1,16 @@
 from rest_framework import serializers
 from .models import Zlecenia, Skany
 
+
 class SkanySerializer(serializers.ModelSerializer):
     class Meta:
         model = Skany
-        fields = '__all__'
+        fields = "__all__"
+
 
 class ZleceniaSerializer(serializers.ModelSerializer):
     skany = SkanySerializer(many=True, read_only=True)
 
     class Meta:
         model = Zlecenia
-        fields = '__all__'
+        fields = "__all__"
