4107cc1abc141b3dd0171efcc8538fdd469f8bbb
nnnLik <makhmudov_ra@vk.com>
2023-03-09 11:37:55 +0300

    [ADD] use sql joins

--- src/OrderView/views.py
+++ src/OrderView/views.py
@@ -9,6 +9,7 @@ from src.OrderView.models import Zlecenia, SkanyVsZlecenia
 from src.OrderView.services import order_service
 
 from django.db.models import F
+from django.db import connection
 
 
 class GetAllDataAPIView(generics.ListAPIView):
@@ -23,6 +24,39 @@ class ZleceniaListView(generics.ListAPIView):
     serializer_class = ZleceniaTestSerializer
 
     def get_queryset(self):
-        queryset = SkanyVsZlecenia.objects.filter(skanyvszlecenia__isnull=False).distinct()
-        queryset = queryset.annotate(raport=F('skany__stanowisko__raport'))
+        with connection.cursor() as cursor:
+            cursor.execute("""
+                SELECT DISTINCT Z.*, S.Raport
+                FROM Zlecenia Z
+                JOIN SkanyVsZlecenia SZ ON Z.indeks = SZ.indekszlecenia
+                JOIN Skany S ON SZ.indeksskanu = S.indeks
+                WHERE NOT EXISTS (
+                    SELECT *
+                    FROM SkanyVsZlecenia SZ2
+                    WHERE SZ2.indekszlecenia = Z.indeks
+                    AND SZ2.indeksskanu NOT IN (
+                        SELECT S2.indeks
+                        FROM Skany S2
+                        WHERE S2.Stanowisko IN (
+                            SELECT Stanowisko
+                            FROM Skany
+                            WHERE indeks = S.indeks
+                        ) AND S2.indeks = SZ2.indeksskanu
+                    )
+                )
+                AND S.Stanowisko IN (
+                    SELECT Stanowisko
+                    FROM Skany
+                    WHERE indeks = S.indeks
+                ) AND S.Raport IS NOT NULL
+            """)
+
+            rows = cursor.fetchall()
+            queryset = []
+            for row in rows:
+                zlecenie = Zlecenia(*row[:-1])
+                zlecenie.raport = row[-1]
+                queryset.append(zlecenie)
+
         return queryset
+    
