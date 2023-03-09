30a9aae9ac4e3ff2503dff33c83d3781c35fd01b
nnnLik <makhmudov_ra@vk.com>
2023-03-09 20:39:40 +0300

    [EDIT] commented

--- src/StaffControl/Employees/urls.py
+++ src/StaffControl/Employees/urls.py
@@ -6,12 +6,12 @@ from .views import EmployeeViewSet, PeopleViewSet, UsersViewSet, CreateUserView
 
 router = DefaultRouter()
 
-router.register(r"admin", UsersViewSet, basename="users")
-router.register(r"employ", EmployeeViewSet, basename="employs")
-router.register(r"count-of-people", PeopleViewSet, basename="all people in locations")
+# router.register(r"admin", UsersViewSet, basename="users")
+# router.register(r"employ", EmployeeViewSet, basename="employs")
+# router.register(r"count-of-people", PeopleViewSet, basename="all people in locations")
 
 urlpatterns = [
-    path("create/user/", CreateUserView.as_view(), name="create admin/worker"),
+    # path("create/user/", CreateUserView.as_view(), name="create admin/worker"),
 ]
 
 
