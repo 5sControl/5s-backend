55beaa7c66a4e6a2f8b78de25d7e156a88b1be5c
nnnLik <makhmudov_ra@vk.com>
2023-03-09 20:41:08 +0300

    [EDIT] commenterd imports

--- src/StaffControl/Employees/urls.py
+++ src/StaffControl/Employees/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from rest_framework.routers import DefaultRouter
 
-from .views import EmployeeViewSet, PeopleViewSet, UsersViewSet, CreateUserView
+# from .views import EmployeeViewSet, PeopleViewSet, UsersViewSet, CreateUserView
 
 router = DefaultRouter()
 
