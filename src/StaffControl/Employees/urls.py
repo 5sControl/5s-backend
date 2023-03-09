a50f898491a3c8c7b751238855b2a757ea181ec2
nnnLik <makhmudov_ra@vk.com>
2023-03-09 20:44:55 +0300

    [ADD] dlib

--- src/StaffControl/Employees/urls.py
+++ src/StaffControl/Employees/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 
 from rest_framework.routers import DefaultRouter
 
-# from .views import EmployeeViewSet, PeopleViewSet, UsersViewSet, CreateUserView
+from .views import EmployeeViewSet, PeopleViewSet, UsersViewSet, CreateUserView
 
 router = DefaultRouter()
 
-# router.register(r"admin", UsersViewSet, basename="users")
-# router.register(r"employ", EmployeeViewSet, basename="employs")
-# router.register(r"count-of-people", PeopleViewSet, basename="all people in locations")
+router.register(r"admin", UsersViewSet, basename="users")
+router.register(r"employ", EmployeeViewSet, basename="employs")
+router.register(r"count-of-people", PeopleViewSet, basename="all people in locations")
 
 urlpatterns = [
-    # path("create/user/", CreateUserView.as_view(), name="create admin/worker"),
+    path("create/user/", CreateUserView.as_view(), name="create admin/worker"),
 ]
 
 
