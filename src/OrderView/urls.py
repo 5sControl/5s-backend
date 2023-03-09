2104cb2114cd2662f6c0ddcb8341e39d53699f3c
nnnLik <makhmudov_ra@vk.com>
2023-03-09 21:42:18 +0300

    [FIX]route path

--- src/OrderView/urls.py
+++ src/OrderView/urls.py
@@ -3,6 +3,6 @@ from .views import GetAllDataAPIView, GetAllOrdersAPIView, GetOrderDataByIdAPIVi
 
 urlpatterns = [
     path("", GetAllDataAPIView.as_view(), name="get_all_orders_with_data"),
-    path('<str:zlecenie_id>/', GetOrderDataByIdAPIView.as_view(), name='get_orders_by_id'),
+    path('by/<str:zlecenie_id>/', GetOrderDataByIdAPIView.as_view(), name='get_orders_by_id'),
     path("all-orders/", GetAllOrdersAPIView.as_view(), name="get_all_orders"),
 ]
