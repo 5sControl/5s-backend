ef971d2ec747026c6f40b53ae1717a6a333b83f0
nnnLik <makhmudov_ra@vk.com>
2023-03-10 13:24:45 +0300

    [FIX] ordername

--- src/OrderView/services.py
+++ src/OrderView/services.py
@@ -1,6 +1,7 @@
 from src.OrderView.models import Stanowiska, Zlecenia, SkanyVsZlecenia, Skany
 
 from django.forms.models import model_to_dict
+from django.db.models import Value, CharField
 
 
 class OrderService:
@@ -16,11 +17,8 @@ class OrderService:
         zleceniaQuery = orderView_service.get_zleceniaQuery()
 
         response_list = []
-        counter = 0  # FIXME
 
         for zlecenie in zleceniaQuery:
-            counter += 1  # FIXME
-
             skanyVsZleceniaQuery = SkanyVsZlecenia.objects.using("mssql").filter(
                 indekszlecenia=zlecenie.indeks
             )
@@ -39,13 +37,20 @@ class OrderService:
 
             zlecenie_data = model_to_dict(zlecenie)
             zlecenie_data["skans"] = skany_list
-            zlecenie_data["orderName"] = f"Order Name {couter}"  # FIXME
+            zlecenie_data["orderName"] = "Order Name"  # FIXME
             response_list.append(zlecenie_data)
 
         return response_list
 
     def getAllOrders(self):
-        return Zlecenia.objects.using("mssql").values_list("zlecenie", flat=True)
+        return (
+            Zlecenia.objects.using("mssql")
+            .annotate(orderName=Value("Order Name", output_field=CharField()))
+            .values_list("zlecenie", "orderName")
+        )
+        return Zlecenia.objects.using("mssql").values_list(
+            "zlecenie", flat=True
+        )  # FIXME: return it when will get order name
 
     def getOrderDataById(self, order_id):
         zleceniaQuery = orderView_service.get_zleceniaQueryById(order_id)
@@ -71,6 +76,7 @@ class OrderService:
 
             zlecenie_data = model_to_dict(zlecenie)
             zlecenie_data["skans"] = skany_list
+            zlecenie_data["orderName"] = "Order Name"
             response_list.append(zlecenie_data)
 
         return response_list
