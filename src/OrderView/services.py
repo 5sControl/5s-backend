d91ae366adfa5ea858cf42c5c17fdb45a2df3a42
nnnLik <makhmudov_ra@vk.com>
2023-03-10 13:10:27 +0300

    [ADD] orderName

--- src/OrderView/services.py
+++ src/OrderView/services.py
@@ -4,10 +4,11 @@ from django.forms.models import model_to_dict
 
 
 class OrderService:
-
-    def get_zleceniaQuery(self,):
+    def get_zleceniaQuery(
+        self,
+    ):
         return Zlecenia.objects.using("mssql").all()
-    
+
     def get_zleceniaQueryById(self, id):
         return Zlecenia.objects.using("mssql").filter(zlecenie=id)
 
@@ -15,8 +16,11 @@ class OrderService:
         zleceniaQuery = orderView_service.get_zleceniaQuery()
 
         response_list = []
+        couter = 0  # FIXME
 
         for zlecenie in zleceniaQuery:
+            counter += 1  # FIXME
+
             skanyVsZleceniaQuery = SkanyVsZlecenia.objects.using("mssql").filter(
                 indekszlecenia=zlecenie.indeks
             )
@@ -26,20 +30,23 @@ class OrderService:
                     indeks=skanyVsZlecenia.indeksskanu
                 )
                 for skany in skanyQuery:
-                    stanowisko = Stanowiska.objects.using("mssql").get(indeks=skany.stanowisko)
+                    stanowisko = Stanowiska.objects.using("mssql").get(
+                        indeks=skany.stanowisko
+                    )
                     skany_data = model_to_dict(skany)
                     skany_data["raport"] = stanowisko.raport
                     skany_list.append(skany_data)
 
             zlecenie_data = model_to_dict(zlecenie)
             zlecenie_data["skans"] = skany_list
+            zlecenie_data["orderName"] = f"Order Name {couter}"  # FIXME
             response_list.append(zlecenie_data)
 
         return response_list
-    
+
     def getAllOrders(self):
-        return Zlecenia.objects.using("mssql").values_list('zlecenie', flat=True)
-    
+        return Zlecenia.objects.using("mssql").values_list("zlecenie", flat=True)
+
     def getOrderDataById(self, order_id):
         zleceniaQuery = orderView_service.get_zleceniaQueryById(order_id)
 
@@ -55,7 +62,9 @@ class OrderService:
                     indeks=skanyVsZlecenia.indeksskanu
                 )
                 for skany in skanyQuery:
-                    stanowisko = Stanowiska.objects.using("mssql").get(indeks=skany.stanowisko)
+                    stanowisko = Stanowiska.objects.using("mssql").get(
+                        indeks=skany.stanowisko
+                    )
                     skany_data = model_to_dict(skany)
                     skany_data["raport"] = stanowisko.raport
                     skany_list.append(skany_data)
@@ -63,7 +72,7 @@ class OrderService:
             zlecenie_data = model_to_dict(zlecenie)
             zlecenie_data["skans"] = skany_list
             response_list.append(zlecenie_data)
-            
+
         return response_list
 
 
