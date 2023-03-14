20806ab2e472c76c11597dd819c8c5f9cf511e4e
anikeenko <dimskay1988@icloud.com>
2023-03-14 17:00:32 +0300

    EDIT decorators

--- src/CompanyLicense/decorators.py
+++ src/CompanyLicense/decorators.py
@@ -61,7 +61,7 @@ def check_active_algorithms(view_func):
         if not company.is_active:
             return HttpResponseBadRequest("Your license is inactive.")
 
-        active_algorithms_count = CameraAlgorithm.objects.values('algorithm').distinct().count()
+        active_algorithms_count = CameraAlgorithm.objects.values('algorithm_id').distinct().count()
         if active_algorithms_count >= company.neurons_active:
             return HttpResponseBadRequest(
                 "You have exceeded the limit of active algorithm."
