a7808c6cb94726264538cee68c4c66fef27ed5ea
nnnLik <makhmudov_ra@vk.com>
2023-03-14 12:38:37 +0300

    [FIX] filtred data

--- src/OrderView/views.py
+++ src/OrderView/views.py
@@ -10,7 +10,7 @@ class GetAllProductAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        response = orderView_service.get_all_ordersList()
+        response = orderView_service.get_filtered_orders_list()
         return Response(response, status=status.HTTP_200_OK)
 
 
