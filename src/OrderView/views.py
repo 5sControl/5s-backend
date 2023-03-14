a7b3b15c9b347fa85148ef316a1e12aebde45fc5
nnnLik <makhmudov_ra@vk.com>
2023-03-14 12:25:40 +0300

    [EDIT] remove searching all records

--- src/OrderView/views.py
+++ src/OrderView/views.py
@@ -6,21 +6,11 @@ from rest_framework.permissions import IsAuthenticated
 from src.OrderView.services import orderView_service
 
 
-class GetAllDataAPIView(APIView):
-    # If you have a lot of time u can run it
-
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request):
-        response = orderView_service.get_all()
-        return Response(response, status=status.HTTP_200_OK)
-
-
 class GetAllProductAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        response = orderView_service.get_allProduct()
+        response = orderView_service.get_all_ordersList()
         return Response(response, status=status.HTTP_200_OK)
 
 
