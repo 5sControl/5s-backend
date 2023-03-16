12afe8c40d2f43156b371f3cf1c50bb5ad97fbab
nnnLik <makhmudov_ra@vk.com>
2023-03-16 10:06:45 +0300

    [FIX] imports

--- src/OrderView/urls.py
+++ src/OrderView/urls.py
@@ -1,11 +1,24 @@
 from django.urls import path
-
-from .views import GetAllDataAPIView, GetAllOrdersAPIView, GetOrderDataByIdAPIView, TestApiView
+from .views import (
+    GetAllProductAPIView,
+    GetOrderDataByindexAPIView,
+    GetOrderDataByZlecenieAPIView,
+    GetOrderDataByZlecenieAPIView,
+    TestApiView,
+)
 
 urlpatterns = [
-    path("", GetAllDataAPIView.as_view(), name="get_all_orders_with_data"),
-    path('by/<str:zlecenie_id>/', GetOrderDataByIdAPIView.as_view(), name='get_orders_by_id'),
-    path("all-orders/", GetAllOrdersAPIView.as_view(), name="get_all_orders"),
+    path(
+        "by/<str:index>/",
+        GetOrderDataByindexAPIView.as_view(),
+        name="get_orders_by_id",
+    ),
+    path(
+        "by-order/<str:zlecenie>/",
+        GetOrderDataByZlecenieAPIView.as_view(),
+        name="get_orders_by_id",
+    ),
+    path("all-orders/", GetAllProductAPIView.as_view(), name="get_all_orders"),
     # test
     path("test/", TestApiView.as_view(), name="testapi")
 ]
