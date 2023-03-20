7e54d59f1d7d48d2fa2365c24cd8a8aaf471f833
nnnLik <makhmudov_ra@vk.com>
2023-03-20 13:29:10 +0300

    [ADD] view for ms sql cursor #96

--- src/OrderView/views.py
+++ src/OrderView/views.py
@@ -1,13 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 
-from src.OrderView.services import orderView_service
+from src.OrderView.services import orderView_service, ms_sql_service
 
 
 class GetAllProductAPIView(APIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get(self, request):
         response = orderView_service.get_filtered_orders_list()
@@ -20,3 +20,11 @@ class GetOrderDataByZlecenieAPIView(APIView):
     def get(self, request, zlecenie_id):
         response = orderView_service.get_order(zlecenie_id)
         return Response(response, status=status.HTTP_200_OK)
+
+
+class CreateConectionAPIView(generics.CreateAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        response = ms_sql_service.create_connection(request.data)
+        return Response(response, status=status.HTTP_201_CREATED)
