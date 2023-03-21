17f579ea6205d55fab461599226c0ba9fdac195b
anikeenko <dimskay1988@icloud.com>
2023-03-21 12:09:57 +0300

    ADD ItemsViewSet

--- src/Inventory/views.py
+++ src/Inventory/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
 
-# Create your views here.
+from models import Items
+
+from serializers import ItemsSerializer
+
+
+class ItemsViewSet(viewsets.ModelViewSet):
+    queryset = Items.objects.all().order_by("-id")
+    serializer_class = ItemsSerializer
+    # permission_classes = [IsAuthenticated]
