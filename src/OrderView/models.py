67d906b4c63d1a8f5dc994763d1022e64d4ec123
nnnLik <makhmudov_ra@vk.com>
2023-03-21 16:49:21 +0300

    [FIX] blank

--- src/OrderView/models.py
+++ src/OrderView/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class DatabaseConnection(models.Model):
-    database_type = models.CharField(max_length=50, default="OrderView", ablank=False, null=False)
+    database_type = models.CharField(max_length=50, default="OrderView", blank=False, null=False)
     server = models.CharField(max_length=20, blank=False, null=False)
     database = models.CharField(max_length=100, blank=False, null=False)
     username = models.CharField(max_length=50, blank=False, null=False)
