96aef58a6e4e925f0d6e6988e90357ff18586c6b
anikeenko <dimskay1988@icloud.com>
2023-03-21 11:40:10 +0300

    ADD models item

---
+++ src/Inventory/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+from src.Cameras.models import Camera
+from src.Reports.models import Report
+
+
+class StatusItemChoice(models.TextChoices):
+    in_stock = 'In stock'
+    low_stock_level = 'Low stock level'
+    out_of_stock = 'Out of stock'
+
+
+class Items(models.Model):
+    name = models.TextField(max_length=75, verbose_name="Item name")
+    status = models.CharField(max_length=20, choices=StatusItemChoice.choices, default=StatusItemChoice.out_of_stock)
+    current_stock_level = models.IntegerField(verbose_name="Current stock level")
+    low_stock_level = models.IntegerField(verbose_name="Low stock level")
+    email = models.EmailField(blank=True, null=True, verbose_name="Email to send notifications")
+    camera = models.ForeignKey(Camera, related_name='camera_id', on_delete=models.CASCADE)
+    report = models.ForeignKey(Report, related_name='report_id', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
