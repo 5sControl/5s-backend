5eacdeb4eb02866cca5d68424f578f7771927349
anikeenko <dimskay1988@icloud.com>
2023-03-21 16:31:00 +0300

    ADD models, migrations

--- src/Inventory/models.py
+++ src/Inventory/models.py
@@ -11,9 +11,11 @@ class StatusItemChoice(models.TextChoices):
 
 
 class Items(models.Model):
+    """Models items"""
+
     name = models.TextField(max_length=75, verbose_name="Item name")
     status = models.CharField(max_length=20, choices=StatusItemChoice.choices, default=StatusItemChoice.out_of_stock)
-    current_stock_level = models.IntegerField(verbose_name="Current stock level")
+    current_stock_level = models.IntegerField(verbose_name="Current stock level", blank=True, null=True)
     low_stock_level = models.IntegerField(verbose_name="Low stock level")
     email = models.EmailField(blank=True, null=True, verbose_name="Email to send notifications")
     camera = models.ForeignKey(Camera, related_name='camera_id', on_delete=models.CASCADE)
