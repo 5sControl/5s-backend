c901d3d7e22d73c5fdef856861e6999f58ed82f1
anikeenko <dimskay1988@icloud.com>
2023-03-21 14:22:31 +0300

    EDIT urls, views

--- src/Inventory/urls.py
+++ src/Inventory/urls.py
@@ -1,12 +1,14 @@
+from idlelib.multicall import r
+
 from rest_framework.routers import DefaultRouter
 from django.urls import path, include
-from src.Inventory.views import ItemsViewSet
-
+from src.Inventory.views import ItemsViewSet, ItemsHistoryViewSet
 
 router_inventory = DefaultRouter()
 
 router_inventory.register(r"items", ItemsViewSet, basename="items")
 
 urlpatterns = [
+    path("history/<str:camera_ip>/<str:date>/<str:start_time>/<str:end_time>/", ItemsHistoryViewSet.as_view()),
     path("", include(router_inventory.urls)),
 ]
--- src/Inventory/views.py
+++ src/Inventory/views.py
@@ -1,11 +1,25 @@
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
 from src.Inventory.models import Items
 
 from src.Inventory.serializers import ItemsSerializer
+from src.Reports.views import ReportListView
 
 
 class ItemsViewSet(ModelViewSet):
     queryset = Items.objects.all().order_by("-id")
     serializer_class = ItemsSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
+
+
+class ItemsHistoryViewSet(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, camera_ip, date, start_time, end_time):
+        report_view = ReportListView()
+        response = report_view.get(request, "min_max_control", camera_ip, date, start_time, end_time)
+        data = response.data
+        return Response(data)
