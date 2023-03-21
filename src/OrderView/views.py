0fec6fccab2f988b4e50ae899c5ec40e3297b790
nnnLik <makhmudov_ra@vk.com>
2023-03-21 10:48:53 +0300

    [EDIT] apiview instead generics

--- src/OrderView/views.py
+++ src/OrderView/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 
 from src.OrderView.services import orderView_service, ms_sql_service
@@ -22,9 +22,9 @@ class GetOrderDataByZlecenieAPIView(APIView):
         return Response(response, status=status.HTTP_200_OK)
 
 
-class CreateConectionAPIView(generics.CreateAPIView):
+class CreateConectionAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def create(self, request, *args, **kwargs):
+    def post(self, request, *args, **kwargs):
         response = ms_sql_service.create_connection(request.data)
         return Response(response, status=status.HTTP_201_CREATED)
