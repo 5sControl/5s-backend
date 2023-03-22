5e76b7a511ed63f5d833d82e4b098b79b5f9d999
nnnLik <makhmudov_ra@vk.com>
2023-03-22 11:39:58 +0300

    [ADD] username field

--- src/OrderView/serializers.py
+++ src/OrderView/serializers.py
@@ -6,4 +6,4 @@ from src.OrderView.models import DatabaseConnection
 class DatabaseConnectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = DatabaseConnection
-        fields = ["database_type", "server", "database"]
+        fields = ["database_type", "server", "database", "username"]
