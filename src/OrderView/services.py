6e16322dfca5bb0f3b433fd725967bafcecbca26
nnnLik <makhmudov_ra@vk.com>
2023-03-22 17:06:04 +0300

    [FIX] connection

--- src/OrderView/services.py
+++ src/OrderView/services.py
@@ -56,7 +56,7 @@ class OrderService:
             results = cursor.fetchall()
         result = self.transform_result(results)
         print("RESULT: ", result)
-        return result
+        return result, connection
 
     def get_filtered_orders_list(
         self,
@@ -103,22 +103,15 @@ class OrderService:
         response = {}
         status = "Completed"
 
-        zlecenia_dict = self.get_zlecenia_query_by_zlecenie(zlecenie_id)
-        if not zlecenia_dict:
-            return False
+        zlecenia_dict, connection = self.get_zlecenia_query_by_zlecenie(zlecenie_id)
 
-        connection = self._get_connection()
-        if not connection:
-            return False
-        
-        skany_dict = defaultdict(list)  # move initialization outside the for loop
-        
         for zlecenie_obj in zlecenia_dict:
             print("Zlecenie obj is ", zlecenie_obj)
-            
+            skany_dict = defaultdict(list)
             if zlecenie_obj["status"] == "Started":
                 status = "Started"
-            
+
+            print("Zlecenie connection is ", connection)
             with connection.cursor() as cursor:
                 cursor.execute(
                     f"""
@@ -134,6 +127,8 @@ class OrderService:
                 )
                 results = cursor.fetchall()
 
+                skany_ids_added = set()
+                print("skany_ids_added: ", skany_ids_added)
                 for row in results:
                     skany = {
                         "indeks": row[0],
@@ -143,13 +138,17 @@ class OrderService:
                         "raport": row[4],
                         "worker": f"{row[5]} {row[6]}",
                     }
-                    
                     formatted_time = skany["data"].strftime("%Y.%m.%d")
-                    skany_dict[zlecenie_obj["indeks"]].append(skany)  # use indeks as key for skany_dict
+                    if skany["indeks"] not in skany_ids_added:
+                        skany_ids_added.add(skany["indeks"])
+                        skany_dict[formatted_time].append(skany)
 
-            zlecenie_obj["skans"] = skany_dict[zlecenie_obj["indeks"]]  # assign list of skany for this zlecenie_obj
+            zlecenie_obj["skans"] = []
+            for formatted_time, skany_list in skany_dict.items():
+                for skany in skany_list:
+                    zlecenie_obj["skans"].append(skany)
 
-        response["products"] = zlecenia_dict  # no need to create a new list, use original zlecenia_dict
+        response["products"] = list(zlecenia_dict)
         response["status"] = status
 
         response["indeks"] = response["products"][0]["indeks"]
