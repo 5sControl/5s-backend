fb8445ce3a57eea3206cf98454fe0975d5df2b9a
nnnLik <makhmudov_ra@vk.com>
2023-03-22 11:24:34 +0300

    [ADD] todo

--- src/OrderView/models.py
+++ src/OrderView/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class DatabaseConnection(models.Model):
-    database_type = models.CharField(max_length=50, default="OrderView", blank=False, null=False)
+    database_type = models.CharField(
+        max_length=50, default="OrderView", blank=False, null=False
+    )
     server = models.CharField(max_length=20, blank=False, null=False)
     database = models.CharField(max_length=100, blank=False, null=False)
     username = models.CharField(max_length=50, blank=False, null=False)
-    password = models.CharField(max_length=200, blank=False, null=False)  # TODO: should be hashed
+    password = models.CharField(
+        max_length=200, blank=False, null=False
+    )  # TODO: should be hashed
