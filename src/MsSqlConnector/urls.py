67fc0cd44dfa4a25c6b6fd603f42131057b3405e
nnnLik <makhmudov_ra@vk.com>
2023-03-22 23:58:14 +0300

    [ADD] create skany

---
+++ src/MsSqlConnector/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import (
+    CreateSkanyAPIView
+)
+
+urlpatterns = [
+    # get data
+    path(
+        "skany/create/",
+        CreateSkanyAPIView.as_view(),
+        name="create skany in order",
+    ),
+]
