a1d95c13aad9b3f47ca4ecafa02faf2cf02e41c1
nnnLik <makhmudov_ra@vk.com>
2023-03-23 12:12:37 +0300

    [FIX] paginations query

--- src/OrderView/serializers.py
+++ src/OrderView/serializers.py
@@ -7,3 +7,10 @@ class DatabaseConnectionSerializer(serializers.ModelSerializer):
     class Meta:
         model = DatabaseConnection
         fields = ["id", "database_type", "server", "database", "username"]
+
+
+class ProductSerializer(serializers.Serializer):
+    indeks = serializers.IntegerField()
+    zlecenie = serializers.CharField(max_length=255)
+    status = serializers.CharField(max_length=255)
+    terminrealizacji = serializers.DateTimeField()
