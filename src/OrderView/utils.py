3642780d3c20e02deac4196f8758d5adb530045f
nnnLik <makhmudov_ra@vk.com>
2023-03-23 16:20:33 +0300

    [FIX] all_page_count by math lib

--- src/OrderView/utils.py
+++ src/OrderView/utils.py
@@ -1,4 +1,7 @@
+import math
+
 from collections import OrderedDict
+
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
@@ -9,11 +12,15 @@ class OrderViewPaginnator(PageNumberPagination):
     max_page_size = 100
 
     def get_paginated_response(self, data):
+        count = self.page.paginator.count
+        page_size = self.get_page_size(self.request)
+        all_page_count = math.ceil(count / page_size)
+
         return Response(OrderedDict([
-            ('count', self.page.paginator.count),
+            ('count', count),
             ("current_page", self.page.number),
-            ("records_on_page", self.page_size),
-            ("all_page_count", (self.page.paginator.count // self.page_size + 1 if self.page.paginator.count % self.page_size != 0 else self.page.paginator.count // self.page_size)),
+            ("records_on_page", page_size),
+            ("all_page_count", all_page_count),
             ('next', self.get_next_link()),
             ('previous', self.get_previous_link()),
             ('results', data),
