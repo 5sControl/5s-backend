471aa8e2894780e84ccee7c13f5f44e94862517e
anikeenko <dimskay1988@icloud.com>
2023-03-24 17:16:11 +0300

    EDIT items

--- src/Inventory/service.py
+++ src/Inventory/service.py
@@ -4,8 +4,8 @@ from src.Inventory.models import Items
 def process_item_status(data):
     """Updates the item status and adds the status to the extra"""
 
-    item_data = data['itemId']
-    count = data['count']
+    item_data = data[0]['itemId']
+    count = data[0]['count']
     data_item = Items.objects.filter(id=item_data).values('current_stock_level', 'low_stock_level')
     min_item = data_item[0].get('low_stock_level')
 
@@ -21,7 +21,7 @@ def process_item_status(data):
     item.current_stock_level = count
     item.save()
 
-    data["status"] = item_status
-    data["low_stock_level"] = min_item
+    data[0]["status"] = item_status
+    data[0]["low_stock_level"] = min_item
 
     return data
