8b4a70fe0a55cf5d87cd7845ddbae8ab34429a58
nnnlik <makhmudov_ra@vk.com>
2023-03-24 17:13:21 +0300

    [EDIT] by onvif-discovery

--- config/camera_finder.py
+++ config/camera_finder.py
@@ -1,56 +1,20 @@
-import re
-import os
-import nmap3
 from wsdiscovery.discovery import ThreadedWSDiscovery as WSDiscovery
-from concurrent.futures import ThreadPoolExecutor
-import netifaces
+from wsdiscovery import Scope
+import re
 
 
 class Finder:
-    def __init__(self):
-        self.started = False
-        self.ips = []
-        for iface in netifaces.interfaces():
-            if netifaces.AF_INET in netifaces.ifaddresses(iface):
-                self.ips.append(
-                    netifaces.ifaddresses(iface)[netifaces.AF_INET][0]["addr"]
-                )
-        self.scope = [".".join(ip.split(".")[:2]) for ip in self.ips]
-        self.wsd = WSDiscovery()
+    def fetch_devices():
+        wsd = WSDiscovery()
+        scope1 = Scope("onvif://www.onvif.org/Profile")
+        wsd.start()
+        services = wsd.searchServices(scopes=[scope1])
+        ipaddresses = []
+        for service in services:
+            ipaddress = re.search('(\d+|\.)+', str(service.getXAddrs()[0])).group(0)
+            ipaddresses.append(ipaddress)
 
-    def start(self):
-        self.wsd.start()
-        self.ret = self.wsd.searchServices()
-        self.onvif_services = [
-            s for s in self.ret if str(s.getTypes()).find("onvif") >= 0
-        ]
-        self.urls = [ip for s in self.onvif_services for ip in s.getXAddrs()]
-        self.lst = []
-        with ThreadPoolExecutor() as executor:
-            futures = [
-                executor.submit(self.check_camera, ip)
-                for ip in self.get_ips_from_urls(self.urls)
-            ]
-            for ip, result in zip(self.get_ips_from_urls(self.urls), futures):
-                if result and ip not in self.lst:
-                    self.lst.append(ip)
-        em_ip_camera = os.getenv("IP")
-        self.lst.append(em_ip_camera)
-        return self.lst
-
-    def check_camera(self, ip_address):
-        nm = nmap.PortScanner()
-        result = nm.scan(hosts=ip_address, arguments="-sn")
-        if ip_address in result["scan"]:
-            if result["scan"][ip_address]["status"]["state"] == "up":
-                return True
-        return False
-
-    def get_ips_from_urls(self, urls):
-        for url in urls:
-            match = re.search(r"\d+\.\d+\.\d+\.\d+", url)
-            if match:
-                yield match.group(0)
-
+        wsd.stop()
+        return ipaddresses
 
 finder = Finder()
