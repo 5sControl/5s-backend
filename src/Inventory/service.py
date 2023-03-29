ef9649a9b581514284fb4493c21da882b205d2fe
anikeenko <dimskay1988@icloud.com>
2023-03-29 12:17:51 +0300

    EDIT field extra in reports min_max

--- src/Inventory/service.py
+++ src/Inventory/service.py
@@ -5,25 +5,30 @@ from src.Mailer.service import send_message
 def process_item_status(data):
     """Updates the item status and adds the status to the extra"""
 
-    item_data = data[0]['itemId']
-    count = data[0]['count']
-    data_item = Items.objects.filter(id=item_data).values('current_stock_level', 'low_stock_level')
-    min_item = data_item[0].get('low_stock_level')
+    result = []
 
-    if count == 0:
-        item_status = "Out of stock"
-    elif count > 0 and count < min_item:
-        item_status = "Low stock level"
-        send_message(data_item.values(), count)
-    else:
-        item_status = "In stock"
+    for item_data in data:
 
-    item = Items.objects.filter(id=item_data).first()
-    item.status = item_status
-    item.current_stock_level = count
-    item.save()
+        count = item_data['count']
+        data_item = Items.objects.filter(id=item_data['itemId']).values('current_stock_level', 'low_stock_level')
+        min_item = data_item[0].get('low_stock_level')
 
-    data[0]["status"] = item_status
-    data[0]["low_stock_level"] = min_item
+        if count == 0:
+            item_status = "Out of stock"
+        elif count > 0 and count < min_item:
+            item_status = "Low stock level"
+            send_message(data_item.values(), count)
+        else:
+            item_status = "In stock"
 
-    return data
+        item = Items.objects.filter(id=item_data['itemId']).first()
+        item.status = item_status
+        item.current_stock_level = count
+        item.save()
+
+        item_data["status"] = item_status
+        item_data["low_stock_level"] = min_item
+
+        result.append(item_data)
+
+    return result
