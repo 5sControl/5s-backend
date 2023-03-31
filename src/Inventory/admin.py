74d615331f4e0adbd7791a7bc654b41769244742
anikeenko <dimskay1988@icloud.com>
2023-03-31 13:17:30 +0300

    ADDED admin in inventory items

--- src/Inventory/admin.py
+++ src/Inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from src.Inventory.models import Items
 
-# Register your models here.
+
+@admin.register(Items)
+class ItemsAdmin(admin.ModelAdmin):
+    list_display = (
+        "name", "id", "status", "current_stock_level", "low_stock_level", "email", "camera", "date_updated", "coords")
+    list_filter = ("name", "id", "status", "current_stock_level", "low_stock_level", "camera")
