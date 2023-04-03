15a59a52ddd8eb12ad4d553fdf9d9c9cd979b4f8
nnnlik <makhmudov_ra@vk.com>
2023-04-03 16:30:45 +0300

    [FIX] datetime

--- src/OrderView/services.py
+++ src/OrderView/services.py
@@ -225,8 +225,9 @@ class OrderService:
                     if skany_report:
                         status = skany_report.report.violation_found
 
-                    time_obj = datetime.fromisoformat(row[1].replace(tzinfo=timezone.utc).replace('Z', '+00:00'))
-                    new_time_str = time_obj.strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+                    time_obj = datetime.fromisoformat(row[1].replace(tzinfo=timezone.utc).replace('Z', '+00:00')).replace(tzinfo=timezone.utc)
+                    new_time_str = time_obj.strftime("%Y-%m-%d %H:%M:%S.%f")
+                    
                     video_data = get_skany_video_info(time=new_time_str)
 
                     skany = {
