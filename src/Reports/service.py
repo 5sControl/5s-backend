a1fd200b3ed0fdcb5b9419f5df38ef1f14668672
anikeenko <dimskay1988@icloud.com>
2023-04-03 15:48:04 +0300

    EDIT service added operation_index

--- src/Reports/service.py
+++ src/Reports/service.py
@@ -1,10 +1,12 @@
 from src.MsSqlConnector.services import create_records
+from src.OrderView.models import IndexOperations
 from src.Reports.models import SkanyReport
 
 
 def edit_extra(data):
     try:
-        skany_index = create_records.get_max_skany_indeks_by_typ(2)
+        operation_index = IndexOperations.objects.first().type_operation
+        skany_index = create_records.get_max_skany_indeks_by_typ(operation_index)
         data["skany_index"] = skany_index
     except Exception as e:
         print(f'failed to get skany index {e}')
