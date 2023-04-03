3f88457f54c390a3cf8ebb1404bdd73af1c2ec04
anikeenko <dimskay1988@icloud.com>
2023-04-03 11:01:28 +0300

    EDIT models added information print

--- src/Inventory/models.py
+++ src/Inventory/models.py
@@ -43,9 +43,11 @@ class Items(models.Model):
 
                 # started process
                 camera = Camera.objects.filter(id=self.camera)
+                print("camera", [camera])
                 algorithm = Algorithm.objects.filter(name='min_max_control')
+                print("algorithm", algorithm)
                 server_url = yolo_proccesing.get_algorithm_url()
-                print(server_url)
+                print("server_url", server_url)
                 algorithms_services.create_new_records(cameras=[camera], algorithm=algorithm, server_url=server_url)
         except Exception as exc:
             print(f"Exception {exc}")
