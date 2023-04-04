9b3f16d3abf6c957cff9662137492f61f1ec1789
anikeenko <dimskay1988@icloud.com>
2023-04-04 12:01:32 +0300

    EDIT admin, added readonly info

--- src/CompanyLicense/admin.py
+++ src/CompanyLicense/admin.py
@@ -5,3 +5,12 @@ from .models import Company
 @admin.register(Company)
 class CamerasAdmin(admin.ModelAdmin):
     list_filter = ("id",)
+    readonly_fields = (
+        'license_key',
+        'name_company',
+        'date_joined',
+        'valid_until',
+        'is_active',
+        'count_cameras',
+        'neurons_active'
+    )
