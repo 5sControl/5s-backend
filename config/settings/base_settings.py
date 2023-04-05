ca7aacb3021aeb2d88c3860ee3aeb691568602d5
RasulMakhmudov <makhmudov_ra@vk.com>
2023-04-05 15:57:45 +0300

    [ADD] base settings

---
+++ config/settings/base_settings.py
@@ -0,0 +1,93 @@
+from pathlib import Path
+from decouple import config
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+SECRET_KEY = config("SECRET_KEY", default="default_secret_key")
+
+DEBUG = config("DEBUG", default=False, cast=bool)
+
+ALLOWED_HOSTS = ["*"]
+
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    # Third party application
+    "django_filters",
+    "rest_framework",
+    "djoser",
+    "corsheaders",
+    "drf_yasg",
+    # Main application
+    "src.StaffControl.Employees.apps.EmployeesConfig",
+    "src.StaffControl.Locations.apps.LocationsConfig",
+    "src.StaffControl.History.apps.HistoryConfig",
+    # Common application
+    "src.Algorithms",
+    "src.OrderView",
+    "src.MsSqlConnector",
+    "src.Cameras.apps.CamerasConfig",
+    "src.CompanyLicense.apps.CompanyLicenseConfig",
+    "src.Inventory.apps.InventoryConfig",
+    # Collections reports
+    "src.Reports.apps.ReportsConfig",
+    "src.ImageReport.apps.ImageConfig",
+    "src.Core"
+]
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+]
+
+
+ROOT_URLCONF = "config.urls"
+
+WSGI_APPLICATION = "config.wsgi.application"
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "Etc/GMT"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static/"
+
+MEDIA_URL = "/images/"
+MEDIA_ROOT = BASE_DIR / "images/"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
