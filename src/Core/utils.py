da2ab9c24a2f5c6ca79f04fd1c90e3de57bf7c32
nnnlik <makhmudov_ra@vk.com>
2023-04-06 13:21:58 +0300

    [ADD] logs to deployment

--- src/Core/utils.py
+++ src/Core/utils.py
@@ -7,5 +7,6 @@ HOST = yolo_proccesing.get_algorithm_url()
 
 
 def send_request_to_update_service(service):
+    print(f"Sending request to update service {service}")
     request = requests.post(f"{HOST}/deploy?service={service}")
     return request.status_code
