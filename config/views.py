0f49eb3a33ee48e8ba64f8341a68a4506781581f
nnnlik <makhmudov_ra@vk.com>
2023-04-06 15:55:54 +0300

    [ADD] logs camera

--- config/views.py
+++ config/views.py
@@ -32,6 +32,7 @@ class RegisterView(generics.GenericAPIView):
 class FindCameraAPIView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         cameras = requests.get(f"{yolo_proccesing.get_algorithm_url()}:7654/get_all_onvif_cameras/")
+        print(cameras.json())
         serializer = CameraListSerializer(data=cameras.json())
         serializer.is_valid(raise_exception=True)
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
