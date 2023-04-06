ffc98b0507a06ea1d0924e13ae81046e669b8229
nnnlik <makhmudov_ra@vk.com>
2023-04-06 15:58:40 +0300

    [FIX] many items

--- config/views.py
+++ config/views.py
@@ -33,7 +33,7 @@ class FindCameraAPIView(generics.GenericAPIView):
     def get(self, request, *args, **kwargs):
         cameras = requests.get(f"{yolo_proccesing.get_algorithm_url()}:7654/get_all_onvif_cameras/")
         print(cameras.json())
-        serializer = CameraListSerializer(data=cameras.json())
+        serializer = CameraListSerializer(data=cameras.json(), many=True)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.validated_data, status=status.HTTP_200_OK)
 
