a783f5ea239e5b132d8ebe0fdaf9cf3bd5a72a8b
nnnlik <makhmudov_ra@vk.com>
2023-04-06 15:29:00 +0300

    [EDIT] key in ser

--- config/serializers.py
+++ config/serializers.py
@@ -2,7 +2,4 @@ from rest_framework import serializers
 
 
 class CameraListSerializer(serializers.ListSerializer):
-    child = serializers.CharField()
-
-    def to_representation(self, data):
-        return data
+    result = serializers.ListField(child=serializers.IPAddressField())
