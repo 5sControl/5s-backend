f57beaddb4df520dbac1dba060d105d641e80860
anikeenko <dimskay1988@icloud.com>
2023-04-11 14:41:25 +0300

    ADD EmailAdmin, RecipientAdmin, MessagesAdmin, SMTPSettingsAdmin

--- src/Mailer/admin.py
+++ src/Mailer/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from src.Mailer.models import Emails, Recipients, Messages, SMTPSettings
 
-# Register your models here.
+
+@admin.register(Emails)
+class EmailAdmin(admin.ModelAdmin):
+    list_display = ("email", "id",)
+
+
+@admin.register(Recipients)
+class RecipientAdmin(admin.ModelAdmin):
+    list_display = ("item", "email", "message", "id")
+
+
+@admin.register(Messages)
+class MessagesAdmin(admin.ModelAdmin):
+    list_display = ("subject", "message", "is_send", "date_created", "date_updated", "id",)
+
+
+@admin.register(SMTPSettings)
+class SMTPSettingsAdmin(admin.ModelAdmin):
+    list_display = ("server", "port", "username", "password", "email_use_tls", "email_use_ssl", "id")
