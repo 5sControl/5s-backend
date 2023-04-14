3af8207f66be72e4a26c6c7130bbe4e744fb41f5
anikeenko <dimskay1988@icloud.com>
2023-04-14 15:08:39 +0300

    ADD WorkingTimeAdmin

--- src/Mailer/admin.py
+++ src/Mailer/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from src.Mailer.models import Emails, SMTPSettings
+from src.Mailer.models import Emails, SMTPSettings, WorkingTime
 
 
 @admin.register(Emails)
 class EmailAdmin(admin.ModelAdmin):
     list_display = ("email", "id", 'is_active')
+
+
+@admin.register(WorkingTime)
+class WorkingTimeAdmin(admin.ModelAdmin):
+    list_display = ('time_start', 'time_end', 'id')
 
 
 @admin.register(SMTPSettings)
