d4cdf49b5fa46066fd0c62f0541f8b4901db45a2
anikeenko <dimskay1988@icloud.com>
2023-04-19 15:43:45 +0300

    EDIT check_low message text

--- src/Mailer/check_low_stock_items.py
+++ src/Mailer/check_low_stock_items.py
@@ -17,7 +17,7 @@ def run():
     stock_items = Items.objects.filter(Q(status="Low stock level") | Q(status="Out of stock"))
     if len(stock_items) >= 1:
         email_list = Emails.objects.filter(is_active=True).values('email')
-        subject = 'Daily Low Stock Report'
+        subject = '5S Control Daily Low Stock Report'
         server_url = yolo_proccesing.get_algorithm_url()
 
         # email service check
@@ -35,9 +35,9 @@ def run():
             # Build email content for all items with low stock
             message = 'The inventory level of the following items has fallen to a low stock level. We recommend that you take immediate action to replenish your stock to avoid stockouts or shortages.\nHere is the list of items and their corresponding quantities:\n'
             for item in stock_items:
-                message += f"{item.name} - {item.current_stock_level}\n"
+                message += f"{item.name} : {item.current_stock_level} (low stock level: {item.low_stock_level})\n"
 
-            message += f"\n{server_url}:3000/inventory"
+            message += f"\n{server_url}:3000/inventory\n\nYou are receiving this email because your email account was entered in 5S Control system to receive notifications regarding low stock levels of inventory."
             # Send email to all users
             connection = EmailBackend(
                 host=smtp_settings.server,
