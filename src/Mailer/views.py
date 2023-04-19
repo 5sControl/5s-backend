83dc79fc6f19306edb5547c50939c66e41da37a7
anikeenko <dimskay1988@icloud.com>
2023-04-19 16:18:48 +0300

    EDIT views added permission

--- src/Mailer/views.py
+++ src/Mailer/views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from src.Mailer.models import SMTPSettings, Emails, WorkingTime
@@ -6,20 +7,28 @@ from rest_framework import generics
 
 
 class SMTPSettingsListCreateView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
     queryset = SMTPSettings.objects.all().order_by('id')
     serializer_class = SMTPSettingsSerializer
 
 
 class SMTPSettingsRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
     queryset = SMTPSettings.objects.all().order_by('id')
     serializer_class = SMTPSettingsSerializer
 
 
 class EmailsView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
     queryset = Emails.objects.all().order_by('id')
     serializer_class = EmailsSerializer
 
 
 class WorkingTimeView(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    pagination_class = None
     queryset = WorkingTime.objects.all().order_by('id')
     serializer_class = WorkingTimeSerializer
