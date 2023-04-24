816c2661d710646f41d6e5001ed846e5ea1ddc94
RasulMakhmudov <makhmudov_ra@vk.com>
2023-04-24 11:08:33 +0300

    [ADD] host in allowed

--- config/settings/security_settings.py
+++ config/settings/security_settings.py
@@ -29,11 +29,11 @@ CORS_ORIGIN_WHITELIST = [
     "http://0.0.0.0:8000",
     "http://*",
     "https://*",
+    "https://cfce-81-7-77-205.ngrok-free.app",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
     "http://*",
     "https://*",
-    "https://fe9e-134-17-26-206.eu.ngrok.io/",
-    "https://cfce-81-7-77-205.ngrok-free.app/"
+    "https://cfce-81-7-77-205.ngrok-free.app"
 ]
