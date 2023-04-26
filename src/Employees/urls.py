a4f3330a9e891f5dd7bb8434c9739dfdbe6d1030
nnnlik <makhmudov_ra@vk.com>
2023-04-26 12:37:03 +0300

    [REMOVED] register

--- src/Employees/urls.py
+++ src/Employees/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from src.Employees.views import CreateUserView, RegisterView, UserListApiView
+from src.Employees.views import CreateUserView, UserListApiView
 
 
 urlpatterns = [
     path("create/", CreateUserView.as_view()),
-    path("register/", RegisterView.as_view()),
     path("", UserListApiView.as_view()),
 ]
