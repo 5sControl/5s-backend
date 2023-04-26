9c8eef866399e9631303fb8f256ceea3b0d0a538
RasulMakhmudov <109548451+nnnLik@users.noreply.github.com>
2023-04-26 16:03:55 +0300

    Update camera_delete.py

--- src/Cameras/camera_delete.py
+++ src/Cameras/camera_delete.py
@@ -8,25 +8,25 @@ from src.Cameras.models import Camera
 def delete_camera(camera_id):
     query_list_cameraalgorithms = CameraAlgorithm.objects.filter(camera=camera_id)
 
-    if query_list_cameraalgorithms:
-        for camera_algorithms in query_list_cameraalgorithms:
-            pid = camera_algorithms.process_id
-            result_stop_process = yolo_proccesing.stop_process(pid)
-            if not result_stop_process["success"]:
-                return {
-                    "status": False,
-                    "message": f"Error deleting camera"
-                }
-            result_update_status = algorithms_services.update_status_of_algorithm_by_pid(pid)
-            if not result_update_status["status"]:
-                return result_update_status
-    else:
+    for camera_algorithms in query_list_cameraalgorithms:
+        pid = camera_algorithms.process_id
+        result_stop_process = yolo_proccesing.stop_process(pid)
+        if not result_stop_process["success"]:
+            return {
+                "status": False,
+                "message": f"Can't stop process on camera {camera_algorithms.camera} algorithm {camera_algorithms.algorithm}"
+            }
+        algorithms_services.update_status_of_algorithm_by_pid(pid)
+
+    camera = Camera.objects.filter(id=camera_id)
+    if not camera:
         return {
             "status": False,
             "message": f"Camera {camera_id} does not exist"
         }
-    Camera.objects.filter(id=camera_id).delete()
-    return {
-        "status": True,
-        "message": f"Camera {camera_id} was successfully deleted."
-    }
+    else:
+        camera.delete()
+        return {
+            "status": True,
+            "message": f"Camera {camera_id} was successfully deleted."
+        }
