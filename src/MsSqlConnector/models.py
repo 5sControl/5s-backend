52d58ec31eac67bab5c38d3082f5dd16a93fba1e
nnnlik <makhmudov_ra@vk.com>
2023-04-26 12:37:35 +0300

    [EDIT] password field

--- src/MsSqlConnector/models.py
+++ src/MsSqlConnector/models.py
@@ -9,18 +9,18 @@ class DatabaseConnection(models.Model):
     server = models.CharField(max_length=200)
     database = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
-    password_encrypted = models.BinaryField()
+    password = models.BinaryField()
     port = models.IntegerField(default=1433)
 
     def get_password(self):
         key = os.environ.get("HASH")
         f = Fernet(key)
-        password = f.decrypt(self.password_encrypted).decode()
+        password = f.decrypt(self.password).decode()
         return password
 
     def set_password(self, password):
         key = os.environ.get("HASH")
         f = Fernet(key)
-        self.password_encrypted = f.encrypt(password.encode())
+        self.password = f.encrypt(password.encode())
 
     password = property(get_password, set_password)
