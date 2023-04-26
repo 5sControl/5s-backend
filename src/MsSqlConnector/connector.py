cab361724b36d3fdf2f804961b5d54e505f7caaa
nnnlik <makhmudov_ra@vk.com>
2023-04-26 11:45:00 +0300

    [ADD] ports

--- src/MsSqlConnector/connector.py
+++ src/MsSqlConnector/connector.py
@@ -18,9 +18,12 @@ class MsSqlConnector:
         database = connection_data["database"]
         username = connection_data["username"]
         password = connection_data["password"]
+        port = connection_data["port"]
 
-        self._is_database_connection_is_stable(server, database, username, password)
-        self._is_database_connection_exist(server, database, username)
+        self._is_database_connection_is_stable(
+            server, database, username, password, port
+        )
+        self._is_database_connection_exist(server, database, username, port)
 
         ms_sql_connection = DatabaseConnection(
             database_type=database_type,
@@ -37,12 +40,15 @@ class MsSqlConnector:
             "server": ms_sql_connection.server,
             "database": ms_sql_connection.database,
             "username": ms_sql_connection.username,
+            "port": ms_sql_connection.port,
         }
         return connection
 
-    def _is_database_connection_is_stable(self, server, database, username, password):
+    def _is_database_connection_is_stable(
+        self, server, database, username, password, port
+    ):
         master_conn_str = self._get_connection_string(
-            server, "master", username, password, self.driver
+            server, "master", username, password, self.driver, port
         )
         try:
             with pyodbc.connect(master_conn_str) as connection:
@@ -65,7 +71,7 @@ class MsSqlConnector:
             )
 
         conn_str = self._get_connection_string(
-            server, database, username, password, self.driver
+            server, database, username, password, self.driver, port
         )
         try:
             with pyodbc.connect(conn_str) as connection:
@@ -74,7 +80,7 @@ class MsSqlConnector:
             print("Database done 0_0")
             raise ValidationError({"detail": f"Database connection error: {str(e)}"})
 
-    def _is_database_connection_exist(self, server, database, username):
+    def _is_database_connection_exist(self, server, database, username, port):
         if DatabaseConnection.objects.filter(
             server=server, database=database, username=username
         ):
@@ -91,17 +97,20 @@ class MsSqlConnector:
         database = connection_data["database"]
         username = connection_data["username"]
         password = connection_data["password"]
+        port = connection_data["port"]
 
         conn_str = self._get_connection_string(
-            server, database, username, password, self.driver
+            server, database, username, password, self.driver, port
         )
 
         connection = pyodbc.connect(conn_str)
 
         return connection
 
-    def _get_connection_string(self, server, database, username, password, driver):
-        return f"SERVER={server};DATABASE={database};UID={username};PWD={password};DRIVER={driver};TrustServerCertificate=yes"  # noqa
+    def _get_connection_string(
+        self, server, database, username, password, driver, port
+    ):
+        return f"SERVER={server};PORT={port};DATABASE={database};UID={username};PWD={password};DRIVER={driver};TrustServerCertificate=yes"  # noqa
 
     def get_conections(self):
         return DatabaseConnection.objects.all()
--- src/MsSqlConnector/models.py
+++ src/MsSqlConnector/models.py
@@ -2,12 +2,9 @@ from django.db import models
 
 
 class DatabaseConnection(models.Model):
-    database_type = models.CharField(
-        max_length=50, default="OrderView", blank=False, null=False
-    )
-    server = models.CharField(max_length=300, blank=False, null=False)
-    database = models.CharField(max_length=100, blank=False, null=False)
-    username = models.CharField(max_length=50, blank=False, null=False)
-    password = models.CharField(
-        max_length=200, blank=False, null=False
-    )  # TODO: should be hashed
+    database_type = models.CharField(max_length=50, default="OrderView")
+    server = models.CharField(max_length=200)
+    database = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)  # TODO: should be hashed
+    port = models.IntegerField()
