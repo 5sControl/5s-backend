07fcc83f5fe282f2a633716719d8c91a9e84cc02
nnnlik <makhmudov_ra@vk.com>
2023-04-26 12:16:26 +0300

    [ADD] hashed password

--- src/MsSqlConnector/models.py
+++ src/MsSqlConnector/models.py
@@ -1,4 +1,7 @@
+import os
 from django.db import models
+
+from cryptography.fernet import Fernet
 
 
 class DatabaseConnection(models.Model):
@@ -6,5 +9,18 @@ class DatabaseConnection(models.Model):
     server = models.CharField(max_length=200)
     database = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
-    password = models.CharField(max_length=200)  # TODO: should be hashed
+    password_encrypted = models.BinaryField()
     port = models.IntegerField(default=1433)
+
+    def get_password(self):
+        key = os.environ.get("HASH")
+        f = Fernet(key)
+        password = f.decrypt(self.password_encrypted).decode()
+        return password
+
+    def set_password(self, password):
+        key = os.environ.get("HASH")
+        f = Fernet(key)
+        self.password_encrypted = f.encrypt(password.encode())
+
+    password = property(get_password, set_password)
