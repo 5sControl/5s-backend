7c2ce1dffc26b05f9b8be3f46d17a5d1dd6a4f7b
RasulMakhmudov <makhmudov_ra@vk.com>
2023-04-27 13:25:30 +0300

    [ADD] description field

--- src/Algorithms/management/commands/algorithm.py
+++ src/Algorithms/management/commands/algorithm.py
@@ -4,44 +4,50 @@ from src.Core.logger import logger
 from src.Algorithms.models import Algorithm
 
 
-class Command(BaseCommand):
-    """
-    Fill table with available algorithms
-    """
+safety_control_ear_protection_description = """
+designed to ensure that the workers in a particular area are wearing ear protection to safeguard their hearing.
+This control is important in environments where workers are exposed to loud noises that could damage their hearing over time.
+"""
+safety_control_head_protection_description = """
+designed to ensure that workers are wearing safety helmets to protect their heads from potential hazards.
+This control is crucial in construction sites, mining areas, and other hazardous workplaces where head injuries are common.
+"""
+safety_control_hand_protection_description = """
+designed to ensure that workers are wearing protective gloves to prevent hand injuries.
+This control is essential in workplaces where the workers are exposed to sharp objects, chemicals, or high-temperature materials that could cause burns or cuts.
+"""
+safety_control_reflective_jacket_description = """
+designed to ensure that workers are wearing reflective jackets to increase their visibility and reduce the risk of accidents caused by low visibility.
+This control is critical in workplaces where workers are exposed to low light conditions, such as construction sites, mining areas, or transportation facilities.
+"""
+machine_control_description = """
+is designed to ensure that the machine is not left unsupervised, which could lead to accidents, breakdowns, or other issues (downtime & lost profits).
+This control is essential in workplaces where machines are used, such as factories, construction sites, or warehouses.
+"""
 
+
+class Command(BaseCommand):
     def handle(self, *args, **options):
         self.ALGORITHMS = [
-            {"name": "staff_control", "is_available": False},
-            {"name": "idle_control", "is_available": True},
-            {"name": "operation_control", "is_available": True},
-            {"name": "tool_control", "is_available": False},
-            {"name": "machine_control", "is_available": True},
-            {"name": "safety_control_ear_protection", "is_available": True},
-            {"name": "safety_control_head_protection", "is_available": False},
-            {"name": "safety_control_hand_protection", "is_available": False},
-            {"name": "safety_control_reflective_jacket", "is_available": True},
-            {"name": "min_max_control", "is_available": True},
+            {"name": "idle_control", "is_available": True, "description": ""},
+            {"name": "operation_control", "is_available": True, "description": ""},
+            {"name": "machine_control", "is_available": True, "description": machine_control_description},
+            {"name": "safety_control_ear_protection", "is_available": True, "description": safety_control_ear_protection_description},
+            {"name": "safety_control_head_protection", "is_available": False, "description": safety_control_head_protection_description},
+            {"name": "safety_control_hand_protection", "is_available": False, "description": safety_control_hand_protection_description},
+            {"name": "safety_control_reflective_jacket", "is_available": True, "description": safety_control_reflective_jacket_description},
+            {"name": "min_max_control", "is_available": True, "description": ""},
         ]
-        self.algorithms_was_exist = 0
-        self.algorithms_was_created = 0
         self.create_algorithms()
-        logger.info(f"{self.algorithms_was_exist} - was exist")
-        logger.info(f"{self.algorithms_was_created} - was created")
 
     def create_algorithms(self):
         for algorithms_data in self.ALGORITHMS:
             if Algorithm.objects.filter(name=algorithms_data["name"]).first():
-                logger.warning(f"algorithms {algorithms_data['name']} exist")
-                self.algorithms_was_exist += 1
                 continue
             else:
                 algorithms = Algorithm.objects.create(
                     name=algorithms_data["name"],
                     is_available=algorithms_data["is_available"],
+                    description=algorithms_data["description"]
                 )
                 algorithms.save()
-            logger.info(
-                f"algorithms {algorithms_data['name']} was successfully created"
-            )
-
-            self.algorithms_was_created += 1
--- src/Algorithms/models.py
+++ src/Algorithms/models.py
@@ -10,6 +10,7 @@ class Algorithm(models.Model):
 
     name = models.CharField(max_length=100)
     is_available = models.BooleanField(default=False)
+    description = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.name
