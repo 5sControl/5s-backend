27af5e50c7330bcdb08ea62600ce1365dc46fcce
RasulMakhmudov <makhmudov_ra@vk.com>
2023-04-27 12:59:57 +0300

    [ADD] url names

--- src/Cameras/urls.py
+++ src/Cameras/urls.py
@@ -11,10 +11,10 @@ from django.urls import path
 
 
 urlpatterns = [
-    path("", GetCameraAPIView.as_view()),
-    path("get-camera-http-links/", GetHttpCamerasLinkAPIView.as_view()),
-    path("get-camera-rtsp-by-ip-links/", GetRtspCamerasLinkByIpAPIView.as_view()),
-    path("create-camera/", CreateCameraAPIView.as_view()),
-    path("update-camera/", UpdateCameraAPIView.as_view()),
-    path('delete_camera/<str:pk>/', DeleteCameraAPIView.as_view()),
+    path("", GetCameraAPIView.as_view(), name="camera"),
+    path("get-camera-http-links/", GetHttpCamerasLinkAPIView.as_view(), name="http-links"),
+    path("get-camera-rtsp-by-ip-links/", GetRtspCamerasLinkByIpAPIView.as_view(), name="rtsp-by-ip-links"),
+    path("create-camera/", CreateCameraAPIView.as_view(), name="create-camera"),
+    path("update-camera/", UpdateCameraAPIView.as_view(), name="update-camera"),
+    path('delete_camera/<str:pk>/', DeleteCameraAPIView.as_view(), name="delete-camera"),
 ]
