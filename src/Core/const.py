2b56c66e365b0a7a640c12ce4784545e0a3df73c
nnnlik <makhmudov_ra@vk.com>
2023-04-28 16:15:35 +0300

    [FIX] type of production

--- src/Core/const.py
+++ src/Core/const.py
@@ -2,7 +2,12 @@ import os
 
 
 SERVER_URL = os.environ.get("SERVER_URL")
+
 PRODUCTION = os.environ.get("PRODUCTION")
+if PRODUCTION is not None and PRODUCTION.lower() == "true":
+    PRODUCTION = True
+else:
+    PRODUCTION = False
 
 print("Server Configuration:")
 print(f"SERVER_URL -> {SERVER_URL}")
