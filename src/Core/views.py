7fff533ee8da3ef5f207ba36a7e766b196471ec5
nnnlik <makhmudov_ra@vk.com>
2023-05-02 16:07:14 +0300

    [REMOVED] deployment

--- src/Core/views.py
+++ src/Core/views.py
@@ -5,16 +5,6 @@ from rest_framework.response import Response
 from rest_framework import status, generics
 from src.Core.const import SERVER_URL
 
-from src.Core.utils import send_request_to_update_service
-
-
-class StartDeployment(generics.GenericAPIView):
-
-    def post(self, request):
-        print("Starting deployment")
-        service = request.GET.get("service")
-        return Response(send_request_to_update_service(service), status=status.HTTP_102_PROCESSING)
-
 
 class CheckMemoryStatus(generics.GenericAPIView):
     def get(self, request):
