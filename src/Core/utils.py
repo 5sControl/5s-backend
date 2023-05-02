42b45435a1aa5e943d540a83f2538e642a334248
nnnlik <makhmudov_ra@vk.com>
2023-05-02 16:07:04 +0300

    [ADD] sender

--- src/Core/utils.py
+++ src/Core/utils.py
@@ -3,7 +3,31 @@ import requests
 from src.Core.const import SERVER_URL
 
 
-def send_request_to_update_service(service):
-    print(f"Sending request to update service {service}")
-    request = requests.post(f"{SERVER_URL}/deploy?service={service}")
-    return request.status_code
+def Sender(operation, data, cstm_port=None):
+    url = None
+    port = None
+
+    if operation == "add_camera":
+        url = "/add_camera"
+        port = 3456
+
+    if operation == "run":
+        url = "/run"
+        if data["algorithm"] == "min_max_control":
+            port = 3020
+        else:
+            port = 3333
+
+    if operation == "stop":
+        url = "/stop"
+        port = 3333
+
+    if cstm_port:
+        link = f"{SERVER_URL}:{cstm_port}{url}"
+    else:
+        link = f"{SERVER_URL}:{port}{url}"
+
+    request = requests.post(link, json=data)
+    request.raise_for_status()
+
+    return request.json()
