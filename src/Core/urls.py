912da08ea9d1aa3ba439a8d3ed7c43f90b9b3175
nnnlik <makhmudov_ra@vk.com>
2023-05-04 12:31:00 +0300

    [REFACTOR] black

--- src/Core/urls.py
+++ src/Core/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import (
-    FindCameraAPIView, CheckMemoryStatus
-)
+from .views import FindCameraAPIView, CheckMemoryStatus
 
 urlpatterns = [
     path(
-        "is_enough_memory/", CheckMemoryStatus.as_view(), name="memory_available",
+        "is_enough_memory/",
+        CheckMemoryStatus.as_view(),
+        name="memory_available",
     ),
     path("find_cameras/", FindCameraAPIView.as_view(), name="find cameras"),
 ]
