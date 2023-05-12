929bd512e384fa6c4b77c00f885b8cfc2bfbb7d6
anikeenko <dimskay1988@icloud.com>
2023-05-12 16:54:12 +0300

    EDIT method of deleting an item

--- src/Inventory/models.py
+++ src/Inventory/models.py
@@ -54,7 +54,10 @@ class Items(models.Model):
         from src.Inventory.service import stopped_process, started_process
         stopped_process(self.camera)
 
-        # started process
-        started_process(self.camera)
+        # started process if there are more than 0 items
+        items_camera = Items.objects.filter(camera_id=self.camera)
+        if len(items_camera) > 0:
+
+            started_process(self.camera)
 
         return instance
