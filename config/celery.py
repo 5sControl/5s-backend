2a035cfbc80057a94cb800d083fc38aed77fe5fb
anikeenko <dimskay1988@icloud.com>
2023-05-13 12:14:21 +0300

    EDIT celery settings

--- config/celery.py
+++ config/celery.py
@@ -2,6 +2,9 @@ import os
 
 from celery import Celery
 
+from src.Mailer.tasks import send_low_stock_notification
+
+
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -18,5 +21,5 @@ app.autodiscover_tasks()
 
 
 @app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+def send_low_stock_task(self):
+    send_low_stock_notification()
