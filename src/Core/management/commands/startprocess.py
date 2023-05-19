6ae70af84c1c2bbee83978b1684ccdb94da9900b
nnnlik <makhmudov_ra@vk.com>
2023-05-19 11:19:18 +0300

    [ADD] logs for request

--- src/Core/management/commands/startprocess.py
+++ src/Core/management/commands/startprocess.py
@@ -47,6 +47,8 @@ class Command(BaseCommand):
                 "algorithm": algorithm_obj.name,
                 "extra": extra_params,
             }
+            
+            logger.warning(f"Request data for algorithm {request}")
 
             try:
                 result = send_run_request(request)
