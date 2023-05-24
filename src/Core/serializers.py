be1c55957ebebb0af164d4e30e156e75e52b3f49
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-24 15:20:14 +0300

    [EDIT] required false

--- src/Core/serializers.py
+++ src/Core/serializers.py
@@ -4,7 +4,7 @@ from src.Core.models import SystemMessage
 
 
 class SystemMessagesSerializer(serializers.ModelSerializer):
-    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S.%f")
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S.%f", required=False)
 
     class Meta:
         model = SystemMessage
