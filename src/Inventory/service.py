f61c9d2246ebaa62070cd6fe1d37541a54e43366
anikeenko <dimskay1988@icloud.com>
2023-05-29 10:30:35 +0300

    ADD information print

--- src/Inventory/service.py
+++ src/Inventory/service.py
@@ -141,6 +141,7 @@ def started_process(camera):
         camera_algorithm.save()
     except CameraAlgorithm.DoesNotExist:
         print("first algorithm not found")
+        print("data", data)
         response = send_run_request(data)
         print("response", response)
         CameraAlgorithm.objects.create(algorithm=algorithm, camera=camera, process_id=response["pid"])
