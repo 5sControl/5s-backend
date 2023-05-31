7c6fc6a7a43bf30632e62db2164973dc3f9ed342
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-31 13:50:25 +0300

    [EDIT] join

--- src/newOrderView/services/order_services.py
+++ src/newOrderView/services/order_services.py
@@ -3,34 +3,46 @@ from typing import List, Any, Tuple, Dict
 from src.MsSqlConnector.connector import connector as connector_service
 
 
-class OrderServices:
-    def get_order(self, from_date: str, to_date: str):
-        order_query = """
-                SELECT DISTINCT z.zlecenie as order, z.indeks as id
-                FROM zlecenia z
-                WHERE 1=1
-            """
-
-        connection = connector_service.get_database_connection()
-        params: List[Any] = []
-
-        if from_date and to_date:
-            if from_date == to_date:
-                order_query += " AND CAST(datawejscia AS DATE) = ?"
-                params.append(from_date)
-            else:
-                order_query += " AND z.datawejscia BETWEEN ? AND ?"
-                params.extend([from_date, to_date])
-
-        data: List[Tuple[Any]] = connector_service.executer(
-            connection=connection, query=order_query, params=params
-        )
-
         result: List[Dict[str, Any]] = [
             {"id": item[0], "order": item[1].strip()} for item in data
         ]
 
         return result
 
+class OrderServices:
+    def get_order(self, from_date: str, to_date: str):
+        query: str = f'''
+            SELECT sk.indeks, sk.data, st.OperationID, st.OperationName
+            FROM skany sk
+            JOIN Stanowiska st ON sk.stanowisko = st.indeks
+            WHERE 1=1 
+        '''
+
+        connection = connector_service.get_database_connection()
+        params: List[Any] = []
+
+        if from_date and to_date:
+                query += " AND sk.data >= ? AND sk.data <= ?"
+                params.extend([from_date, to_date])
+        print(query, params)
+
+        results = connector_service.executer(
+            connection=connection,
+            query=query,
+            params=params,
+        )
+        print(results)
+        return result
+
 
 services = OrderServices()
+
+class OrderServices:
+    def get_operation_by_date(self, from_date: str, to_date: str):
+
+        
+        
+
+
+services = OrderServices()
+
