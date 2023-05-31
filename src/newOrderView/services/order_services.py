ed8bb42e93bc75dc46f1fd60810185796815a22f
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-31 15:15:15 +0300

    [EDIT] response to list

--- src/newOrderView/services/order_services.py
+++ src/newOrderView/services/order_services.py
@@ -7,55 +7,69 @@ from src.MsSqlConnector.connector import connector as connector_service
 
 class OrderServices:
     def get_order(self, from_date: str, to_date: str):
-        query: str = """
-            SELECT
-                sk.indeks AS id,
-                sk.data AS startTime,
-                st.indeks as workplace,
-                st.raport AS operationName,
-                z.indeks AS orderID,
-                z.zlecenie AS orderName
-            FROM Skany sk
-            JOIN Stanowiska st ON sk.stanowisko = st.indeks
-            JOIN Skany_vs_Zlecenia sz ON sk.indeks = sz.indeks
-            JOIN zlecenia z ON sz.indekszlecenia = z.indeks
-            WHERE 1=1
-        """
-
         connection: pyodbc.Connection = connector_service.get_database_connection()
-        params: List[Any] = []
 
-        if from_date and to_date:
-            query += " AND sk.data >= ? AND sk.data <= ?"
-            params.extend([from_date, to_date])
-
-        print(query, params)
-
-        data = connector_service.executer(
+        # Получаем список всех indeks из таблицы Stanowiska
+        stanowiska_query = """
+            SELECT indeks
+            FROM Stanowiska
+        """
+        stanowiska_data = connector_service.executer(
             connection=connection,
-            query=query,
-            params=params,
+            query=stanowiska_query
         )
-        print(data)
 
-        result_dict = {
-            "operationID": None,
-            "operationName": None,
-            "operations": []
-        }
+        result_list = []
 
-        for row in data:
-            operation = {
-                "indeks": row[0],
-                "date": row[1],
-                "orderID": row[4],
-                "orderName": row[5],
+        for row in stanowiska_data:
+            operation_id = row[0]
+            operations_query = """
+                SELECT
+                    sk.indeks AS id,
+                    sk.data AS startTime,
+                    st.indeks AS workplace,
+                    st.raport AS operationName,
+                    z.indeks AS orderID,
+                    z.zlecenie AS orderName
+                FROM Skany sk
+                JOIN Stanowiska st ON sk.stanowisko = st.indeks
+                JOIN Skany_vs_Zlecenia sz ON sk.indeks = sz.indeks
+                JOIN zlecenia z ON sz.indekszlecnia = z.indeks
+                WHERE st.indeks = ?
+            """
+
+            params = [operation_id]
+
+            if from_date and to_date:
+                operations_query += " AND sk.data >= ? AND sk.data <= ?"
+                params.extend([from_date, to_date])
+
+            operations_data = connector_service.executer(
+                connection=connection,
+                query=operations_query,
+                params=params
+            )
+
+            operations_list = []
+
+            for operation_row in operations_data:
+                operation = {
+                    "indeks": operation_row[0],
+                    "zlecenieID": operation_row[4],
+                    "zlecenie": operation_row[5].strip(),
+                    "data": operation_row[1]
+                }
+                operations_list.append(operation)
+
+            result = {
+                "OperationID": operation_id,
+                "OperationName": operations_data[0][3],
+                "operations": operations_list
             }
-            result_dict["operations"].append(operation)
-            result_dict["OperationID"] = row[2]
-            result_dict["OperationName"] = row[3]
 
-        return result_dict
+            result_list.append(result)
+
+        return result_list
 
 
 services = OrderServices()
