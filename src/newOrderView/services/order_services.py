33380777cc396b54454a9f81e33b9d7450b6ca89
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-31 14:44:45 +0300

    [ADD] order name and id

--- src/newOrderView/services/order_services.py
+++ src/newOrderView/services/order_services.py
@@ -12,9 +12,12 @@ class OrderServices:
                 sk.indeks AS id,
                 sk.data AS startTime,
                 st.indeks as workplace,
-                st.raport AS operationName
+                st.raport AS operationName,
+                z.indeks AS zlecenieIndex,
+                z.zlecenie AS zlecenie
             FROM skany sk
             JOIN Stanowiska st ON sk.stanowisko = st.indeks
+            JOIN zlecenia z ON sk.indeks = z.indekszlecnie
             WHERE 1=1
         """
 
@@ -33,20 +36,23 @@ class OrderServices:
             params=params,
         )
         result_dict = {
-            "OperationID": None,
-            "OperationName": None,
+            "operationID": None,
+            "operationName": None,
             "operations": []
         }
 
         for row in data:
             operation = {
                 "indeks": row[0],
-                "data": row[1]
+                "data": row[1],
+                "orderID": row[4],
+                "orderName": row[5],
             }
             result_dict["operations"].append(operation)
             result_dict["OperationID"] = row[2]
             result_dict["OperationName"] = row[3]
 
         return result_dict
+
 
 services = OrderServices()
