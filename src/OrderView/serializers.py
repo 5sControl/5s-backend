24c8f0fb9132354ba0bc9a7668f1ed28a875d5ee
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-31 12:46:01 +0300

    [EDIT] serializers

--- src/OrderView/serializers.py
+++ src/OrderView/serializers.py
@@ -20,11 +20,11 @@ class DatabaseConnectionSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    indeks = serializers.IntegerField()
     zlecenie = serializers.CharField(max_length=255)
     status = serializers.CharField(max_length=255)
-    deadline = serializers.DateTimeField()
-    entryDate = serializers.DateTimeField()
+    terminrealizacji = serializers.DateTimeField()
+    datawejscia = serializers.DateTimeField()
 
 
 class IndexStanowiskoSerializer(serializers.ModelSerializer):
