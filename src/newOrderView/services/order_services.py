7dd55c55216f3e043d8ea3f312e47e7c31f8f593
RasulMakhmudov <makhmudov_ra@vk.com>
2023-05-31 13:55:44 +0300

    [FIX] column name

--- src/newOrderView/services/order_services.py
+++ src/newOrderView/services/order_services.py
@@ -1,18 +1,20 @@
 from typing import List, Any, Tuple, Dict
 
+import pyodbc
+
 from src.MsSqlConnector.connector import connector as connector_service
 
 
 class OrderServices:
     def get_order(self, from_date: str, to_date: str):
         query: str = '''
-            SELECT sk.indeks, sk.data, st.OperationID, st.OperationName
+            SELECT sk.indeks, sk.data, st.indeks, st.raport
             FROM skany sk
             JOIN Stanowiska st ON sk.stanowisko = st.indeks
             WHERE 1=1
         '''
 
-        connection = connector_service.get_database_connection()
+        connection: pyodbc.Connection = connector_service.get_database_connection()
         params: List[Any] = []
 
         if from_date and to_date:
