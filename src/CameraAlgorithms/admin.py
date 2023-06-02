18a2a4fced5ff475c95fcb384bfe4c3fc88198f1
nnnLik <makhmudov_ra@vk.com>
2023-06-02 14:35:02 +0300

    [ADD] camera algorithms admin

--- src/CameraAlgorithms/admin.py
+++ src/CameraAlgorithms/admin.py
@@ -1,23 +1,23 @@
-# from django.contrib import admin
-# from .models.camera import Camera
-# from .models.algorithm import Algorithm, CameraAlgorithm, CameraAlgorithmLog
+from django.contrib import admin
+from .models.camera import Camera
+from .models.algorithm import Algorithm, CameraAlgorithm, CameraAlgorithmLog
 
 
-# @admin.register(Camera)
-# class CamerasAdmin(admin.ModelAdmin):
-#     list_filter = ("id",)
+@admin.register(Camera)
+class CamerasAdmin(admin.ModelAdmin):
+    list_filter = ("id",)
 
 
-# @admin.register(Algorithm)
-# class AlgorithmAdmin(admin.ModelAdmin):
-#     list_filter = ("id", "name")
+@admin.register(Algorithm)
+class AlgorithmAdmin(admin.ModelAdmin):
+    list_filter = ("id", "name")
 
 
-# @admin.register(CameraAlgorithm)
-# class CameraAlgorithmAdmin(admin.ModelAdmin):
-#     list_filter = ("id", "algorithm", "camera")
+@admin.register(CameraAlgorithm)
+class CameraAlgorithmAdmin(admin.ModelAdmin):
+    list_filter = ("id", "algorithm", "camera")
 
 
-# @admin.register(CameraAlgorithmLog)
-# class CameraAlgorithmLogAdmin(admin.ModelAdmin):
-#     list_filter = ("id",)
+@admin.register(CameraAlgorithmLog)
+class CameraAlgorithmLogAdmin(admin.ModelAdmin):
+    list_filter = ("id",)
