565f3a57f1da92c5739c0a71fc36456beee7aef9
anikeenko <dimskay1988@icloud.com>
2023-06-02 14:32:31 +0300

    ADD new fields

--- src/Inventory/serializers.py
+++ src/Inventory/serializers.py
@@ -2,11 +2,15 @@ from rest_framework import serializers
 
 from src.Inventory.models import Items
 
+from src.Suppliers.serializers import SuppliersSerializer
+
 
 class ItemsSerializer(serializers.ModelSerializer):
     """
     Items Serializer
     """
+
+    suppliers = SuppliersSerializer(many=False)
 
     class Meta:
         model = Items
@@ -19,5 +23,7 @@ class ItemsSerializer(serializers.ModelSerializer):
                   "date_created",
                   "date_updated",
                   "coords",
-                  "multi_row"
+                  "multi_row",
+                  "order_quantity",
+                  "suppliers"
                   ]
