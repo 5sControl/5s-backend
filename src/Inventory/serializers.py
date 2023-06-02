2b4095482d031543422fbd157467c195dd1cb281
anikeenko <dimskay1988@icloud.com>
2023-06-02 15:03:02 +0300

    EDIT serializers

--- src/Inventory/serializers.py
+++ src/Inventory/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from src.Inventory.models import Items
+from src.Suppliers.models import Suppliers
 
 from src.Suppliers.serializers import SuppliersSerializer
 
@@ -14,16 +15,11 @@ class ItemsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Items
-        fields = ["id",
-                  "name",
-                  "status",
-                  "current_stock_level",
-                  "low_stock_level",
-                  "camera",
-                  "date_created",
-                  "date_updated",
-                  "coords",
-                  "multi_row",
-                  "order_quantity",
-                  "suppliers"
-                  ]
+        fields = ["id", "name", "status", "current_stock_level", "low_stock_level", "camera", "date_created",
+                  "date_updated", "coords", "multi_row", "order_quantity", "suppliers"]
+
+    def create(self, validated_data):
+        suppliers_data = validated_data.pop('suppliers')
+        item = Items.objects.create(**validated_data)
+        Suppliers.objects.create(item=item, **suppliers_data)
+        return item
--- src/Suppliers/serializers.py
+++ src/Suppliers/serializers.py
@@ -3,6 +3,10 @@ from src.Suppliers.models import Suppliers
 
 
 class SuppliersSerializer(serializers.ModelSerializer):
+
+    name_company = serializers.CharField(allow_blank=True)
+    website = serializers.CharField(allow_blank=True)
+
     class Meta:
         model = Suppliers
         fields = '__all__'
