5b77a19e369cb5c96c4f4f0798064242bd483588
nnnLik <makhmudov_ra@vk.com>
2023-06-07 10:16:35 +0300

    [EDIT] utils

--- src/OrderView/utils.py
+++ src/OrderView/utils.py
@@ -5,19 +5,19 @@ from src.Core.const import SERVER_URL
 
 
 def get_skany_video_info(time, camera_ip):
-    response = {
+    request_data = {
         "camera_ip": camera_ip,
         "time": time,
     }
     try:
-        request = requests.post(
+        response = requests.post(
             url=f"{SERVER_URL}:3456/is_video_available/",
-            json=response,
+            json=request_data,
         )
     except Exception:
         return {"status": False}
 
-    result = request.json()
+    result = response.json()
     result["camera_ip"] = camera_ip
 
     return result
