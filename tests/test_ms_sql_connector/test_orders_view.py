29f6e2a09ec25a92098b22a4d94d0cc58151cd2c
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-13 12:02:36 +0300

    [FIX] orders view tests #264

--- tests/test_ms_sql_connector/test_orders_view.py
+++ tests/test_ms_sql_connector/test_orders_view.py
@@ -26,7 +26,7 @@ class CreateDatabaseConnectionAPIViewTestCase(APITestCase):
         for fixture in cls.fixtures:
             call_command("loaddata", fixture, verbosity=0)
 
-    def test_get_orders_view_orders(self):
+    def test_get_orders_view_orders_1(self):
         url = "/api/order/all-orders/"
 
         self.client.force_authenticate(user=self.user)
@@ -36,8 +36,8 @@ class CreateDatabaseConnectionAPIViewTestCase(APITestCase):
         self.assertTrue("count" in response.data)
         self.assertTrue("results" in response.data)
 
-    def test_get_orders_by_id_1(self):
-        url = "/api/order/by-order/PRW199234"
+    def test_get_orders_view_orders_2(self):
+        url = "/api/order/all-orders/?search=PRW199234"
 
         self.client.force_authenticate(user=self.user)
         response = self.client.get(url, follow=True)
@@ -58,7 +58,7 @@ class CreateDatabaseConnectionAPIViewTestCase(APITestCase):
         self.assertEqual(response.data["results"][0]["datawejscia"], "23.02.2023 14:18:00")
 
 
-    def test_get_orders_by_id_2(self):
+    def test_get_orders_by_id_1(self):
         url = "/api/order/by-order/G59811"
 
         self.client.force_authenticate(user=self.user)
@@ -67,9 +67,14 @@ class CreateDatabaseConnectionAPIViewTestCase(APITestCase):
         data = response.data
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(data, G59811)
+        self.assertEqual(len(data[0]["products"]), len(G59811[0]["products"]))
+        self.assertEqual(data[0]["status"], G59811[0]["status"])
+        self.assertEqual(data[0]["indeks"], G59811[0]["indeks"])
+        self.assertEqual(data[0]["klient"], G59811[0]["klient"])
 
-    def test_get_orders_by_id_3(self):
+
+
+    def test_get_orders_by_id_4(self):
         for order in ORDERS:
             url = f"/api/order/by-order/{order}"
 
