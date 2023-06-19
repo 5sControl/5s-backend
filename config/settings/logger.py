e2458d755bd5810e01d422cdc354251f127e84c0
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-19 12:47:35 +0300

    [FIX] logger

--- config/settings/logger.py
+++ config/settings/logger.py
@@ -1,23 +1,23 @@
-import logging
-from django.utils.log import DEFAULT_LOGGING
-
-# Получение конфигурации логгера по умолчанию
-LOGGING = DEFAULT_LOGGING
-
-# Обновление форматирования логов
-LOGGING["formatters"]["verbose"] = {
-    "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": "app.log",
+            "maxBytes": 1024 * 1024,
+            "backupCount": 5,
+            "formatter": "verbose",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "INFO",
+            "propagate": True,
+        },
+    },
+    "formatters": {
+        "verbose": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
+    },
 }
-
-# Добавление обработчика "file"
-LOGGING["handlers"]["file"] = {
-    "class": "logging.FileHandler",
-    "filename": "log/logs.log",
-    "formatter": "verbose",
-}
-
-# Добавление обработчика "console" (если требуется)
-LOGGING["handlers"]["console"]["formatter"] = "verbose"
-
-# Применение настроек логгера
-logging.config.dictConfig(LOGGING)
