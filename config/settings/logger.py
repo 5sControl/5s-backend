1eba613426dee75e4f612b292b76dd09bec89cb7
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-19 11:22:51 +0300

    [ADD] time to logger file

--- config/settings/logger.py
+++ config/settings/logger.py
@@ -1,13 +1,22 @@
+import logging
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        },
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            "formatter": "standard",
         },
         "file": {
             "class": "logging.FileHandler",
             "filename": "log/logs.log",
+            "formatter": "standard",
         },
     },
     "loggers": {
@@ -17,3 +26,5 @@ LOGGING = {
         },
     },
 }
+
+logging.config.dictConfig(LOGGING)
