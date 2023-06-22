a4fe35c6728f71d1694e5724673e574eb7d6d148
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-22 11:45:56 +0300

    [ADD] name

--- src/newOrderView/models.py
+++ src/newOrderView/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class FiltrationOperationsTypeID(models.Model):
     operation_type_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=150, blank=True)
     is_active = models.BooleanField(default=True)
 
     class Meta:
