85d0b4d200c5685108744b83e2f9a7c3d943e886
anikeenko <dimskay1988@icloud.com>
2023-06-28 10:42:09 +0300

    EDIT restart machine_control

--- src/Core/management/commands/startprocess.py
+++ src/Core/management/commands/startprocess.py
@@ -41,16 +41,15 @@ class Command(BaseCommand):
 
             if camera_algorithm.algorithm.name == "machine_control":
                 all_zones = camera_algorithm.zones
-                all_cords = []
+                coords = {}
                 for zone_id in all_zones:
                     zone_camera = ZoneCameras.objects.get(id=zone_id["id"], camera=camera_obj)
                     coords = zone_camera.coords
-                    coords[0]["zoneId"] = zone_camera.id
-                    coords[0]["zoneName"] = zone_camera.name
+                    coords["zoneId"] = zone_camera.id
+                    coords["zoneName"] = zone_camera.name
 
-                    all_cords.append(coords)
 
-                request["extra"] = {"coords": all_cords[0]}
+                request["extra"] = {"coords": coords}
 
             if camera_algorithm.algorithm.name == "min_max_control":
                 algorithm_items = Items.objects.filter(camera=camera_obj)
