79a987ca2d62c53da8a63738c927804850d3a7a8
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-29 14:39:51 +0300

    [EDIT] skip if operation control

--- src/CameraAlgorithms/services/cameraalgorithm.py
+++ src/CameraAlgorithms/services/cameraalgorithm.py
@@ -221,6 +221,8 @@ def create_camera_algorithms(
                 if index_operations_obj.type_operation != operation_control_id:
                     index_operations_obj.type_operation = operation_control_id
                     index_operations_obj.save()
+
+                    continue
                 elif not compare_zones(algorithm_obj, camera_obj, zones):
                     pid: int = camera_algo_obj.get(
                         algorithm=algorithm_obj, camera=camera_obj
