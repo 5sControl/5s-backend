c2787d2be77fc8cfe5ce8403d539fc0779176861
RasulMakhmudov <makhmudov_ra@vk.com>
2023-06-30 16:06:51 +0300

    [ADD] db connection

---
+++ src/DatabaseConnections/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import DatabaseConnection
+
+
+@admin.register(DatabaseConnection)
+class DatabaseConnectionAdmin(admin.ModelAdmin):
+    list_display = (
+        "database_type",
+        "server",
+        "database",
+        "username",
+        "password",
+    )
---
+++ src/DatabaseConnections/services.py
@@ -0,0 +1,34 @@
+from typing import Any, Dict, List
+
+from src.DatabaseConnections.models import DatabaseConnection
+from src.DatabaseConnections.repositories.ms_repository import MsSqlServerRepository
+
+
+class DatabaseConnectionManager:
+    def create_connection(self, credentials: Dict[str, Any], db_type: str) -> bool:
+        server = credentials["server"]
+        database = credentials["database"]
+        username = credentials["username"]
+        password = credentials["password"]
+        port = credentials["port"]
+
+        if DatabaseConnection.objects.filter(
+            server=server, database=database, username=username, port=port
+        ).exists():
+            return False
+
+        if db_type == "mssql":
+            if MsSqlServerRepository().is_stable(server, database, username, password, port):
+                return True
+
+        return False
+
+    def get_connections(self) -> List[Dict[str, Any]]:
+        return DatabaseConnection.objects.all()
+
+    def delete_connection(self, connection_id: int) -> bool:
+        try:
+            DatabaseConnection.objects.get(id=connection_id).delete()
+            return True
+        except DatabaseConnection.DoesNotExist:
+            return False
