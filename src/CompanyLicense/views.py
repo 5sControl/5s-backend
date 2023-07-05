23d10a5ff710496667bc5b3985b566ceef23ecf8
Artsiom <artsiomzhuchkou2401@gmail.com>
2023-07-05 13:36:54 +0300

    chore: new version

--- src/CompanyLicense/views.py
+++ src/CompanyLicense/views.py
@@ -82,8 +82,8 @@ def version(request):
     versions = []
     versions = versions + [{
         "name": "5S Control version",
-        "version": "v0.4.3",
-        "date": "06.28.2023",
+        "version": "v0.4.4",
+        "date": "07.05.2023",
         "description": ""
     }]
 
