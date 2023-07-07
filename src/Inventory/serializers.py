c92f7f1576d135486f51c33c251a8431f22abc20
RasulMakhmudov <makhmudov_ra@vk.com>
2023-07-07 15:16:22 +0300

    [ADD] counting bottles

--- src/Inventory/serializers.py
+++ src/Inventory/serializers.py
@@ -8,6 +8,22 @@ class ItemsSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Items
-        fields = ['id', 'name', 'status', 'current_stock_level', 'low_stock_level', 'camera',
-                  'date_created', 'date_updated', 'coords', 'prev_status', 'multi_row',
-                  'order_quantity', 'suppliers', 'to_emails', 'copy_emails', 'subject']
+        fields = [
+            "id",
+            "name",
+            "object_type",
+            "status",
+            "current_stock_level",
+            "low_stock_level",
+            "camera",
+            "date_created",
+            "date_updated",
+            "coords",
+            "prev_status",
+            "multi_row",
+            "order_quantity",
+            "suppliers",
+            "to_emails",
+            "copy_emails",
+            "subject",
+        ]
