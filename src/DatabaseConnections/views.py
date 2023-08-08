899e02fbaa57ad6b2d879522255505786cdb725f
nnnLik <rslmakhmudov@gmail.com>
2023-08-08 15:54:08 +0300

    [FIX] get connection

--- src/DatabaseConnections/views.py
+++ src/DatabaseConnections/views.py
@@ -12,11 +12,15 @@ class UpdateActiveResourceView(generics.UpdateAPIView):
         connector_type: str = request.data.get("type")
 
         if connector_type == "database":
-            ConnectionInfo.objects.filter(type="api").update(is_active=False)
-            ConnectionInfo.objects.filter(type="database").update(is_active=True)
+            connector: ConnectionInfo = ConnectionInfo.objects.filter(type="database")
+            if not connector.exists():
+                return Response({"detail": "Connection doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
+            connector.update(is_active=True)
         elif connector_type == "api":
-            ConnectionInfo.objects.filter(type="database").update(is_active=False)
-            ConnectionInfo.objects.filter(type="api").update(is_active=True)
+            connector: ConnectionInfo = ConnectionInfo.objects.filter(type="api")
+            if not connector.exists():
+                return Response({"detail": "Connection doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
+            connector.update(is_active=True)
         else:
             return Response(
                 {"detail": "Either 'api' or 'database' parameter must be provided."},
