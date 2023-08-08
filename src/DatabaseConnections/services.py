b05067ee25d1b1846ac6e75062abf26151faffbf
nnnLik <rslmakhmudov@gmail.com>
2023-08-08 11:04:52 +0300

    [EDIT] managers

--- src/DatabaseConnections/services.py
+++ src/DatabaseConnections/services.py
@@ -1,42 +1,66 @@
+import logging
 from typing import Any, Dict, List
 
-from src.DatabaseConnections.models import DatabaseConnection
+from src.DatabaseConnections.models import ConnectionInfo
 from src.DatabaseConnections.repositories.ms_repository import WinHRepository
-from src.DatabaseConnections.repositories.odoo import OdooRepository
+
+logger = logging.getLogger(__name__)
 
 
-class DatabaseConnectionManager:
-    def create_connection(self, credentials: Dict[str, Any]) -> bool:
-        database_type = credentials["database_type"]
-        server = credentials["server"]
-        database = credentials["database"]
-        username = credentials["username"]
-        password = credentials["password"]
-        port = credentials["port"]
+class CreateConnectionManager:
+    def create_api_connection(self, credentials: Dict[str, Any]) -> bool:
+        host = credentials["host"]
 
-        if DatabaseConnection.objects.filter(
-            server=server, database=database, username=username, port=port
-        ).exists():
+        connection, created = ConnectionInfo.objects.update_or_create(
+            type="api",
+            defaults={"is_active": True, "host": host},
+        )
+
+        return created or connection
+
+    def create_database_connection(
+        self, credentials: Dict[str, Any]
+    ) -> bool:
+        dbms: str = credentials["dbms"]
+        server: str = credentials["server"]
+        database: str = credentials["database"]
+        username: str = credentials["username"]
+        password: str = credentials["password"]
+        port: int = credentials["port"]
+
+        if dbms == "mssql":
+            is_stable: bool = WinHRepository().is_stable(
+                server, database, username, password, port
+            )
+            if not is_stable:
+                logger.critical("Database connection is not stable")
+                return False
+
+        elif dbms == "postgres":
+            logger.critical("Database connection is not implemented")
+            raise ValueError("Not implemented")
+        else:
+            logger.critical("Type of dbms is not supported")
             return False
 
-        if database_type == "WBNet":
-            if WinHRepository().is_stable(
-                server, database, username, password, port
-            ):
-                return True
-        elif database_type == "Odoo":
-            if OdooRepository().is_stable(
-                server, database, username, password, port
-            ):
-                return True
-        return False
+        connection, created = ConnectionInfo.objects.update_or_create(
+            type="database",
+            defaults={
+                "is_active": True,
+                "dbms": dbms,
+                "server": server,
+                "database": database,
+                "username": username,
+                "password": password,
+                "port": port,
+            },
+        )
 
-    def get_connections(self) -> List[Dict[str, Any]]:
-        return DatabaseConnection.objects.all()
+        return created or connection
 
     def delete_connection(self, connection_id: int) -> bool:
         try:
-            DatabaseConnection.objects.get(id=connection_id).delete()
+            ConnectionInfo.objects.get(id=connection_id).delete()
             return True
-        except DatabaseConnection.DoesNotExist:
+        except ConnectionInfo.DoesNotExist:
             return False
