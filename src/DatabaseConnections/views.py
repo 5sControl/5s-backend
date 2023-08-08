c4ce68d2d8a7070e920ae2562a7a9a80d53b6cfb
nnnLik <rslmakhmudov@gmail.com>
2023-08-08 16:03:20 +0300

    [FIX] multi activation

--- src/DatabaseConnections/views.py
+++ src/DatabaseConnections/views.py
@@ -12,19 +12,19 @@ class UpdateActiveResourceView(generics.UpdateAPIView):
         connector_type: str = request.data.get("type")
 
         if connector_type == "database":
-            connector: ConnectionInfo = ConnectionInfo.objects.filter(type="database")
-            if not connector.exists():
-                return Response({"detail": "Connection doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
-            connector.update(is_active=True)
+            connector_to_activate: ConnectionInfo = ConnectionInfo.objects.filter(type="database")
+            connector_to_deactivate: ConnectionInfo = ConnectionInfo.objects.filter(type="api")
         elif connector_type == "api":
-            connector: ConnectionInfo = ConnectionInfo.objects.filter(type="api")
-            if not connector.exists():
-                return Response({"detail": "Connection doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
-            connector.update(is_active=True)
+            connector_to_activate: ConnectionInfo = ConnectionInfo.objects.filter(type="api")
+            connector_to_deactivate: ConnectionInfo = ConnectionInfo.objects.filter(type="database")
         else:
             return Response(
                 {"detail": "Either 'api' or 'database' parameter must be provided."},
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
+        if not connector_to_activate.exists():
+            return Response({"detail": "Connection doesn't exist"}, status=status.HTTP_400_BAD_REQUEST)
+        connector_to_deactivate.update(is_active=False)
+        connector_to_activate.update(is_active=True)
         return Response({"detail": "Active resource updated successfully."})
