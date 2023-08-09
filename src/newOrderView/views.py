e99ffdb3b35dedee2f1be3703a87dccc93530474
nnnLik <rslmakhmudov@gmail.com>
2023-08-09 10:50:25 +0300

    [ADD] odoo implementation

--- src/newOrderView/views.py
+++ src/newOrderView/views.py
@@ -4,14 +4,17 @@ from django.http import JsonResponse
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
+from django.shortcuts import get_object_or_404
 
 from rest_framework import generics, status
 from rest_framework.response import Response
 
 from src.Core.paginators import NoPagination
+from src.DatabaseConnections.models import ConnectionInfo
 from src.DatabaseConnections.utils import check_database_connection
 from src.newOrderView.models import FiltrationOperationsTypeID
 from src.newOrderView.serializers import FilterOperationsTypeIDSerializer
+from src.newOrderView.services.connector import connector_services 
 
 from .services import OperationServices, OrderServices
 from .utils import generate_hash
@@ -37,14 +40,18 @@ class GetOperation(generics.GenericAPIView):
         )
         response = cache.get(key)
 
-        if response is None:
+        connector = get_object_or_404(ConnectionInfo, is_active=True).type
+
+        if connector == "database" and response is None:
             response: List[Dict[str, Any]] = OperationServices.get_operations(
                 from_date, to_date, operation_type_ids
             )
             cache.set(key, response, timeout=120)
-
-        if response:
+        elif connector == "api" and response is None:
+            response: List[Dict[str, Any]] = connector_services.get_operations(from_date, to_date)
+        else:
             return JsonResponse(data=response, status=status.HTTP_200_OK, safe=False)
+
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -68,14 +75,18 @@ class GetMachine(generics.GenericAPIView):
         )
         response = cache.get(key)
 
-        if response is None:
+        connector = get_object_or_404(ConnectionInfo, is_active=True).type 
+
+        if connector == "database" and response is None:
             response: List[Dict[str, Any]] = OperationServices.get_machine(
                 from_date, to_date, operation_type_ids
             )
             cache.set(key, response, timeout=60)
-
-        if response:
+        elif connector == "api" and response is None:
+            response: List[Dict[str, Any]] = connector_services.get_orders(from_date, to_date)
+        else:
             return JsonResponse(data=response, status=status.HTTP_200_OK, safe=False)
+
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
