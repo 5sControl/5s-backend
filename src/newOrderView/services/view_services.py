d691ce2cee70b9122af01b74ed3a709f42a500f1
nnnLik <rslmakhmudov@gmail.com>
2023-08-10 15:03:57 +0300

    [FIX] if api request server

--- src/newOrderView/services/view_services.py
+++ src/newOrderView/services/view_services.py
@@ -17,13 +17,18 @@ def get_response(cache_key: str, from_date: str, to_date: str, operation_type_id
         if type == "operation":
             response: List[Dict[str, Any]] = connector_services.get_operations(from_date, to_date)
         elif type == "orders":
+            response: List[Dict[str, Any]] = connector_services.get_orders(from_date, to_date)
             response: List[Dict[str, Any]] = OrderServices.get_order(from_date, to_date, operation_type_ids)
     elif connector == "database":
         response = cache.get(cache_key)
         if response is None:
-            response: List[Dict[str, Any]] = OperationServices.get_operations(
-                from_date, to_date, operation_type_ids
-            )
+            if type == "operation":
+                response: List[Dict[str, Any]] = OperationServices.get_operations(
+                    from_date, to_date, operation_type_ids
+                )
+            elif type == "orders":
+                response: List[Dict[str, Any]] = OrderServices.get_order(from_date, to_date, operation_type_ids)
+
             cache.set(cache_key, response, timeout=120)
 
     return response
