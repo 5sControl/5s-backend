98f18ef93d45d62eabda2ca4edea5cd35821254a
nnnLik <rslmakhmudov@gmail.com>
2023-08-10 14:38:53 +0300

    [EDIT] response handler

--- src/newOrderView/views.py
+++ src/newOrderView/views.py
@@ -4,20 +4,17 @@ from django.http import JsonResponse
 from django.core.cache import cache
 from django.views.decorators.cache import cache_page
 from django.utils.decorators import method_decorator
-from django.shortcuts import get_object_or_404
 
 from rest_framework import generics, status
 from rest_framework.response import Response
 
 from src.Core.paginators import NoPagination
-from src.DatabaseConnections.models import ConnectionInfo
 from src.DatabaseConnections.utils import check_database_connection
 from src.newOrderView.models import FiltrationOperationsTypeID
 from src.newOrderView.serializers import FilterOperationsTypeIDSerializer
-from src.newOrderView.services.connector import connector_services
 
-from .services import OperationServices, OrderServices
-from .utils import get_cache_data, get_date_interval
+from .services import OperationServices
+from .utils import get_cache_data, get_date_interval, get_response
 
 
 class GetOperation(generics.GenericAPIView):
@@ -28,18 +25,7 @@ class GetOperation(generics.GenericAPIView):
         from_date, to_date = get_date_interval(request)
         cache_key, operation_type_ids = get_cache_data(from_date, to_date)
 
-        response = cache.get(cache_key)
-
-        connector = get_object_or_404(ConnectionInfo, is_active=True).type
-
-        if connector == "api":
-            print("API")
-            response: List[Dict[str, Any]] = connector_services.get_operations(from_date, to_date)
-        elif connector == "database" and response is None:
-            response: List[Dict[str, Any]] = OperationServices.get_operations(
-                from_date, to_date, operation_type_ids
-            )
-            cache.set(cache_key, response, timeout=120)
+        response: List[Dict[str, Any]] = get_response(cache_key, from_date, to_date, operation_type_ids, "operation")
 
         return JsonResponse(data=response, status=status.HTTP_200_OK, safe=False)
 
@@ -52,18 +38,7 @@ class GetOrders(generics.GenericAPIView):
         from_date, to_date = get_date_interval(request)
         cache_key, operation_type_ids = get_cache_data(from_date, to_date)
 
-        response = cache.get(cache_key)
-
-        connector = get_object_or_404(ConnectionInfo, is_active=True).type
-
-        if connector == "api" and response is None:
-            print("API")
-            response: List[Dict[str, Any]] = connector_services.get_orders(from_date, to_date)
-        elif connector == "database" and response is None:
-            response: List[Dict[str, Any]] = OrderServices.get_order(
-                from_date, to_date, operation_type_ids
-            )
-            cache.set(cache_key, response, timeout=120)
+        response: List[Dict[str, Any]] = get_response(cache_key, from_date, to_date, operation_type_ids, "orders")
 
         return JsonResponse(data=response, status=status.HTTP_200_OK, safe=False)
 
