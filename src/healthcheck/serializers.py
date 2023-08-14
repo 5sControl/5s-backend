084dff3cba57343253601f4a6c6e129ed11ac444
nnnLik <rslmakhmudov@gmail.com>
2023-08-14 12:30:37 +0300

    [FIX] healthcheck app

--- src/healthcheck/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-
-class CpuLoadSerializer(serializers.Serializer):
-    cpu_load = serializers.FloatField(default=0)
--- src/healthcheck/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from .views import GetHealthCheckApiView
-
-urlpatterns = [
-    path("", GetHealthCheckApiView.as_view(), name="health_check"),
-]
