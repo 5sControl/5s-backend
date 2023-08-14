a781e383d08b859d67a40de6780c066a7ed15093
nnnLik <rslmakhmudov@gmail.com>
2023-08-14 12:23:05 +0300

    [EDIT] refactor

--- src/healthcheck/utils.py
+++ src/healthcheck/utils.py
@@ -1,14 +1,14 @@
-from typing import Dict, List, Any
+from typing import Dict, List, Any, Union
 import psutil
 
 from src.CameraAlgorithms.models.camera import Camera
 
 
-def get_cpu_load_percentage():
+def get_cpu_load_percentage() -> float:
     return psutil.cpu_percent(interval=5)
 
 
-def get_devices():
+def get_devices() -> Dict[str, Union[int, List[str]]]:
     camera: List[str] = Camera.objects.all().values_list("id")
     count: int = len(camera)
     return {
@@ -17,11 +17,11 @@ def get_devices():
     }
 
 
-def get_nvidia_gpu():
+def get_nvidia_gpu() -> bool:
     return False
 
 
-def get_healthckeck_data():
+def get_healthckeck_data() -> Dict[str, Any]:
     cpu_load: float = get_cpu_load_percentage()
     devices: Dict[str, Any] = get_devices()
     nvidia_gpu: bool = get_nvidia_gpu()
