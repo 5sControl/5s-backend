90f4f0cd72c46e3de75f96d9781df2334d012036
slanx <andraffal@gmail.com>
2023-08-23 13:20:39 +0300

    chore:update version

--- src/CompanyLicense/views.py
+++ src/CompanyLicense/views.py
@@ -82,8 +82,8 @@ def version(request):
     versions = []
     versions = versions + [{
         "name": "5S Control version",
-        "version": "v0.5.0",
-        "date": "16.08.2023",
+        "version": "v0.5.1",
+        "date": "08.23.2023",
         "description": ""
     }]
 
