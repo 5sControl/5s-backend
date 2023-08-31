1c137687015714d0b4762bc001a4ace2397b8179
nnnLik <rslmakhmudov@gmail.com>
2023-08-31 12:22:07 +0300

    [EDIT] optimize system messages

--- src/Core/models.py
+++ src/Core/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class SystemMessage(models.Model):
+    id = models.AutoField(primary_key=True, db_index=True)
     title = models.CharField(max_length=150)
     content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
