c01f0a03c4ee4519a5de256db8b38bf2bd51fed0
anikeenko <dimskay1988@icloud.com>
2023-09-08 15:55:05 +0300

    EDIT restart

--- src/Core/management/commands/startprocess.py
+++ src/Core/management/commands/startprocess.py
@@ -116,9 +116,9 @@ class Command(BaseCommand):
             except SenderError as e:
                 logger.critical(f"Yolo server is not available. Details: {e}")
             except InvalidResponseError as e:
+                print(f"Delete records {camera_algorithm}")
                 records_to_delete = CameraAlgorithm.objects.filter(algorithm=request.get('algorithm'), camera=camera_algorithm)
                 records_to_delete.delete()
-                print(f"Delete records {camera_algorithm}")
                 logger.critical(
                     f"Yolo can't start algorithm {algorithm_obj.name} on camera {camera_obj.id}. Details: {e}"
                 )
