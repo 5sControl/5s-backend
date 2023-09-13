e84a1441ee2a2cfb30a774eaa919cf041a7ac3c8
anikeenko <dimskay1988@icloud.com>
2023-09-13 13:02:27 +0300

    EDIT views update date and version

--- src/CompanyLicense/views.py
+++ src/CompanyLicense/views.py
@@ -83,7 +83,7 @@ def version(request):
     versions = versions + [{
         "name": "5S Control version",
         "version": "v0.5.2",
-        "date": "08.31.2023",
+        "date": "13.09.2023",
         "description": ""
     }]
 
