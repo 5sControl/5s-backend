51ffa7ae845422028acae74f9adc8e1cd0aceb13
anikeenko <dimskay1988@icloud.com>
2023-09-18 10:57:49 +0300

    EDIT models camera

--- src/CameraAlgorithms/models/camera.py
+++ src/CameraAlgorithms/models/camera.py
@@ -1,8 +1,6 @@
 import logging
 
 from django.db import models
-from django.contrib.auth.hashers import make_password
-from django.contrib.auth.hashers import check_password
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
 
@@ -30,12 +28,6 @@ class Camera(models.Model):
 
     def __str__(self):
         return self.id
-
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-
-    def check_password(self, raw_password):
-        return check_password(raw_password, self.password)
 
     def save(self, *args, **kwargs):
         if not self.name:
