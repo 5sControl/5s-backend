3f0b2ed86f40796302d971d36312545c90cdc62d
anikeenko <dimskay1988@icloud.com>
2023-09-25 16:18:56 +0300

    EDIT type object

--- src/CameraAlgorithms/views.py
+++ src/CameraAlgorithms/views.py
@@ -157,6 +157,6 @@ class UploadAlgorithmView(APIView):
     def post(self, request, id_algorithm: int, format=None):
 
         algorithm = Algorithm.objects.get(id=id_algorithm)
-        uploading_algorithm.apply_async(algorithm)
+        uploading_algorithm.apply_async((algorithm,))
 
         return Response({"message": "File upload started"}, status=status.HTTP_202_ACCEPTED)
