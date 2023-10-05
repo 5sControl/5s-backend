a7481a7d1b5efa61d1e1fa987abc18d2ff4da37e
anikeenko <dimskay1988@icloud.com>
2023-10-05 17:37:15 +0300

    EDIT process_item_status

--- src/Inventory/service.py
+++ src/Inventory/service.py
@@ -12,136 +12,119 @@ logger = logging.getLogger(__name__)
 
 
 def process_item_status(data):
-    """Updates the status of an item and adds it to the result."""
+    """Updates the item status and adds the status to the extra"""
+
     result = []
-    items_data = data[0].get("items")
-    for item_data in items_data:
-        item_id = item_data["itemId"]
+    data = data[0].get("items")
+    for item_data in data:
         count = item_data["count"]
+        red_line = item_data["low_stock_level"]
+        data_item = Items.objects.filter(id=item_data["itemId"]).values(
+            "current_stock_level", "low_stock_level", "status", "object_type"
+        )
+        min_item = data_item[0]["low_stock_level"]
         image_path = item_data["image_item"]
-        low_stock_level = item_data["low_stock_level"]
+        level_previous_status = data_item[0]["status"]
+        item = Items.objects.filter(id=item_data["itemId"]).first()
 
-        item = Items.objects.filter(id=item_id).first()
+        item_serializer = ItemsSerializer(item)
+        serialized_item = item_serializer.data
 
-        if item:
-            process_item(item, count, image_path, low_stock_level)
-            result.append(item_data)
+        if data_item[0]["object_type"] == "red line":
+            if red_line:
+                item_status = "Low stock level"
+                if min_item == 0:
+                    item.current_stock_level = 0
+                    item_data["count"] = 0
+                else:
+                    item.current_stock_level = min_item - 1
+                    item_data["count"] = min_item - 1
 
-    return result
+                if item.prev_status == "In stock":
+                    # send_notification
+                    try:
+                        item.prev_status = None
+                        send_notification_email.apply_async(
+                            args=[serialized_item, count, image_path, item_status],
+                            countdown=0,
+                        )
+                    except Exception as e:
+                        print(f"Email notification errors: {e}")
 
+                    # send_notification suppliers
+                    try:
+                        item.prev_status = None
+                        send_email_to_suppliers.apply_async(
+                            args=[serialized_item, image_path]
+                        )
+                    except Exception as e:
+                        print(f"Email notification errors: {e}")
 
-def process_item(item, count, image_path, low_stock_level):
-    """Handles item status."""
-    result = []
-
-    item_serializer = ItemsSerializer(item)
-    serialized_item = item_serializer.data
-
-    if serialized_item["object_type"] == "red line":
-        if low_stock_level:
-            item_status = "Low stock level"
-            if serialized_item["low_stock_level"] == 0:
-                item.current_stock_level = 0
-                count = 0
-            else:
-                item.current_stock_level = serialized_item["low_stock_level"] - 1
-                count = serialized_item["low_stock_level"] - 1
-
-            if item.prev_status == "In stock":
-                # send_notification Low stock level
-                try:
-                    item.prev_status = None
-                    send_notification_email.apply_async(
-                        args=[serialized_item, count, image_path, item_status],
-                        countdown=0,
-                    )
-                except Exception as e:
-                    print(f"Email notification errors: {e}")
-
-                # send_notification suppliers Low stock level
-                try:
-                    item.prev_status = None
-                    send_email_to_suppliers.apply_async(
-                        args=[serialized_item, image_path]
-                    )
-                except Exception as e:
-                    print(f"Email notification errors: {e}")
-
-            item.prev_status = "Low stock level"
-
-        else:
-            item_status = "In stock"
-            item.current_stock_level = serialized_item["low_stock_level"] + 1
-            count = serialized_item["low_stock_level"] + 1
-            item.prev_status = "In stock"
-
-    else:
-        if count == 0:
-            item_status = "Out of stock"
-            if item.prev_status == "Low stock level":
-                item.prev_status = None
-                # send_notification Out of stock
-                try:
-                    item.prev_status = None
-                    send_notification_email.apply_async(
-                        args=[serialized_item, count, image_path, item_status],
-                        countdown=0,
-                    )
-                except Exception as e:
-                    print(f"Email notification errors: {e}")
-
-            else:
-                if item.prev_status is not None:
-                    item.prev_status = item.status
-
-        elif 0 < count <= serialized_item["low_stock_level"]:
-            item_status = "Low stock level"
-            if serialized_item["status"] == "In stock":
                 item.prev_status = "Low stock level"
 
-                # send_notification suppliers Low stock level
-                try:
-                    item.prev_status = None
-                    send_email_to_suppliers.apply_async(
-                        args=[serialized_item, image_path]
-                    )
-                except Exception as e:
-                    print(f"Email notification errors: {e}")
-
-                # send_notification Low stock level
-                try:
-                    item.prev_status = None
-                    send_notification_email.apply_async(
-                        args=[serialized_item, count, image_path, item_status],
-                        countdown=0,
-                    )
-                except Exception as e:
-                    print(f"Email notification errors: {e}")
-            elif item.prev_status is not None:
-                item.prev_status = serialized_item["status"]
+            else:
+                item_status = "In stock"
+                item.current_stock_level = min_item + 1
+                item_data["count"] = min_item + 1
+                item.prev_status = "In stock"
 
         else:
-            item_status = "In stock"
-            if item.prev_status is None:
-                item.prev_status = "In stock"
+            if count == 0:
+                item_status = "Out of stock"
+                if item.prev_status == "Low stock level":
+                    item.prev_status = None
+
+                else:
+                    if item.prev_status != None:
+                        item.prev_status = item.status
+
+            elif count > 0 and count <= min_item:
+                item_status = "Low stock level"
+                if level_previous_status == "In stock":
+                    item.prev_status = "Low stock level"
+
+                    # send_notification suppliers
+                    try:
+                        item.prev_status = None
+                        send_email_to_suppliers.apply_async(
+                            args=[serialized_item, image_path]
+                        )
+                    except Exception as e:
+                        print(f"Email notification errors: {e}")
+
+                    # send_notification
+                    try:
+                        item.prev_status = None
+                        send_notification_email.apply_async(
+                            args=[serialized_item, count, image_path, item_status],
+                            countdown=0,
+                        )
+                    except Exception as e:
+                        print(f"Email notification errors: {e}")
+                elif item.prev_status != None:
+                    item.prev_status = item.status
+
             else:
-                item.prev_status = serialized_item["status"]
-        item.current_stock_level = count
-        serialized_item["low_stock_level"] = serialized_item["low_stock_level"]
+                item_status = "In stock"
+                if item.prev_status == None:
+                    item.prev_status = "In stock"
+                else:
+                    item.prev_status = item.status
+            item.current_stock_level = count
+            item_data["low_stock_level"] = min_item
 
-    print(
-        f"item_id=={item.id}, item_name=={item.name}, item_status {item_status}, "
-        f"low_stock_level == {low_stock_level}, item_type == {serialized_item['object_type']}"
-    )
+        print(
+            f"item_id=={item.id}, item_name=={item.name}, item_status {item_status}, "
+            f"red_line == {red_line}, item_type == {data_item[0]['object_type']}"
+        )
 
-    item.status = item_status
-    item.save()
+        item.status = item_status
+        item.save()
 
-    serialized_item["status"] = item_status
-    serialized_item["low_stock_level"] = low_stock_level
+        item_data["status"] = item_status
+        item_data["red_line"] = red_line
 
-    result.append(serialized_item)
-
+        result.append(item_data)
     return result
 
 
