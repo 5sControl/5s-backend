cc6a7533e665bbbb518dd959830e202f8f55f203
anikeenko <dimskay1988@icloud.com>
2023-10-08 11:44:32 +0300

    EDIT port

--- config/settings/database.py
+++ config/settings/database.py
@@ -4,7 +4,7 @@ import sys
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "fiveScontrol",
+        "NAME": "fivescontrol",
         "USER": "admin",
         "PASSWORD": "just4Taqtile",
         "HOST": "localhost",
