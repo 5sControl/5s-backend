034f4184e969cb9f55ffbb2066ea637f9777a84f
anikeenko <dimskay1988@icloud.com>
2023-10-09 12:27:37 +0300

    EDIT database connection

--- config/settings/database.py
+++ config/settings/database.py
@@ -1,15 +1,18 @@
+from config.settings.base import config, BASE_DIR
+
 import sys
 
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "fiveScontrol",
-        "USER": "admin",
-        "PASSWORD": "just4Taqtile",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": config("DJANGO_DB_ENGINE", default="django.db.backends.sqlite3"),
+        "NAME": config("DJANGO_DB_NAME", default=BASE_DIR / "db.sqlite3"),
+        "USER": config("DJANGO_DB_USER", default=None),
+        "PASSWORD": config("DJANGO_DB_PASSWORD", default=None),
+        "HOST": config("DJANGO_DB_HOST", default=None),
+        "PORT": config("DJANGO_DB_PORT", default=None),
     },
+
     "database_for_test": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "test_database",
