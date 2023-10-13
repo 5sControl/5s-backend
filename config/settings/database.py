77223ca1b337ab0df74f2b8f67c0adaa9ec19336
artsiom <artsiomzhuchkou2401@gmail.com>
2023-10-13 10:57:11 +0300

    k8s: add REDIS_HOST env

--- config/settings/database.py
+++ config/settings/database.py
@@ -29,7 +29,7 @@ if "test" in sys.argv:
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://localhost:6379/0",
+        "LOCATION": "redis://" + config("REDIS_HOST", default="localhost") + ":6379/0",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         },
