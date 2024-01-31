4653093c373ecfff0b1f9f5e84163cf27b3b43c7
anikeenko <dimskay1988@icloud.com>
2024-01-31 14:29:43 +0300

    EDIT new field created_at

--- src/Extension/models.py
+++ src/Extension/models.py
@@ -5,11 +5,12 @@ class ExtensionReport(models.Model):
     text_post = models.TextField()
     text_comment = models.TextField()
     link_author_comment = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     # UserInfo
     about_user = models.TextField()
     about_author = models.TextField()
-    user_company = models.CharField(max_length=255)
+    user_company = models.CharField(max_length=255, default=None, null=True)
     user_experience = models.CharField(max_length=255)
     user_link = models.TextField(null=True, blank=True)
     user_name = models.CharField(max_length=255)
