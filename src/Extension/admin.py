0d96b859fde8c85f8c6e61bd441938fe8201883d
anikeenko <dimskay1988@icloud.com>
2024-01-31 14:42:01 +0300

    EDIT ExtensionReportAdmin

--- src/Extension/admin.py
+++ src/Extension/admin.py
@@ -4,8 +4,8 @@ from src.Extension.models import ExtensionReport
 
 @admin.register(ExtensionReport)
 class ExtensionReportAdmin(admin.ModelAdmin):
-    list_display = ('project_id', 'extension_user', 'user_name')
-    list_filter = ('project_id', 'extension_user')
+    list_display = ('project_id', 'extension_user', 'user_name', 'created_at')
+    list_filter = ('project_id', 'extension_user', 'created_at')
 
     def has_change_permission(self, request, obj=None):
         return False
