a00721d23e93ee9139c1eb771a5052884fafff72
anikeenko <dimskay1988@icloud.com>
2024-07-12 15:35:39 +0300

    ADDED get all assets

--- src/manifest_api/urls.py
+++ src/manifest_api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework import routers
-from src.manifest_api.views import ManifestConnectionViewSet, AssetClassesView
+from src.manifest_api.views import ManifestConnectionViewSet, AssetClassesView, AssetsView
 
 router = routers.DefaultRouter()
 router.register(r'login', ManifestConnectionViewSet)
 
 urlpatterns = [
     path('asset_classes/', AssetClassesView.as_view(), name='asset-classes'),
+    path('all_assets/', AssetsView.as_view(), name='assets'),
 ]
 
 urlpatterns += router.urls
--- src/manifest_api/views.py
+++ src/manifest_api/views.py
@@ -5,7 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from src.manifest_api.get_asset_classes import get_asset_classes
+from src.manifest_api.get_data import get_asset_classes
 from src.manifest_api.models import ManifestConnection
 from src.manifest_api.serializers import ManifestConnectionSerializer
 
@@ -18,7 +18,16 @@ class ManifestConnectionViewSet(viewsets.ModelViewSet):
 class AssetClassesView(APIView):
     def get(self, request):
         try:
-            data = get_asset_classes()
+            data = get_asset_classes(query="asset_class")
+            return Response(data, status=status.HTTP_200_OK)
+        except requests.exceptions.RequestException as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class AssetsView(APIView):
+    def get(self, request):
+        try:
+            data = get_asset_classes(query="asset")
             return Response(data, status=status.HTTP_200_OK)
         except requests.exceptions.RequestException as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
