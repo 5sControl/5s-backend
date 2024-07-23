0e2ffe2dbda311b9c8942617d5087c9c0c8250b3
anikeenko <dimskay1988@icloud.com>
2024-07-23 15:22:56 +0300

    EDIT method get_steps_by_asset_class

--- src/manifest_api/get_data.py
+++ src/manifest_api/get_data.py
@@ -1,5 +1,30 @@
 import requests
+
 from src.manifest_api.models import ManifestConnection
+
+
+def send_request(payload):
+    manifest = ManifestConnection.objects.last()
+    host = manifest.host
+    token = manifest.token
+    url = f"{host}graphql/v3"
+
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': token,
+        'User-Agent': 'PostmanRuntime/7.37.3',
+    }
+
+    response = None
+    try:
+        response = requests.post(url, headers=headers, data=payload)
+        response.raise_for_status()
+        data = response.json()
+        return data, response.status_code
+    except requests.exceptions.HTTPError as http_err:
+        return {'error': f'HTTP error occurred: {http_err}'}, response.status_code
+    except requests.exceptions.RequestException as req_err:
+        return {'error': f'Request exception occurred: {req_err}'}, 400
 
 
 def get_asset_classes(query, **kwargs):
@@ -28,36 +53,30 @@ def get_asset_classes(query, **kwargs):
 
     response = None
     try:
-
-        response = requests.request("POST", url, headers=headers, data=payload)
+        response = requests.post(url, headers=headers, data=payload)
+        response.raise_for_status()
         data = response.json()
         return data, response.status_code
-
-    except requests.exceptions.RequestException as e:
-        return {'error': str(e)}, response.status_code
+    except requests.exceptions.HTTPError as http_err:
+        return {'error': f'HTTP error occurred: {http_err}'}, response.status_code
+    except requests.exceptions.RequestException as req_err:
+        return {'error': f'Request exception occurred: {req_err}'}, 400
 
 
 def get_steps_by_asset_class():
-    manifest = ManifestConnection.objects.last()
-    # asset_class_id = kwargs.get("asset_class_id")
-    host = manifest.host
-    token = manifest.token
-    url = f"{host}graphql/v3"
+    data = []
+
     payload = "{\"query\":\"query(\\n  $itemsPerPage: Int\\n  $pageNumber: Int\\n  $filters: TemplateVersionsFiltersInput\\n  $orderBy: String\\n  $reverseOrder: Boolean\\n  $search: TemplateVersionSearchInput\\n  $sort: SortInput\\n) {\\n  allVersionsWithPagination(\\n    itemsPerPage: $itemsPerPage\\n    pageNumber: $pageNumber\\n    filters: $filters\\n    orderBy: $orderBy\\n    reverseOrder: $reverseOrder\\n    search: $search\\n    sort: $sort\\n  ) {\\n    id\\n    title\\n    createdAt\\n    assetClass {\\n      name\\n      id\\n    }\\n    author {\\n      id\\n    }\\n    customEvidence {\\n      id\\n      name\\n    }\\n    createdBy {\\n      firstName\\n      lastName\\n      avatarDetails {\\n        url\\n      }\\n    }\\n    steps {\\n        id\\n      step\\n      title\\n      noteTypesOrder\\n      requiredEvidence\\n      evidenceRequirements\\n      highlights {\\n        id\\n        position\\n        rotation\\n        type\\n        data\\n      }\\n      notes {\\n        autoplay\\n        id\\n        userId\\n        animationName\\n        modelViewId\\n        type\\n        meterRequirements {\\n          id\\n          value\\n          evaluationType\\n          meterId\\n        }\\n      }\\n    }\\n    actualVersion {\\n      id\\n      name\\n      state\\n      rootTemplateId\\n      templateId\\n      description\\n      activities {\\n        id\\n        action\\n        comment\\n        createdByUser {\\n          id\\n          firstName\\n          lastName\\n          avatarDetails {\\n            id\\n            url\\n          }\\n          isOnline\\n        }\\n        createdAt\\n      }\\n    }\\n    versions {\\n      id\\n      name\\n      state\\n      rootTemplateId\\n      description\\n      templateId\\n      activities {\\n        id\\n        action\\n        comment\\n        createdByUser {\\n          id\\n          firstName\\n          lastName\\n          avatarDetails {\\n            id\\n            url\\n          }\\n          isOnline\\n        }\\n        createdAt\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{\"pageNumber\":null,\"itemsPerPage\":3,\"filters\":{\"asset_class_id\":[2685],\"type\":[],\"status\":[\"published\"]},\"search\":{},\"sort\":{\"propertyName\":\"id\",\"reverse\":true}}}"
 
-    headers = {
-        'Content-Type': 'application/json',
-        'Authorization': token,
-        'User-Agent': 'PostmanRuntime/7.37.3',
-    }
+    response, status_code = send_request(payload)
+    if status_code != 200:
+        return [], status_code
 
-    response = None
-    try:
-        data = []
-        response = requests.request("POST", url, headers=headers, data=payload)
-        all_data = response.json().get('data').get('allVersionsWithPagination')
+    response_data = response.get('data')
+    if response_data:
+        all_data = response_data.get('allVersionsWithPagination', [])
         for item in all_data:
-            steps = item.get('steps')
+            steps = item.get('steps', [])
             for step in steps:
                 data.append(
                     {
@@ -65,8 +84,4 @@ def get_steps_by_asset_class():
                         "operationName": f'Step {step.get("step")} {step.get("title")}',
                     }
                 )
-
-        return data, response.status_code
-
-    except requests.exceptions.RequestException as e:
-        return {'error': str(e)}, response.status_code
+    return data, status_code
