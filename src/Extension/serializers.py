5b340f9faedcd1b8735a25e69e2e3b6157f7e3b6
anikeenko <dimskay1988@icloud.com>
2024-08-02 10:17:03 +0300

    DELETED extension

--- src/Extension/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from src.Extension.models import ExtensionReport
-
-
-class ExtensionReportSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ExtensionReport
-        fields = '__all__'
