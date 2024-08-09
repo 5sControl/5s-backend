4c22be5985d3ad0e50a6dc18014f7a4af86fc38b
anikeenko <dimskay1988@icloud.com>
2024-08-09 13:51:23 +0300

    ADDED response for operations

--- src/manifest_api/get_data.py
+++ src/manifest_api/get_data.py
@@ -104,11 +104,29 @@ def get_asset_classes(query, **kwargs):
         return {'error': f'Request exception occurred: {req_err}'}, 400
 
 
+def get_all_assets():
+    result = []
+    payload = "{\"query\":\"query(\\n  $assetClassId: Int, \\n  $locationId: Int, \\n  $maintRequired: Boolean, \\n  $outOfToleranceRange: Boolean\\n) {\\n  assets(\\n    assetClassId: $assetClassId, \\n    locationId: $locationId, \\n    maintRequired: $maintRequired, \\n    outOfToleranceRange: $outOfToleranceRange\\n  ) {\\n    assetClassId\\n    assets {\\n      id\\n      assetClassId\\n      locationId\\n      serialNumber\\n      internalId\\n      department\\n      criticality\\n      status\\n      assetTagId\\n      latitude\\n      longitude\\n      altitude\\n      assetClass {\\n        model\\n        make\\n        models {\\n          id\\n          name\\n          modelRotation\\n          isDefault\\n          modelOffset\\n          modelScale\\n          modelFiles {\\n            id\\n            name\\n            originalName\\n            url\\n            fileType\\n            contentType\\n            type\\n          }\\n        }\\n      }\\n      createdBy {\\n        id\\n        firstName\\n        lastName\\n        avatarDetails {\\n          id\\n          url\\n        }\\n        isOnline\\n      }\\n      updatedBy {\\n        id\\n        firstName\\n        lastName\\n        avatarDetails {\\n          id\\n          url\\n        }\\n        isOnline\\n      }\\n      location {\\n        name\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{}}"
+    response, status_code = send_request(payload)
+    if status_code != 200:
+        return [], status_code
+    asset_classes = response.get('data').get('assets')
+    for assets in asset_classes:
+        for asset in assets.get("assets"):
+            result.append(
+                {
+                    "asset_class_id": asset.get("assetClassId"),
+                    'id_asset': asset.get('id'),
+                    "location_id": asset.get('locationId'),
+                    "serial_number": asset.get('serialNumber'),
+                 }
+            )
+    return result
+
+
 def get_steps_by_asset_class():
     data = []
-    manifest = ManifestConnection.objects.last()
-    asset_class = manifest.asset_class_id
-    payload = "{\"query\":\"query(\\n  $itemsPerPage: Int\\n  $pageNumber: Int\\n  $filters: TemplateVersionsFiltersInput\\n  $orderBy: String\\n  $reverseOrder: Boolean\\n  $search: TemplateVersionSearchInput\\n  $sort: SortInput\\n) {\\n  allVersionsWithPagination(\\n    itemsPerPage: $itemsPerPage\\n    pageNumber: $pageNumber\\n    filters: $filters\\n    orderBy: $orderBy\\n    reverseOrder: $reverseOrder\\n    search: $search\\n    sort: $sort\\n  ) {\\n    id\\n    title\\n    createdAt\\n    assetClass {\\n      name\\n      id\\n    }\\n    author {\\n      id\\n    }\\n    customEvidence {\\n      id\\n      name\\n    }\\n    createdBy {\\n      firstName\\n      lastName\\n      avatarDetails {\\n        url\\n      }\\n    }\\n    steps {\\n        id\\n      step\\n      title\\n      noteTypesOrder\\n      requiredEvidence\\n      evidenceRequirements\\n      highlights {\\n        id\\n        position\\n        rotation\\n        type\\n        data\\n      }\\n      notes {\\n        autoplay\\n        id\\n        userId\\n        animationName\\n        modelViewId\\n        type\\n        meterRequirements {\\n          id\\n          value\\n          evaluationType\\n          meterId\\n        }\\n      }\\n    }\\n    actualVersion {\\n      id\\n      name\\n      state\\n      rootTemplateId\\n      templateId\\n      description\\n      activities {\\n        id\\n        action\\n        comment\\n        createdByUser {\\n          id\\n          firstName\\n          lastName\\n          avatarDetails {\\n            id\\n            url\\n          }\\n          isOnline\\n        }\\n        createdAt\\n      }\\n    }\\n    versions {\\n      id\\n      name\\n      state\\n      rootTemplateId\\n      description\\n      templateId\\n      activities {\\n        id\\n        action\\n        comment\\n        createdByUser {\\n          id\\n          firstName\\n          lastName\\n          avatarDetails {\\n            id\\n            url\\n          }\\n          isOnline\\n        }\\n        createdAt\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{\"pageNumber\":null,\"itemsPerPage\":3,\"filters\":{\"asset_class_id\":[" + f"{asset_class}" + "],\"type\":[],\"status\":[\"published\"]},\"search\":{},\"sort\":{\"propertyName\":\"id\",\"reverse\":true}}}"
+    payload = "{\"query\":\"query(\\n  $itemsPerPage: Int\\n  $pageNumber: Int\\n  $filters: TemplateVersionsFiltersInput\\n  $orderBy: String\\n  $reverseOrder: Boolean\\n  $search: TemplateVersionSearchInput\\n  $sort: SortInput\\n) {\\n  allVersionsWithPagination(\\n    itemsPerPage: $itemsPerPage\\n    pageNumber: $pageNumber\\n    filters: $filters\\n    orderBy: $orderBy\\n    reverseOrder: $reverseOrder\\n    search: $search\\n    sort: $sort\\n  ) {\\n    id\\n    title\\n    createdAt\\n    assetClass {\\n      name\\n      id\\n    }\\n    author {\\n      id\\n    }\\n    customEvidence {\\n      id\\n      name\\n    }\\n    createdBy {\\n      firstName\\n      lastName\\n      avatarDetails {\\n        url\\n      }\\n    }\\n    steps {\\n        id\\n      step\\n      title\\n      noteTypesOrder\\n      requiredEvidence\\n      evidenceRequirements\\n      highlights {\\n        id\\n        position\\n        rotation\\n        type\\n        data\\n      }\\n      notes {\\n        autoplay\\n        id\\n        userId\\n        animationName\\n        modelViewId\\n        type\\n        meterRequirements {\\n          id\\n          value\\n          evaluationType\\n          meterId\\n        }\\n      }\\n    }\\n    actualVersion {\\n      id\\n      name\\n      state\\n      rootTemplateId\\n      templateId\\n      description\\n      activities {\\n        id\\n        action\\n        comment\\n        createdByUser {\\n          id\\n          firstName\\n          lastName\\n          avatarDetails {\\n            id\\n            url\\n          }\\n          isOnline\\n        }\\n        createdAt\\n      }\\n    }\\n    versions {\\n      id\\n      name\\n      state\\n      rootTemplateId\\n      description\\n      templateId\\n      activities {\\n        id\\n        action\\n        comment\\n        createdByUser {\\n          id\\n          firstName\\n          lastName\\n          avatarDetails {\\n            id\\n            url\\n          }\\n          isOnline\\n        }\\n        createdAt\\n      }\\n    }\\n  }\\n}\\n\",\"variables\":{\"pageNumber\":null,\"itemsPerPage\":3,\"filters\":{\"asset_class_id\":[],\"type\":[],\"status\":[\"published\"]},\"search\":{},\"sort\":{\"propertyName\":\"id\",\"reverse\":true}}}"
 
     response, status_code = send_request(payload)
     if status_code != 200:
@@ -116,14 +134,34 @@ def get_steps_by_asset_class():
 
     response_data = response.get('data')
     if response_data:
+        all_assets = get_all_assets()
         all_data = response_data.get('allVersionsWithPagination', [])
-        for item in all_data:
-            steps = item.get('steps', [])
-            for step in steps:
+        for asset in all_assets:
+            for template in all_data:
+                asset_name = asset.get("serial_number")
+                asset_id = asset.get("id_asset")
+                steps = template.get('steps', [])
                 data.append(
                     {
-                        "id": step.get("id"),
-                        "operationName": f'Step {step.get("step")} {step.get("title")}',
+                        # "id": step.get("id"),
+                        "id": None,
+                        "operationName": f"{asset_name}({asset_id}){template.get('title')}({template.get('id')})",
+                        "asset_class_id": asset.get("asset_class_id"),
+                        "id_asset": asset_id,
+                        "location_id": asset.get("location_id"),
+                        "serial_number": asset_name
                     }
                 )
-    return data, status_code
+                for step in steps:
+                    result = (
+                        {
+                            "id": step.get("id"),
+                            "operationName": f"{asset_name}({asset_id}){template.get('title')}({template.get('id')}).{step.get('title')}(Step{step.get('step')})",
+                            "asset_class_id": asset.get("asset_class_id"),
+                            "id_asset": asset_id,
+                            "location_id": asset.get("location_id"),
+                            "serial_number": asset_name
+                        }
+                    )
+                    data.append(result)
+        return data, status_code
