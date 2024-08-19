122933098783b97dd1de24c445a4533823910730
anikeenko <dimskay1988@icloud.com>
2024-08-19 12:11:07 +0300

    EDIT models FiltrationOperationsTypeID

--- src/newOrderView/models.py
+++ src/newOrderView/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class FiltrationOperationsTypeID(models.Model):
-    operation_type_id = models.IntegerField(unique=True)
+    operation_type_id = models.IntegerField(unique=True, null=True, blank=True)
     name = models.CharField(max_length=150, blank=True)
     is_active = models.BooleanField(default=True)
 
