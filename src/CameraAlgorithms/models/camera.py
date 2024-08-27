41a5e577cb96ef661b78fa3ba36e0013b48a2cf2
anikeenko <dimskay1988@icloud.com>
2024-08-27 16:05:38 +0300

    FIXED restart algorithm #4

--- src/CameraAlgorithms/models/camera.py
+++ src/CameraAlgorithms/models/camera.py
@@ -30,8 +30,7 @@ class Camera(models.Model):
         return self.id
 
     def save(self, *args, **kwargs):
-        print("camera", self.id, "password", self.password)
-        if self.password:
+        if self._state.adding and self.password:
             self.password = encrypt(self.password)
         if not self.name:
             self.name = self.id
