b711b16adefd47b67f495d0b661d4049c1f28b9f
anikeenko <dimskay1988@icloud.com>
2024-08-30 11:18:49 +0300

    UPDATE verify

--- src/manifest_api/models.py
+++ src/manifest_api/models.py
@@ -44,7 +44,7 @@ class ManifestConnection(models.Model):
         }
 
         try:
-            response = requests.post(url, json=data, headers=headers)
+            response = requests.post(url, json=data, headers=headers, verify=False)
             response.raise_for_status()
             response_data = response.json()
             token = response_data.get('user').get('token')
