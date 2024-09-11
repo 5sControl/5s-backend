b22ff7bb84f0c87ddaf200d31aa19dde1409401e
anikeenko <dimskay1988@icloud.com>
2024-09-11 16:22:23 +0300

    RENAME tables SystemMessage

--- src/Core/models.py
+++ src/Core/models.py
@@ -6,3 +6,12 @@ class SystemMessage(models.Model):
     title = models.CharField(max_length=150)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "System message"
+        verbose_name_plural = "System message"
+        db_table = "system_message"
+
