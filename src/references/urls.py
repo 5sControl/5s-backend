988f14bda799a6c983668c1efcdbcc9a30a1a57e
anikeenko <dimskay1988@icloud.com>
2024-10-02 11:10:57 +0300

    EDITED urlpatterns

--- src/references/urls.py
+++ src/references/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.urls import re_path
 
 from src.references.views import ErpReferenceView
 
 router = DefaultRouter()
 
 urlpatterns = [
-    path('<str:reference_type>/', ErpReferenceView.as_view(), name='erp-reference'),
+    re_path(r'^(?P<reference_type>.*)/$', ErpReferenceView.as_view(), name='erp-reference'),
     path("", include(router.urls)),
 ]
