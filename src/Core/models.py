e15c41eac36d67917bc41f8e8d3f6d7f8f03907b
anikeenko <dimskay1988@icloud.com>
2024-11-13 13:22:09 +0300

    ADDED CustomPermission

--- src/Core/models.py
+++ src/Core/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 
+#
+class CustomPermission(models.Model):
+    codename = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'auth_permission_custom'
+        verbose_name = 'Custom Permission'
+        verbose_name_plural = 'Custom Permissions'
+
+    def __str__(self):
+        return self.name
+
+
 class SystemMessage(models.Model):
     id = models.AutoField(primary_key=True, db_index=True)
     title = models.CharField(max_length=150)
