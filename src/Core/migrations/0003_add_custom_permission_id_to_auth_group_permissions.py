0575d4b5d7757c055ba9e60da74ff1eaab220e1b
anikeenko <dimskay1988@icloud.com>
2024-11-14 09:21:39 +0300

    UPDATED migrations CustomPermission

--- src/Core/migrations/0003_add_custom_permission_id_to_auth_group_permissions.py
+++ src/Core/migrations/0003_add_custom_permission_id_to_auth_group_permissions.py
@@ -4,9 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ('Core', '0002_custompermission'),
+        ('auth', '0001_initial'),
     ]
 
     operations = [
