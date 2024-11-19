92aeea0c6d6d54b8c1b12bfadf15ab95bc331757
anikeenko <dimskay1988@icloud.com>
2024-11-19 15:38:45 +0300

    FIXED FiltrationOperationsAdmin

--- src/newOrderView/admin.py
+++ src/newOrderView/admin.py
@@ -4,4 +4,4 @@ from src.newOrderView.models import FiltrationOperationsTypeID
 
 @admin.register(FiltrationOperationsTypeID)
 class FiltrationOperationsAdmin(admin.ModelAdmin):
-    list_display = ("operation_type_id", "name", "is_active", "id")
+    list_display = ("operation_type_id", "type_erp", "name", "is_active", "id")
