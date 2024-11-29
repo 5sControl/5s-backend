eba3f01f9ee3c510b7dace0a9b36cb75fd10ea91
anikeenko <dimskay1988@icloud.com>
2024-11-29 12:50:50 +0300

    UPDATED migrations

--- src/Employees/migrations/0002_customuser_workplace_id.py
+++ src/Employees/migrations/0002_customuser_workplace_id.py
@@ -6,7 +6,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("erp_5s", "0003_alter_referenceitems_table"),
         ("Employees", "0001_initial"),
     ]
 
