471a7c835ac29d84377d52b340e5491e03075a48
anikeenko <dimskay1988@icloud.com>
2024-12-02 14:04:21 +0300

    UPDATED models FiltrationOperationsTypeID

--- src/newOrderView/models.py
+++ src/newOrderView/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class FiltrationOperationsTypeID(models.Model):
-    operation_type_id = models.IntegerField(unique=True, null=True, blank=True)
+    operation_type_id = models.IntegerField(null=True, blank=True)
     name = models.CharField(max_length=150, blank=True)
     is_active = models.BooleanField(default=True)
     type_erp = models.CharField(max_length=50, default=None, null=True, blank=True)
