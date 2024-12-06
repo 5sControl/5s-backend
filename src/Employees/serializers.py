6376fb2c8eb7feb9bef15f6c0f6713efb3ec0c91
anikeenko <dimskay1988@icloud.com>
2024-12-06 11:28:03 +0300

    UPDATED UserSerializer

--- src/Employees/serializers.py
+++ src/Employees/serializers.py
@@ -1,17 +1,27 @@
 from rest_framework import serializers
 
 from src.Employees.models import CustomUser
+from src.erp_5s.models import ReferenceItems
 from src.erp_5s.serializers import ReferenceItemsSerializerEmployees
 
 
 class UserSerializer(serializers.ModelSerializer):
-    workplace = ReferenceItemsSerializerEmployees()
+    workplace = serializers.SerializerMethodField()
     date_joined = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
         model = CustomUser
         fields = ["id", "username", "password", "first_name", "last_name", "date_joined", "role", "workplace"]
         read_only_fields = ["date_joined"]
+
+    def get_workplace(self, obj):
+        if obj.workplace_id:
+            try:
+                workplace = ReferenceItems.objects.get(id=obj.workplace_id)
+                return ReferenceItemsSerializerEmployees(workplace).data
+            except ReferenceItems.DoesNotExist:
+                return None
+        return None
 
     def update(self, instance, validated_data):
         password = validated_data.pop('password', None)
