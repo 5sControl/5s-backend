9b73426eee86c699dce091df4132f9e6efb298dd
anikeenko <dimskay1988@icloud.com>
2024-12-16 12:10:54 +0300

    UPDATED permissions

--- src/Core/permissions.py
+++ src/Core/permissions.py
@@ -20,6 +20,11 @@ class IsSuperuserPermission(permissions.BasePermission):
         return request.user.is_authenticated and request.user.is_superuser
 
 
+class IsAdminPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == CustomUser.ADMIN
+
+
 class IsStaffPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.is_staff
