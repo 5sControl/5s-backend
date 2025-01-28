9eeff398189ef5b9ab3f5e0cf919ba48f5008305
anikeenko <dimskay1988@icloud.com>
2025-01-28 18:45:20 +0300

    ADDED new migrations

--- src/Employees/models.py
+++ src/Employees/models.py
@@ -20,7 +20,7 @@ class CustomUser(AbstractUser):
 
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=WORKER)
     workplace_id = models.IntegerField(blank=True, null=True, default=None)
-    email = models.EmailField(null=True, blank=True, unique=True, default=None)
+    email = models.EmailField(blank=True, unique=True, default="")
 
     def __str__(self):
         return self.username
