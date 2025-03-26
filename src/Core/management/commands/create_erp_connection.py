0d5d70f19ae4499c8ae2757075300d66d3a36526
anikeenko <dimskay1988@icloud.com>
2025-03-26 09:32:12 +0300

    UPDATED management command

--- src/Core/management/commands/create_erp_connection.py
+++ src/Core/management/commands/create_erp_connection.py
@@ -1,5 +1,4 @@
 import logging
-
 from django.core.management.base import BaseCommand
 from src.DatabaseConnections.models import ConnectionInfo
 
@@ -11,25 +10,30 @@ class Command(BaseCommand):
     help = "Create connection for erp-service."
 
     def handle(self, *args, **options):
-        connection, created = ConnectionInfo.objects.get_or_create(
-            erp_system="5s_control",
-            defaults={
-                "type": "api",
-                "dbms": "postgres",
-                "is_active": True,
-                "host": "http://erp-service",
-                "server": None,
-                "database": None,
-                "username": "admin",
-                "password": "admin",
-                "port": 3005,
-                "used_in_orders_view": True,
-            },
-        )
+        try:
+            connection, created = ConnectionInfo.objects.get_or_create(
+                erp_system="5s_control",
+                defaults={
+                    "type": "api",
+                    "dbms": "postgres",
+                    "is_active": True,
+                    "host": "http://erp-service",
+                    "server": None,
+                    "database": None,
+                    "username": "admin",
+                    "password": "admin",
+                    "port": 3005,
+                    "used_in_orders_view": True,
+                },
+            )
 
-        if created:
-            logger.info("Connection created successfully.")
-            self.stdout.write(self.style.SUCCESS("✅ Connection created successfully."))
-        else:
-            logger.info("Connection already exists.")
-            self.stdout.write(self.style.SUCCESS("ℹ️ Connection already exists."))
+            if created:
+                logger.info("Connection created successfully.")
+                self.stdout.write(self.style.SUCCESS("✅ Connection created successfully."))
+            else:
+                logger.info("Connection already exists.")
+                self.stdout.write(self.style.SUCCESS("ℹ️ Connection already exists."))
+
+        except Exception as e:
+            logger.error(f"Error creating connection: {e}")
+            self.stdout.write(self.style.ERROR(f"❌ Error creating connection: {e}"))
